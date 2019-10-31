c802f78c6ca5ce3d9771411a73434689154b0e70
JiangfengLi <lijiangfeng2016@email.arizona.edu>
2019-10-30 19:24:25 -0700

    Jiangfeng Branch commit -- change of CraftItemGoal 19:23pm 10/30/2019

--- external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Mission/Goal/CraftItemGoal.java
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Mission/Goal/CraftItemGoal.java
@@ -1,5 +1,46 @@
 package edu.arizona.tomcat.Mission.Goal;
 
-public class CraftItemGoal {
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CraftItemGoal extends  MissionGoal {
+    private static ItemStack itemStack;
+
+    /* constructor to recieve Itemstack from libraries, check the player's inventory, */
+
+    /* update check whether the item is contained */
+
+    // check the type of Item
+
+    public CraftItemGoal(ItemStack itemSt){
+        this.itemStack =  itemSt;
+    }
+
+    @Override
+    public void update(World world)
+    {
+        if (!this.goalAchieved) {
+            EntityPlayer player = world.getPlayerEntityByName(Minecraft.getMinecraft().player.getName());
+            Iterator itemStacks = player.getHeldEquipment().iterator();
+            int count = 0;
+            while(itemStacks.hasNext()) {
+                ItemStack pItemSt = (ItemStack)itemStacks.next();
+                if (itemStacks.equals(pItemSt)) {
+                    hasBeenAchieved();
+                }
+                System.out.println("Item(Stack) " + pItemSt.getDisplayName() + " is contained");
+                count++;
+            }
+            System.out.println("Totally " + count + " items(Stack) in players inventory");
+        }
+
+    }
 
 }
