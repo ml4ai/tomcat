351804911c2a22ef734e04e7dcaae1f20264e950
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-11-01 06:45:07 -0700

    Changing name of Python bindings module to pytomcat, updating src/CMakeLists.txt

--- src/Python_wrapper.cpp
+++ src/Python_wrapper.cpp
@@ -4,11 +4,12 @@
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
+using namespace tomcat;
 
-PYBIND11_MODULE(tomcatPython, m) {
+PYBIND11_MODULE(pytomcat, m) {
   py::class_<LocalAgent>(m, "LocalAgent")
       .def(py::init<>())
       .def("sendCommand", &LocalAgent::sendCommand)
-      .def("set_mission", &LocalAgent::set_mission)
+      .def("set_mission", &LocalAgent::setMission)
       .def("startMission", &LocalAgent::startMission);
 }
