fa5a194eb13bf15c16e358e53ceaf95d57e8f4e6
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-11-19 08:52:07 -0700

    Factoring out OpenFace model download from the build

--- tools/download_tomcat_data.sh
+++ tools/download_tomcat_data.sh
@@ -9,4 +9,7 @@ cd ${TOMCAT}/data
 # Download and extract the manually-created worlds from vanga.
 curl -O http://vanga.sista.arizona.edu/tomcat/data/worlds.tgz
 tar -xvzf worlds.tgz && rm -rf worlds.tgz
-cd ..
+
+# Download and extract the OpenFace models
+curl -O http://vision.cs.arizona.edu/adarsh/OpenFace_models.tgz
+tar -xvzf OpenFace_models.tgz && rm -rf OpenFace_models.tgz
