5fa7d68b32b76f88eb5501262bd3506b7111f469
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-11-24 16:38:52 -0700

    Fixed loading of face landmark detector parameters in OpenFace to use the TOMCAT environment variable. Also cleaned/refactored some OpenFace code.

--- external/OpenFace/lib/local/LandmarkDetector/include/LandmarkDetectionValidator.h
+++ external/OpenFace/lib/local/LandmarkDetector/include/LandmarkDetectionValidator.h
@@ -4,8 +4,9 @@
 //
 // ACADEMIC OR NON-PROFIT ORGANIZATION NONCOMMERCIAL RESEARCH USE ONLY
 //
-// BY USING OR DOWNLOADING THE SOFTWARE, YOU ARE AGREEING TO THE TERMS OF THIS LICENSE AGREEMENT.  
-// IF YOU DO NOT AGREE WITH THESE TERMS, YOU MAY NOT USE OR DOWNLOAD THE SOFTWARE.
+// BY USING OR DOWNLOADING THE SOFTWARE, YOU ARE AGREEING TO THE TERMS OF THIS
+// LICENSE AGREEMENT. IF YOU DO NOT AGREE WITH THESE TERMS, YOU MAY NOT USE OR
+// DOWNLOAD THE SOFTWARE.
 //
 // License can be found in OpenFace-license.txt
 //
@@ -14,26 +15,28 @@
 //       reports and manuals, must cite at least one of the following works:
 //
 //       OpenFace 2.0: Facial Behavior Analysis Toolkit
-//       Tadas Baltrušaitis, Amir Zadeh, Yao Chong Lim, and Louis-Philippe Morency
-//       in IEEE International Conference on Automatic Face and Gesture Recognition, 2018  
+//       Tadas Baltrušaitis, Amir Zadeh, Yao Chong Lim, and Louis-Philippe
+//       Morency in IEEE International Conference on Automatic Face and Gesture
+//       Recognition, 2018
 //
-//       Convolutional experts constrained local model for facial landmark detection.
-//       A. Zadeh, T. Baltrušaitis, and Louis-Philippe Morency,
-//       in Computer Vision and Pattern Recognition Workshops, 2017.    
+//       Convolutional experts constrained local model for facial landmark
+//       detection. A. Zadeh, T. Baltrušaitis, and Louis-Philippe Morency, in
+//       Computer Vision and Pattern Recognition Workshops, 2017.
 //
 //       Rendering of Eyes for Eye-Shape Registration and Gaze Estimation
-//       Erroll Wood, Tadas Baltrušaitis, Xucong Zhang, Yusuke Sugano, Peter Robinson, and Andreas Bulling 
-//       in IEEE International. Conference on Computer Vision (ICCV),  2015 
+//       Erroll Wood, Tadas Baltrušaitis, Xucong Zhang, Yusuke Sugano, Peter
+//       Robinson, and Andreas Bulling in IEEE International. Conference on
+//       Computer Vision (ICCV),  2015
 //
-//       Cross-dataset learning and person-specific normalisation for automatic Action Unit detection
-//       Tadas Baltrušaitis, Marwa Mahmoud, and Peter Robinson 
-//       in Facial Expression Recognition and Analysis Challenge, 
-//       IEEE International Conference on Automatic Face and Gesture Recognition, 2015 
+//       Cross-dataset learning and person-specific normalisation for automatic
+//       Action Unit detection Tadas Baltrušaitis, Marwa Mahmoud, and Peter
+//       Robinson in Facial Expression Recognition and Analysis Challenge, IEEE
+//       International Conference on Automatic Face and Gesture Recognition,
+//       2015
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef LANDMARK_DETECTION_VALIDATOR_H
-#define LANDMARK_DETECTION_VALIDATOR_H
+#pragma once
 
 // OpenCV includes
 #include <opencv2/core/core.hpp>
@@ -44,74 +47,74 @@
 // Local includes
 #include "PAW.h"
 
-using namespace std;
 
-namespace LandmarkDetector
-{
-//===========================================================================
-//
-// Checking if landmark detection was successful using a CNN
-// Using multiple validators trained add different views
-// The regressor outputs 1 for ideal alignment and 0 for worst alignment
-//===========================================================================
-class DetectionValidator
-{
-		
-public:    
-	
-	// The orientations of each of the landmark detection validator
-	vector<cv::Vec3d> orientations;
+namespace LandmarkDetector {
+  using namespace std;
+  //===========================================================================
+  //
+  // Checking if landmark detection was successful using a CNN
+  // Using multiple validators trained add different views
+  // The regressor outputs 1 for ideal alignment and 0 for worst alignment
+  //===========================================================================
+  class DetectionValidator {
 
-	// Piecewise affine warps to the reference shape (per orientation)
-	vector<PAW>     paws;
+  public:
+    // The orientations of each of the landmark detection validator
+    vector<cv::Vec3d> orientations;
 
-	//==========================================
-	// Convolutional Neural Network
+    // Piecewise affine warps to the reference shape (per orientation)
+    vector<PAW> paws;
 
-	// CNN layers for each view
-	// view -> layer
-	vector<vector<vector<vector<cv::Mat_<float> > > > > cnn_convolutional_layers;
-	vector<vector<cv::Mat_<float> > > cnn_convolutional_layers_weights;
-	vector<vector<cv::Mat_<float> > > cnn_convolutional_layers_im2col_precomp;
+    //==========================================
+    // Convolutional Neural Network
 
-	vector< vector<int> > cnn_subsampling_layers;
-	vector< vector<cv::Mat_<float> > > cnn_fully_connected_layers_weights;
-	vector< vector<cv::Mat_<float>  > > cnn_fully_connected_layers_biases;
-	// NEW CNN: 0 - convolutional, 1 - max pooling (2x2 stride 2), 2 - fully connected, 3 - relu, 4 - sigmoid
-	vector<vector<int> > cnn_layer_types;
-	
-	//==========================================
+    // CNN layers for each view
+    // view -> layer
+    vector<vector<vector<vector<cv::Mat_<float>>>>> cnn_convolutional_layers;
+    vector<vector<cv::Mat_<float>>> cnn_convolutional_layers_weights;
+    vector<vector<cv::Mat_<float>>> cnn_convolutional_layers_im2col_precomp;
 
-	// Normalisation for face validation
-	vector<cv::Mat_<float> > mean_images;
-	vector<cv::Mat_<float> > standard_deviations;
+    vector<vector<int>> cnn_subsampling_layers;
+    vector<vector<cv::Mat_<float>>> cnn_fully_connected_layers_weights;
+    vector<vector<cv::Mat_<float>>> cnn_fully_connected_layers_biases;
+    // NEW CNN: 0 - convolutional, 1 - max pooling (2x2 stride 2), 2 - fully
+    // connected, 3 - relu, 4 - sigmoid
+    vector<vector<int>> cnn_layer_types;
 
-	// Default constructor
-	DetectionValidator(){;}
+    //==========================================
 
-	// Copy constructor
-	DetectionValidator(const DetectionValidator& other);
+    // Normalisation for face validation
+    vector<cv::Mat_<float>> mean_images;
+    vector<cv::Mat_<float>> standard_deviations;
 
-	// Given an image, orientation and detected landmarks output the result of the appropriate regressor
-	float Check(const cv::Vec3d& orientation, const cv::Mat_<uchar>& intensity_img, cv::Mat_<float>& detected_landmarks);
+    // Default constructor
+    DetectionValidator() { ; }
 
-	// Reading in the model
-	void Read(string location);
-			
-	// Getting the closest view center based on orientation
-	int GetViewId(const cv::Vec3d& orientation) const;
+    // Copy constructor
+    DetectionValidator(const DetectionValidator& other);
 
-private:
+    // Given an image, orientation and detected landmarks output the result of
+    // the appropriate regressor
+    float Check(const cv::Vec3d& orientation,
+                const cv::Mat_<uchar>& intensity_img,
+                cv::Mat_<float>& detected_landmarks);
 
-	// The actual regressor application on the image
+    // Reading in the model
+    void Read(string location);
 
-	// Convolutional Neural Network
-	double CheckCNN(const cv::Mat_<float>& warped_img, int view_id);
+    // Getting the closest view center based on orientation
+    int GetViewId(const cv::Vec3d& orientation) const;
 
-	// A normalisation helper
-	void NormaliseWarpedToVector(const cv::Mat_<float>& warped_img, cv::Mat_<float>& feature_vec, int view_id);
+  private:
+    // The actual regressor application on the image
 
-};
+    // Convolutional Neural Network
+    double CheckCNN(const cv::Mat_<float>& warped_img, int view_id);
 
-}
-#endif // LANDMARK_DETECTION_VALIDATOR_H
+    // A normalisation helper
+    void NormaliseWarpedToVector(const cv::Mat_<float>& warped_img,
+                                 cv::Mat_<float>& feature_vec,
+                                 int view_id);
+  };
+
+} // namespace LandmarkDetector
