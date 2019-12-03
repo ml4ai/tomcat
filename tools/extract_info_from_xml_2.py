64ecfb8762891617281c9d03698af852cd1787bd
ling-postfix <matsuura@lingprefix.info>
2019-12-02 18:56:35 -0700

    Revert "Included ruamel.yaml to output XML Schema to YAML Ontology"
    This reverts commit 6f1bdf5be6cdfb365966b589fdd864f22d78dde7.

--- tools/extract_info_from_xml_2.py
+++ tools/extract_info_from_xml_2.py
@@ -1,28 +1,19 @@
 # This script constructs a dictionary with Minecraft types to bootstrap a
 # Minecraft ontology for reading.
 
-# Usage: python extract_info_from_xml_2.py <SCHEMA_FILE> .. 
-import sys
-
-from ruamel.yaml import YAML
-from pathlib import Path
+# Usage: python extract_info_from_xml_2.py
 
 import xml.etree.ElementTree as ET
 from pprint import pprint
 
-yaml = YAML(typ='safe')
-yaml.default_flow_style = False
-
 minecraft_ontology_dict = {}
 
-if len(sys.argv) > 1:
-    for schema in sys.argv[1:]:
-        tree = ET.parse(schema)
-        out = open(Path(Path(schema).parent).joinpath('../YAML/' + Path(schema).stem + '.yaml'), 'w')
-        for simple_type in tree.getroot():
-            category = simple_type.attrib["name"]
-            for restriction_or_union in simple_type:
-                if restriction_or_union.attrib!={}:
-                    minecraft_ontology_dict[category] = [member.attrib['value'] for member in restriction_or_union]
-        data = yaml.load(str(minecraft_ontology_dict))
-        yaml.dump(data, out)
+tree = ET.parse('../external/malmo/Schemas/Types.xsd.in')
+
+for simple_type in tree.getroot():
+    category = simple_type.attrib["name"]
+    for restriction_or_union in simple_type:
+        if restriction_or_union.attrib!={}:
+            minecraft_ontology_dict[category] = [member.attrib['value'] for member in restriction_or_union]
+
+pprint(minecraft_ontology_dict)
