d4b44060044255ae3eb38a302d897172fbd1c540
kobus-barnard <kobus@cs.arizona.edu>
2020-01-05 16:18:01 -0700

    Adding first version of run_session (#50)
    The script run_session is ready to go, but it does not actually work that well,
    and likely cannot work very well until changes are in the C++ code which we need
    anyway. Some issues are listed in bugs.txt. Still, there is no harm playing with
    run_session, and if things hang, there is the handy script kill_minecraftng,
    also being introduced with this merge. 
    
    This branch also includes some work in making scripts more robust, such as not
    relying on TOMCAT variable. It is actually OK to rely on such a thing, but then
    the script *must* test it is set to something reasonable and give an error
    message and exit with failure if not the case. However, I prefer that the
    scripts that users might run are able to sort out TOMCAT. It you put it in your
    bashrc file, then it is wrong if you branch, etc.

--- tools/download_tomcat_worlds.sh
+++ tools/download_tomcat_worlds.sh
@@ -2,7 +2,31 @@
 
 # Script to download Minecraft worlds for ToMCAT
 
-set -e
+# Kobus says 'set -e' does not make sense if you are checking returns as you
+# are doing.
+#
+# set -e
+
+###############################################################################
+
+# If TOMCAT is set as an enviroment variable, we will respect it. 
+
+declare -x TOMCAT
+if [ ! -z "$TOMCAT" ]; then
+    echo "Script check_minecraft is using requested TOMCAT location ${TOMCAT}."
+else 
+    # This script should be in a directory 'tools' which should be a subdirectory of
+    # the TOMCAT directory. The following uses those assumptions to determine
+    # TOMCAT.
+    #
+    called_as_dir=`echo $0 | sed 's#^[^/][^/]*$#./#'`
+    called_as_dir=`echo $called_as_dir | sed 's#^\(.*\)/.*$#\1#'`
+    pushd "${called_as_dir}" > /dev/null; called_as_dir=`pwd`; popd > /dev/null
+    export TOMCAT=`echo $called_as_dir | sed 's#^\./##' | sed 's#^\(.*\)/tools$#\1#'`
+    echo "Script check_minecraft is using inferreed TOMCAT location ${TOMCAT}."
+fi
+
+###############################################################################
 
 echo "Downloading ToMCAT Minecraft worlds."
 
---
+++ tools/run_session.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+set -u 
+
+mission_one_time=600
+do_tutorial=1
+do_invasion=1
+
+###############################################################################
+
+# If TOMCAT is set as an enviroment variable, we will respect it. 
+
+declare -x TOMCAT
+if [ ! -z "$TOMCAT" ]; then
+    echo "Script check_minecraft is using requested TOMCAT location ${TOMCAT}."
+else 
+    # This script should be in a directory 'tools' which should be a subdirectory of
+    # the TOMCAT directory. The following uses those assumptions to determine
+    # TOMCAT.
+    #
+    called_as_dir=`echo $0 | sed 's#^[^/][^/]*$#./#'`
+    called_as_dir=`echo $called_as_dir | sed 's#^\(.*\)/.*$#\1#'`
+    pushd "${called_as_dir}" > /dev/null; called_as_dir=`pwd`; popd > /dev/null
+    export TOMCAT=`echo $called_as_dir | sed 's#^\./##' | sed 's#^\(.*\)/tools$#\1#'`
+    echo "Script check_minecraft is using inferreed TOMCAT location ${TOMCAT}."
+fi
+
+###############################################################################
+
+# ${TOMCAT}/tools/install.sh
+
+${TOMCAT}/tools/check_minecraft.sh
+
+export TOMCAT_TMP_DIR="/tmp/$USER/tomcat"
+mkdir -p "${TOMCAT_TMP_DIR}"
+if [[ $? -ne 0 ]]; then exit 1; fi;
+
+tutorial_mission_log="${TOMCAT_TMP_DIR}/tutorial_mission.log"
+zombie_invasion_log="${TOMCAT_TMP_DIR}/zombie_invasion.log"
+
+num_tries=2
+
+if [[ ${do_tutorial} -eq 1 ]]; then
+    try=0
+    while [ $try -lt $num_tries ]; do 
+        echo " "
+        echo "Running the tutorial mission in ${TOMCAT}."
+        echo " "
+
+        ${TOMCAT}/build/bin/runExperiment --mission 0 >& ${tutorial_mission_log} &
+        bg_pid=$!
+        echo "Running: ${TOMCAT}/build/bin/runExperiment --mission 0"
+        echo "Process is $bg_pid" 
+        echo "Waiting for it"
+        wait
+        tutorial_mission_status=$?
+
+        if [[ ${tutorial_mission_status} -eq 0 ]]; then
+            tutorial_mission_status=`grep -c 'Error starting mission' ${tutorial_mission_log}`
+        fi
+
+
+        if [[ ${tutorial_mission_status} -eq 0 ]]; then
+            echo "Tutorial mission ended with success status."
+            echo " "
+            break
+        fi 
+
+        let try+=1 
+
+        if [[ $try -lt $num_tries ]]; then 
+            echo "Tutorial mission ended with failure status."
+            echo "Killing all Minecraft and Malmo processes that can be found and trying again."
+            ${TOMCAT}/tools/kill_minecraft.sh
+            ${TOMCAT}/tools/check_minecraft.sh
+        fi 
+    done
+fi
+
+if [[ ${do_invasion} -eq 1 ]]; then
+    echo " "
+    echo "Running the Zombie invasion mission in ${TOMCAT}."
+    echo " "
+
+    try=0
+    while [ $try -lt $num_tries ]; do 
+        ${TOMCAT}/build/bin/runExperiment --mission 1 --time_limit ${mission_one_time}>& ${zombie_invasion_log} &
+        bg_pid=$!
+        echo "Running: ${TOMCAT}/build/bin/runExperiment --mission 1 --time_limit ${mission_one_time}"
+        echo "Process is $bg_pid" 
+        echo "Waiting for it"
+        echo " "
+        wait
+        zombie_invasion_status=$?
+
+        if [[ ${zombie_invasion_status} -eq 0 ]]; then
+            zombie_invasion_status=`grep -c 'Error starting mission' ${zombie_invasion_log}`
+        fi
+
+        if [[ ${zombie_invasion_status} -eq 0 ]]; then
+            echo "Zombie invastion mission ended with success status."
+            echo " "
+            break
+        fi 
+
+        let try+=1 
+
+        if [[ $try -lt $num_tries ]]; then 
+            echo "Zombie invastion mission ended with failure status."
+            echo "Killing all Minecraft and Malmo processes that can be found and trying again."
+            ${TOMCAT}/tools/kill_minecraft.sh
+            ${TOMCAT}/tools/check_minecraft.sh
+        fi 
+    done
+fi
+
+echo "Finished running all sessions in ${TOMCAT}."
+echo " "
+
+
