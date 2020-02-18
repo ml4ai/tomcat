11a00161e31921d6c906ee5e5ed9248c80c072ee
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-02-18 11:35:06 -0700

    Removing stale files

--- src/pddl/test-shop.lisp
+++
@@ -1,7 +0,0 @@
-;; Invocation from command line using SBCL:
-;;     sbcl --load test-shop.lisp
-
-(ql:quickload "shop3")
-(in-package :shop-user)
-(load "sar-individual-domain.lisp")
-(load "sar-individual-problem.lisp")
