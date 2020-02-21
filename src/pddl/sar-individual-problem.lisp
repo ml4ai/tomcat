737a2b7d112d415e87fb47ad8921f5bb4220b397
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-02-21 10:53:59 -0700

    Removing stale pddl files

--- src/pddl/sar-individual-problem.lisp
+++
@@ -1,8 +0,0 @@
-;; Our Domain Definitions will use SHOP3 Syntax
-
-(defproblem sar-individual-problem sar-individual
-  ((:objects room1 room2 - room)
-   (:init victim1 victim2 - victim)
-   (:goal (forall (?r - room) (checked ?r)))
-  )
-)
