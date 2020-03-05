862482e63fe6ed0de9092531cca62436482b14c7
adi-ua <abanerjee@email.arizona.edu>
2020-03-04 22:46:41 -0700

    new button

---
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Utils/SmartStoneButton.java
@@ -0,0 +1,28 @@
+package edu.arizona.tomcat.Utils;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.BlockButton;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class SmartStoneButton extends SmartButton
+{
+    protected SmartStoneButton()
+    {
+        super(false);
+    }
+
+    protected void playClickSound(@Nullable EntityPlayer player, World worldIn, BlockPos pos)
+    {
+        worldIn.playSound(player, pos, SoundEvents.BLOCK_STONE_BUTTON_CLICK_ON, SoundCategory.BLOCKS, 0.3F, 0.6F);
+    }
+
+    protected void playReleaseSound(World worldIn, BlockPos pos)
+    {
+        worldIn.playSound((EntityPlayer)null, pos, SoundEvents.BLOCK_STONE_BUTTON_CLICK_OFF, SoundCategory.BLOCKS, 0.3F, 0.5F);
+    }
+}
