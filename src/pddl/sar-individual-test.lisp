1c209e6d6cf3a0038f8e81685f9cc8f2807201a5
Loren Champlin <lorenchamplin@SBS-7185.local>
2020-03-04 20:43:04 -0700

    Can move to the next room, check it, triage any victims, and then leaves the building

--- src/pddl/sar-individual-test.lisp
+++ src/pddl/sar-individual-test.lisp
@@ -19,32 +19,42 @@
 (defdomain (sar-individual-domain :type pddl-domain :redefine-ok T) (
     (:types human ;; Everything, including 'human' inherits from the base 'object' type
             victim rescuer - human ;; The rescuer and the victims are humans.
-            rooms ;; Rooms (includes elevators and bathrooms)
+            room ;; Rooms (includes elevators and bathrooms)
+            building
     )
    
-    (:predicates (in ?h - human ?r - rooms)
+    (:predicates (in ?h - human ?r - room)
+                 (inside ?h - human ?b - building)
                  (triaged ?v - victim)
-                 (checked ?r - rooms))
+                 (checked ?r - room))
 
     (:action check-room
-      :parameters (?t - rescuer ?r - rooms)
-      :precondition (and (not (checked ?r)) (in ?t ?r))
-      :effect (checked ?r))
+      :parameters (?t - rescuer ?r - room)
+      :precondition (and (in ?t ?r) (not (checked ?r)))
+      :effect (checked ?r)
+    )
 
     (:action move-to
-      :parameters (?t - rescuer ?source - rooms ?destination - rooms)
-      :precondition ((not (in ?t ?destination)) (in ?t ?source))
-      :effect ((in ?t ?destination) (not (in ?t ?source))))
+      :parameters (?t - rescuer ?source ?destination - room)
+      :precondition (and (in ?t ?source) (not (in ?t ?destination)))
+      :effect (and (in ?t ?destination) (not (in ?t ?source)))
+    )
 
     (:action triage
-      :parameters (?t - rescuer ?v - victim ?r - rooms)
-      :precondition (and (in ?v ?r) (not (triaged ?v)) (in ?t ?r)) 
+      :parameters (?t - rescuer ?v - victim ?r - room)
+      :precondition (and (same-room ?t ?v ?r) (not (triaged ?v))) 
       :effect (triaged ?v)
+    )
+
+    (:action leave-building
+     :parameters (?t - rescuer ?b - building)
+     :precondition (inside ?t ?b)
+     :effect ((not (inside ?t ?b)))
     )
 
     (:method (search_and_triage ?t) 
              room-not-checked
-             ((not (checked ?r))) 
+             (and (in ?t ?r) (not (checked ?r))) 
              (:ordered (:task :immediate !check-room ?t ?r) 
                        (:task search_and_triage ?t))
 
@@ -53,27 +63,34 @@
              (:ordered (:task :immediate !triage ?t ?v ?r)
                        (:task search_and_triage ?t))
 
+             all-clear
+             (and (num-of-rooms-checked ?c) (num-of-rooms ?n) (eval (eq '?n '?c)))
+             (:ordered (:task !leave-building ?t ?b))
+
              room-checked-all-triaged
-             ((checked ?r))
-             ((:task !move-to ?t ?r ?r2))
+             (and (checked ?r) (in ?t ?r))
+             (:ordered (:task :immediate !move-to ?t ?r ?r2)
+                       (:task search_and_triage ?t))
              )
 
     (:- (same-room ?t ?v ?r) (and (in ?t ?r) (in ?v ?r)))
     (:- (same ?x ?x) nil)
     (:- (different ?x ?y) ((not (same ?x ?y))))
-    ;(:- (all-rooms-checked) (setof ?rooms (rooms ?rooms) ?building))
+    (:- (num-of-rooms-checked ?c) ((setof ?rooms (checked ?rooms) ?ac) (assign ?c (length '?ac))))
+    (:- (num-of-rooms ?n) ((setof ?rooms (room ?rooms) ?b) (assign ?n (length '?b))))
   )
 )
 
 (defproblem sar-individual-problem
-            ((rooms r2) (rooms r1) (rescuer t1) (victim v1) (victim v2) (in t1 r1) (in v1 r1) (in v2 r1))
+            ((building b) (room r1) (room r2) (rescuer t1) (victim v1) (victim v2) (victim v3) 
+                          (victim v4) (inside t1 b) (in t1 r1) (in v1 r1) (in v2 r1) (in v3 r2) (in v4 r2))
             ((search_and_triage t1)))
 
 ;; Find plans and graph the first one.
 
 (let ((plan-trees (nth-value 2 
                              (find-plans 'sar-individual-problem
-                                         :which :all
+                                         :which :first
                                          :verbose :long-plans
                                          :plan-tree t))))
   (cl-dot:dot-graph (spg:graph-plan-tree (first plan-trees))
