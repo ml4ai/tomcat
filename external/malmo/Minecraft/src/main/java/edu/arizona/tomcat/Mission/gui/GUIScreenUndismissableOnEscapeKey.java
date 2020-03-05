2fb6565f589b825f57fd99a39ea73ce9bf6d5af6
Paulo Soares <paulosoares@dhcp-10-142-232-53.uawifi.arizona.edu>
2020-03-05 11:35:39 -0700

    Fixing issue with the mouse

--- external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Mission/gui/GUIScreenUndismissableOnEscapeKey.java
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Mission/gui/GUIScreenUndismissableOnEscapeKey.java
@@ -2,9 +2,17 @@ package edu.arizona.tomcat.Mission.gui;
 
 import java.io.IOException;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GUIScreenUndismissableOnEscapeKey extends GuiScreen {
+	
+	@Override
+	public void initGui() {
+		super.initGui();
+		// Show mouse cursor so that the player can click on a button
+		Minecraft.getMinecraft().mouseHelper.ungrabMouseCursor();
+	}
 	
 	@Override
 	protected void keyTyped(char typedChar, int keyCode) throws IOException {
