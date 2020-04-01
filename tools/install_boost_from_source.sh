a25a5ce6e487b763d1c44d921a5728059993299a
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-04-01 16:39:42 -0700

    Fixed install_boost_from_source.sh script

--- tools/install_boost_from_source.sh
+++ tools/install_boost_from_source.sh
@@ -3,7 +3,7 @@
 # Install Boost from source
 
 boost_minor_version=71
-curl -O\
+curl -LO\
   https://dl.bintray.com/boostorg/release/1."${boost_minor_version}".0/source/boost_1_"${boost_minor_version}"_0.tar.gz
 if [[ $? -ne 0 ]]; then exit 1; fi;
 tar xfz boost_1_"${boost_minor_version}"_0.tar.gz
