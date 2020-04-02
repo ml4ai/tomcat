71c79e6cdf0b99182ccb037996ae838decd4f406
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-04-01 17:42:34 -0700

    Renaming run_tutorial to run_tutorial.sh

--- tools/run_session.sh
+++ tools/run_session.sh
@@ -50,7 +50,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 if [[ ${do_tutorial} -eq 1 ]]; then 
-  "${TOMCAT}"/tools/run_tutorial
+  "${TOMCAT}"/tools/run_tutorial.sh
 fi
 
 /bin/rm -f "${TOMCAT}"/external/malmo/Minecraft/run/saves/discrete_events/discrete_events.json
