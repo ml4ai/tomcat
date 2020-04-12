b35b61adf1529491fd3a5aa21337fb897ca18b86
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-04-12 15:26:47 -0700

    Removing redundant src/ directory

--- external/malmo/Minecraft/src/src/main/java/edu/arizona/tomcat/Emotion/EmotionHandler.java
+++
@@ -1,7 +0,0 @@
-package edu.arizona.tomcat.Emotion;
-
-public class EmotionHandler {
-
-  public static enum Emotion { FEAR, CALMNESS }
-  ;
-}
--- external/malmo/Minecraft/src/src/main/java/edu/arizona/tomcat/Mission/MissionListener.java
+++
@@ -1,9 +0,0 @@
-package edu.arizona.tomcat.Mission;
-
-public interface MissionListener {
-
-  /**
-   * Notifies listeners when a mission ends
-   */
-  public void missionEnded(String exitCode);
-}
