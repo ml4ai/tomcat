293b9d7924b52460615727a93b5e0e1980591197
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-05-04 11:51:18 -0700

    Simplified dog bark event JSON representation

--- external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Events/DogBarkEvent.java
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Events/DogBarkEvent.java
@@ -11,9 +11,10 @@ public class DogBarkEvent extends Event {
     /** The parameters of the command that produces the dog bark chat on the
      * screen. We return the raw array of strings for now, postponing any JSON
      * processing. */
-    private String[] parameters;
+    private int numberOfBarks;
     
     public DogBarkEvent(CommandEvent event) {
-        this.parameters = event.getParameters();
+        String parameters = String.join(" ", event.getParameters());
+        this.numberOfBarks = parameters.contains("woof woof")?2:1;
     }
 }
