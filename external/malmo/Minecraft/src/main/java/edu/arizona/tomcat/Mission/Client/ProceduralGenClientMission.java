8f8c9bd6ecabe15b79e715bf816df1620383f268
adi-ua <abanerjee@email.arizona.edu>
2020-06-22 22:04:15 -0700

    * Added PROCEDURAL enum and switch case condition to Mission.java and MissionFactory.java * Added very basic classes ProceduralGenClientMission and ProceduralGenMission.java with no actual functionality. * runExperiment is still failing as of this commit. Needs fixing..

---
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Mission/Client/ProceduralGenClientMission.java
@@ -0,0 +1,19 @@
+package edu.arizona.tomcat.Mission.Client;
+
+import edu.arizona.tomcat.Messaging.TomcatMessaging;
+
+public class ProceduralGenClientMission extends ClientMission{
+
+    public ProceduralGenClientMission() {
+        super();
+    }
+
+    @Override
+    public void handleMessageFromServer(TomcatMessaging.TomcatMessage message) {
+        super.handleMessageFromServer(message);
+    }
+
+    @Override
+    public void cleanup() {
+    }
+}
---
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Mission/ProceduralGenMission.java
@@ -0,0 +1,68 @@
+package edu.arizona.tomcat.Mission;
+
+import com.microsoft.Malmo.Schemas.PosAndDirection;
+import edu.arizona.tomcat.Mission.Goal.MissionGoal;
+import edu.arizona.tomcat.Mission.gui.SelfReportContent;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.world.World;
+
+import java.math.BigDecimal;
+
+public class ProceduralGenMission extends Mission {
+
+    public ProceduralGenMission() {
+        super();
+        this.id = ID.TUTORIAL;
+    }
+
+    @Override
+    protected void createPhases() {
+    }
+
+    @Override
+    public void init(World world) {
+        super.init(world);
+    }
+
+
+    @Override
+    protected void updateScene(World world) {
+        this.initializer.init(world);
+    }
+
+    @Override
+    public void setTimeLimitInSeconds(long timeLimitInSeconds) {
+        this.timeLimitInSeconds = -1;
+    }
+
+    @Override
+    public PosAndDirection
+    getPlayersInitialPositionAndDirection(EntityPlayerMP player) {
+        PosAndDirection positionAndDirection = new PosAndDirection();
+        positionAndDirection.setX(new BigDecimal(-623));
+        positionAndDirection.setY(new BigDecimal(4));
+        positionAndDirection.setZ(new BigDecimal(1584));
+        return positionAndDirection;
+    }
+
+    @Override
+    protected boolean hasSelfReport() {
+        return false;
+    }
+
+    @Override
+    protected SelfReportContent getSelfReportContent(EntityPlayerMP player,
+                                                     World world) {
+        return null;
+    }
+
+    @Override
+    protected void beforePhaseTrasition() {
+
+    }
+
+    @Override
+    public void goalAchieved(World world, MissionGoal goal) {
+
+    }
+}
