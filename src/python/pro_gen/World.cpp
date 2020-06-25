5859735ff60dcd09e9cf9766d05f09a6c6b6da29
adi-ua <abanerjee@email.arizona.edu>
2020-06-25 14:15:59 -0700

    Added Word implementation

--- src/python/pro_gen/World.cpp
+++ src/python/pro_gen/World.cpp
@@ -1,1 +1,17 @@
+#pragma once
 #include "World.h"
+#include <iostream>
+
+World::World(){}
+
+void World::addAABB(AABB aabb){
+    (this -> aabbList).push_back(aabb);
+}
+
+void World::addBlock(Block block){
+    (this -> blockList).push_back(block);
+}
+
+void World::toJSON(string filename){
+    cout << "TODO: toJSON isn't imlemented yet." << endl;
+}
--- src/python/pro_gen/World.h
+++ src/python/pro_gen/World.h
@@ -1,4 +1,20 @@
+#pragma once
+#include "AABB.h"
+#include "Block.h"
 #include <nlohmann/json.hpp>
+using namespace std;
+
 class World{
+    private:
+        vector<AABB> aabbList;
+        vector<Block> blockList;
+        
+    
+    public:
+        void addAABB(AABB);
+        void addBlock(Block);
+        void toJSON(string);
+        World();
+        ~World();
 
 };
