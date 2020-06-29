7efa122dd712e47b50ad44578c0d2cf47dcf15b7
Loren Champlin <lorenchamplin@SBS-7185.local>
2020-06-28 20:13:20 -0700

    finished revisions of methods for now

--- src/pddl/sar-individual-domain.lisp
+++ src/pddl/sar-individual-domain.lisp
@@ -28,11 +28,11 @@
    
     (:predicates (in ?h - human ?r - room) ;; Indicates which room a human (rescuer, victim, etc) is inside
                  (inside ?h - rescuer ?b - building) ;; Indicates that the rescuer is in the building
-                 (rescued ?v - victim) ;; Indicates that a victim has been triaged
+                 (triaged ?v - victim) ;; Indicates that a victim has been triaged
                  (searched ?t - rescuer ?r - room) ;; Indicates that a rescuer has checked a room
                  (searching ?t - rescuer ?r - room)
                  (spotted ?t - rescuer ?v - victim ?r - room) ;; Indicates that a rescuer has spotted the victim in a specific room
-                 (rescuing ?t - rescuer ?v - victim) ;; Indicates that a rescuer is in the process of examining a victim
+                 (triaging ?t - rescuer ?v - victim) ;; Indicates that a rescuer is in the process of examining a victim
                  (severe-injuries ?v - victim)
                  (victims-cleared ?t - rescuer ?r - room)
     )
@@ -40,26 +40,39 @@
     (:action start-searching ;; Rescuer checks the room, any victims in the room are "spotted" 
       :parameters (?t - rescuer ?r - room)
       :precondition (and (in ?t ?r) (not (searched ?t ?r)))
-      :effect (searching ?t ?r) 
+      :effect (and (searching ?t ?r) (forall (eval (cl-user::check-for-victim)) (and (victim ?v) (spotted ?t ?v ?r)))) 
     )
 
-    (:action finish-searching
+    (:action continue-searching ;; Rescuer checks the room, any victims in the room are "spotted" 
+      :parameters (?t - rescuer ?r - room)
+      :precondition (and (in ?t ?r) (searching ?t ?r))
+      :effect (forall (eval (cl-user::check-for-victim)) (and (victim ?v) (spotted ?t ?v ?r))) 
+    )
+
+    (:action done-searching
       :parameters (?t - rescuer ?r - room)
       :precondition (and (in ?t ?r) (searching ?t ?r))
       :effect (and (searched ?t ?r) (not (searching ?t ?r)))
     )
     
-    (:action start-rescuing-victim ;;
+    (:action start-triaging-victim ;;
       :parameters (?t - rescuer ?v - victim)
-      :precondition (and (spotted ?t ?v ?r) (not (rescued ?v)) (not (rescuing ?t ?v)))
-      :effect (rescuing ?t ?v)
+      :precondition (and (spotted ?t ?v ?r) (not (triaged ?v)) (not (triaging ?t ?v)))
+      :effect (triaging ?t ?v)
     )
 
-    (:action finish-rescuing-victim ;; Rescuer finishs examining the current victim and moves to the next
+    (:action finish-triaging-victim ;; Rescuer finishs examining the current victim and moves to the next
       :parameters (?t - rescuer ?v - victim)
-      :precondition (and (spotted ?t ?v ?r) (rescuing ?t ?v))
-      :effect (and (rescued ?v) (not (rescuing ?t ?v)))
+      :precondition (and (spotted ?t ?v ?r) (triaging ?t ?v))
+      :effect (and (triaged ?v) (not (triaging ?t ?v)))
     )
+
+    (:action stop-triaging-victim ;; Rescuer finishs examining the current victim and moves to the next
+      :parameters (?t - rescuer ?v - victim)
+      :precondition (and (spotted ?t ?v ?r) (triaging ?t ?v))
+      :effect (not (triaging ?t ?v))
+    )
+
 
     (:action move ;; Rescuer moves to another room
       :parameters (?t - rescuer ?source ?destination - room)
@@ -68,16 +81,10 @@
       :cost (+ 1 (cl-user::shortest-path-cost cl-user::*s-paths* '?source '?destination))
     )
 
-    (:action leave-building ;; Rescuer leaves the building
-      :parameters (?t - rescuer ?b - building)
-      :precondition (inside ?t ?b)
-      :effect (not (inside ?t ?b))
-    )
-
-    (:action enter-building ;; Rescuer enters the building and is assumed to be in the given room
-      :parameters (?t - rescuer ?b - building ?r - room)
-      :precondition (not (inside ?t ?b))
-      :effect (and (inside ?t ?b) (in ?t ?r))
+    (:action leave ;; Rescuer leaves the building
+      :parameters (?t - rescuer ?r - room)
+      :precondition (in ?t ?r)
+      :effect (not (in ?t ?r))
     )
 
     (:operator (!!assert ?fact) 
@@ -97,15 +104,60 @@
 
     (:method (perform-next-mission-task ?t ?r) ;; Method for examining and triaging victims
              search-A ;; Search strategy where agent stops searching to treat any victims currently found
-             (and (strategy A) (not searched ?t ?r) 
-                  (or (not (spotted ?t ?v ?r)) (and (spotted ?t ?v ?r) (not (rescued ?v)))))
-             (:task search)
+             (and (not (triaging ?t ?_)) (strategy A) (not searched ?t ?r) 
+                  (or (not (spotted ?t ?v ?r)) (and (spotted ?t ?v ?r) (triaged ?v))))
+             (:task search ?t ?r)
 
              search-B ;; Search strategy where agent only stops to treat gold victims.
-             (and (strategy B) (not searched ?t ?r) 
-                  (or (not (spotted ?t ?v ?r)) (and (spotted ?t ?v ?r) (not (rescued ?v)))))
-             (:task search)
+             (and (not (triaging ?t ?_)) (strategy B) (not searched ?t ?r) 
+                  (or (not (spotted ?t ?v ?r)) (and (spotted ?t ?v ?r) 
+                                                    (or (triaged ?v) (not (severe-injuries ?v))))))
+             (:task search ?t ?r)
 
+             triage-A
+             (and (strategy A) (or (triaging ?t ?v) (and (spotted ?t ?v ?r) (not (triaged ?v)))))
+             (:task triage ?t ?v)
+
+             triage-B
+             (and (strategy B) (or (triaging ?t ?v) (and (spotted ?t ?v ?r) (not (triaged ?v)) (severe-injuries ?v))))
+             (:task triage ?t ?v)
+
+             move
+             (next-location ?r2)
+             (:task !move ?t ?r ?r2)
+
+             leave
+             ()
+             (:task !leave ?t ?r)
+    )
+
+    (:method (search ?t ?r)
+             start-searching
+             (and (not (searching ?t ?r)))
+             (:task !start-searching ?t ?r)
+
+             continue-searching
+             (and (searching ?t ?r) (eval (cl-user::still-searching)))
+             (:task !continue-searching ?t ?r)
+
+             finish-searching
+             (searching ?t ?r)
+             (:ordered (!continue-searching ?t ?r)
+                       (!done-searching ?t ?r))
+    )
+
+    (:method (triage ?t ?v)
+             start-triaging
+             (and (not (triaging ?t ?v)))
+             (:task !start-triaging-victim ?t ?v)
+
+             finish-triaging
+             (and (triaging ?t ?v) (eval (cl-user::triage-succesful)))
+             (:task !finish-triaging-victim ?t ?v)
+
+             stop-triaging
+             (triaging ?t ?v)
+             (:task !stop-triaging-victim ?t ?v)
 
     )
 
