19f5c2477962bcc33b9459820896980e11b8481b
adi-ua <abanerjee@email.arizona.edu>
2020-07-06 18:18:51 -0700

    Updating JSON representation with AABB block_list

--- src/cpp/pro_gen/World.cpp
+++ src/cpp/pro_gen/World.cpp
@@ -50,39 +50,51 @@ string World::toTSV() {
  * @return string The JSON as a string
  */
 string World::toJSON() {
-    vector<map<string, string>> aabb_map_list;
-    vector<map<string, string>> block_map_list;
+    vector<json> json_aabb_list;
+    vector<json> json_block_list;
 
     // Add AABBs to the JSON list
     for (auto aabb : this->aabbList) {
-        map<string, string> m;
-        m["id"] = to_string(aabb.getID());
+        json aabb_json;
+        aabb_json["id"] = to_string(aabb.getID());
 
-        m["x1"] = to_string(aabb.getTopLeft().getX());
-        m["y1"] = to_string(aabb.getTopLeft().getY());
-        m["z1"] = to_string(aabb.getTopLeft().getZ());
+        aabb_json["x1"] = to_string(aabb.getTopLeft().getX());
+        aabb_json["y1"] = to_string(aabb.getTopLeft().getY());
+        aabb_json["z1"] = to_string(aabb.getTopLeft().getZ());
 
-        m["x2"] = to_string(aabb.getBottomRight().getX());
-        m["y2"] = to_string(aabb.getBottomRight().getY());
-        m["z2"] = to_string(aabb.getBottomRight().getZ());
-        m["material"] = aabb.getMaterial();
-        aabb_map_list.push_back(m);
+        aabb_json["x2"] = to_string(aabb.getBottomRight().getX());
+        aabb_json["y2"] = to_string(aabb.getBottomRight().getY());
+        aabb_json["z2"] = to_string(aabb.getBottomRight().getZ());
+        aabb_json["material"] = aabb.getMaterial();
+
+        vector<json> aabb_block_list_json;
+        for (auto block : *(aabb.getBlockList())) {
+            json block_json;
+            block_json["type"] = block.getType();
+            block_json["x"] = to_string(block.getX());
+            block_json["y"] = to_string(block.getY());
+            block_json["z"] = to_string(block.getZ());
+            block_json["material"] = block.getMaterial();
+            aabb_block_list_json.push_back(block_json);
+        }
+        aabb_json["block_list"] = aabb_block_list_json;
+        json_aabb_list.push_back(aabb_json);
     }
 
     // Add Blocks to the JSON List
     for (auto block : this->blockList) {
-        map<string, string> m;
-        m["type"] = block.getType();
-        m["x"] = to_string(block.getX());
-        m["y"] = to_string(block.getY());
-        m["z"] = to_string(block.getZ());
-        m["material"] = block.getMaterial();
-        block_map_list.push_back(m);
+        json block_json;
+        block_json["type"] = block.getType();
+        block_json["x"] = to_string(block.getX());
+        block_json["y"] = to_string(block.getY());
+        block_json["z"] = to_string(block.getZ());
+        block_json["material"] = block.getMaterial();
+        json_block_list.push_back(block_json);
     }
 
     json j;
-    j["aabb_list"] = aabb_map_list;
-    j["block_list"] = block_map_list;
+    j["aabb_list"] = json_aabb_list;
+    j["block_list"] = json_block_list;
     return j.dump(4);
 }
 
