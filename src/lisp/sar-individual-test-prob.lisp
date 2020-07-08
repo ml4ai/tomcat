c1b9dbdbd0f263100d82b3e03961fffa314bca40
Loren Champlin <lorenchamplin@SBS-7185.local>
2020-07-08 10:31:25 -0700

    Can now generate full plans, but still need to implement plan switching

--- src/lisp/sar-individual-test-prob.lisp
+++ src/lisp/sar-individual-test-prob.lisp
@@ -39,7 +39,9 @@
                           (rescuer t1)  
                           (in t1 lobby)))
 
-(defvar *sar-task* '((perform-next-task-that-prioritizes-yellow-victims t1 lobby)))
+(defvar *task-type* 'perform-next-task-that-prioritizes-yellow-victims)
+
+(defvar *sar-task* (list (list *task-type* 't1 'lobby)))
 
 (defvar *mission-ongoing* t)
 
@@ -48,17 +50,17 @@
 (with-open-file 
   (outstream "sar-plan-trees.txt" :direction :output :if-exists :supersede) 
   (format outstream "~a~%" 
-          (loop while *mission-ongoing* collect 
-                (let ((current-plan (multiple-value-list 
+          (list (append '((complete-mission t1)) (loop while *mission-ongoing* collect 
+                (first (let ((current-plan (multiple-value-list 
                                       (find-plans 'sar-individual-problem :which :first :optimize-cost nil :verbose nil :plan-tree t)))) 
                   (setf *sar-state* (state-atoms (first (fourth current-plan)))) 
                   (if (eql '!move (first (first (first (first current-plan))))) 
-                    (setf *sar-task* (list (list 'perform-next-task-that-prioritizes-yellow-victims 
+                    (setf *sar-task* (list (list *task-type* 
                                                  (second (first (first (first current-plan)))) 
                                                  (fourth (first (first (first current-plan)))))))) 
                 (if (eql '!leave (first (first (first (first current-plan))))) 
                   (setf *mission-ongoing* nil))
                 (make-problem 'sar-individual-problem *sar-state* *sar-task*) 
-                (third current-plan)))))
+                (third current-plan))))))))
 
 ;;(print (eql '!move (first (first (first (first (multiple-value-list (find-plans 'sar-individual-problem :which :first :optimize-cost nil :verbose nil :plan-tree t))))))))
