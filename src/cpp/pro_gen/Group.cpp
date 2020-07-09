6f91c4690c02b581d917816689ea4b9a08d7fbf9
adi-ua <abanerjee@email.arizona.edu>
2020-07-09 11:53:41 -0700

    Setting up Group class for complex AABB combination

---
+++ src/cpp/pro_gen/Group.cpp
@@ -0,0 +1,3 @@
+#include "Group.h"
+
+Group::Group(int id): AABB(id,"group","air", new Pos(), new Pos(), true, false){}
---
+++ src/cpp/pro_gen/Group.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "AABB.h"
+
+class Group : public AABB{
+
+    private:
+        std::vector<AABB> aabbList;
+        void recalculateGroupBoundaries();
+
+    public:
+        void addAABB(AABB * aabb);
+        Group(int id);
+        
+
+
+};
