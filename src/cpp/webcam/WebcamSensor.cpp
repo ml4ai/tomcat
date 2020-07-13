4af145410189ab0e40c8a113bfb0b76cbef648c0
Shreeya Jain <shreeyaajain@gmail.com>
2020-07-13 21:53:53 +0530

    Renaming executable to facesensor

--- src/cpp/webcam/WebcamSensor.cpp
+++ src/cpp/webcam/WebcamSensor.cpp
@@ -17,44 +17,32 @@ namespace tomcat {
         if (!this->face_model.loaded_successfully) {
             throw runtime_error("Could not load the landmark detector.");
         }
-
-        if (!this->face_model.eye_model) {
+        
+		if (!this->face_model.eye_model) {
             throw runtime_error("No eye model found");
         }
 
         this->fps_tracker.AddFrame();
         this->sequence_reader.Open(arguments);
-        this->rgb_image = this->sequence_reader.GetNextFrame();
     };
 
     void WebcamSensor::get_observation() {
         using cv::Point3f;
         using cv::Vec6d;
+        using cv::Vec2d;
         using GazeAnalysis::EstimateGaze;
+        using GazeAnalysis::GetGazeAngle;
         using LandmarkDetector::Calculate3DEyeLandmarks;
         using LandmarkDetector::CalculateAllEyeLandmarks;
         using LandmarkDetector::DetectLandmarksInVideo;
         using LandmarkDetector::FaceModelParameters;
         using LandmarkDetector::GetPose;
-
-        // Reading the images
-        this->grayscale_image = this->sequence_reader.GetGrayFrame();
-
-        // The actual facial landmark detection / tracking
-        bool detection_success = DetectLandmarksInVideo(this->rgb_image,
-                                                        this->face_model,
-                                                        this->det_parameters,
-                                                        this->grayscale_image);
-
-        // Gaze tracking, absolute gaze direction
-        Point3f gazeDirection0(0, 0, -1);
-        Point3f gazeDirection1(0, 0, -1);
-
-        // Face analysis
+        
+        // Load facial feature extractor and AU analyser
         FaceAnalysis::FaceAnalyserParameters face_analysis_params(
             this->arguments);
         FaceAnalysis::FaceAnalyser face_analyser(face_analysis_params);
-
+        
         // Recorder open face parameters
         Utilities::RecorderOpenFaceParameters recording_params(
             this->arguments,
@@ -69,128 +57,161 @@ namespace tomcat {
         // Create open_face_rec
         Utilities::RecorderOpenFace open_face_rec(
             this->sequence_reader.name, recording_params, this->arguments);
+            
+            
+		this->rgb_image = this->sequence_reader.GetNextFrame(); 
+		
+		while (!this->rgb_image.empty()) {
+		            
+            // Converting to grayscale
+        	this->grayscale_image = this->sequence_reader.GetGrayFrame();
 
-        // If tracking succeeded and we have an eye model, estimate gaze
-        if (detection_success && this->face_model.eye_model) {
+         	// The actual facial landmark detection / tracking
+        	bool detection_success = DetectLandmarksInVideo(this->rgb_image,
+            	                                            this->face_model,
+            	                                            this->det_parameters,
+            	                                            this->grayscale_image);
 
-            EstimateGaze(this->face_model,
-                         gazeDirection0,
-                         this->sequence_reader.fx,
-                         this->sequence_reader.fy,
-                         this->sequence_reader.cx,
-                         this->sequence_reader.cy,
-                         true);
+        	// Gaze tracking, absolute gaze direction
+        	Point3f gazeDirection0(0, 0, 0);
+        	Point3f gazeDirection1(0, 0, 0);
+        	Vec2d gazeAngle(0, 0);
 
-            EstimateGaze(this->face_model,
-                         gazeDirection1,
-                         this->sequence_reader.fx,
-                         this->sequence_reader.fy,
-                         this->sequence_reader.cx,
-                         this->sequence_reader.cy,
-                         false);
-        }
+        
 
-        // Do face alignment
-        cv::Mat sim_warped_img;
-        cv::Mat_<double> hog_descriptor;
-        int num_hog_rows = 0, num_hog_cols = 0;
+        	// If tracking succeeded and we have an eye model, estimate gaze
+        	if (detection_success && this->face_model.eye_model) {
 
-        // Perform AU detection and HOG feature extraction, as this can be
-        // expensive only compute it if needed by output or visualization
-        if (recording_params.outputAlignedFaces() ||
+            	EstimateGaze(this->face_model,
+                	         gazeDirection0,
+                	         this->sequence_reader.fx,
+                	         this->sequence_reader.fy,
+                	         this->sequence_reader.cx,
+                	         this->sequence_reader.cy,
+                	         true);
+
+            	EstimateGaze(this->face_model,
+                	         gazeDirection1,
+                	         this->sequence_reader.fx,
+                	         this->sequence_reader.fy,
+                	         this->sequence_reader.cx,
+                	         this->sequence_reader.cy,
+                	         false);
+                	         
+               gazeAngle = GetGazeAngle(gazeDirection0, gazeDirection1);
+        	}
+        
+        
+	        // Do face alignment
+	        cv::Mat sim_warped_img;
+	        cv::Mat_<double> hog_descriptor;
+	        int num_hog_rows = 0, num_hog_cols = 0;
+
+        	// Perform AU detection and HOG feature extraction, as this can be
+        	// expensive only compute it if needed by output or visualization
+        	if (recording_params.outputAlignedFaces() ||
             recording_params.outputHOG() || recording_params.outputAUs() ||
             this->visualizer.vis_align || this->visualizer.vis_hog ||
             this->visualizer.vis_aus) {
-            face_analyser.AddNextFrame(this->rgb_image,
-                                       this->face_model.detected_landmarks,
-                                       this->face_model.detection_success,
-                                       this->sequence_reader.time_stamp,
-                                       this->sequence_reader.IsWebcam());
-            face_analyser.GetLatestAlignedFace(sim_warped_img);
-            face_analyser.GetLatestHOG(
-                hog_descriptor, num_hog_rows, num_hog_cols);
-        }
+            	face_analyser.AddNextFrame(this->rgb_image,
+                	                       this->face_model.detected_landmarks,
+                    	                   this->face_model.detection_success,
+                    	                   this->sequence_reader.time_stamp,
+                    	                   this->sequence_reader.IsWebcam());
+            	face_analyser.GetLatestAlignedFace(sim_warped_img);
+            	face_analyser.GetLatestHOG(
+                	hog_descriptor, num_hog_rows, num_hog_cols);
+        	}
 
-        // Work out the pose of the head from the tracked model
-        Vec6d pose_estimate = GetPose(this->face_model,
-                                      this->sequence_reader.fx,
-                                      this->sequence_reader.fy,
-                                      this->sequence_reader.cx,
-                                      this->sequence_reader.cy);
+        	// Work out the pose of the head from the tracked model
+        	Vec6d pose_estimate = GetPose(this->face_model,
+            	                          this->sequence_reader.fx,
+            	                          this->sequence_reader.fy,
+            	                          this->sequence_reader.cx,
+            	                          this->sequence_reader.cy);
 
-        // Keeping track of fps
-        this->fps_tracker.AddFrame();
+        	// Keeping track of fps
+        	this->fps_tracker.AddFrame();
         
-        // Displaying the tracking visualizations
-        this->visualizer.SetImage(this->rgb_image,
-                                  this->sequence_reader.fx,
-                                  this->sequence_reader.fy,
-                                  this->sequence_reader.cx,
-                                  this->sequence_reader.cy);
+        	// Displaying the tracking visualizations
+        	this->visualizer.SetImage(this->rgb_image,
+            	                      this->sequence_reader.fx,
+            	                      this->sequence_reader.fy,
+            	                      this->sequence_reader.cx,
+            	                      this->sequence_reader.cy);
 
         
-        this->visualizer.SetObservationFaceAlign(sim_warped_img);
-        this->visualizer.SetObservationHOG(
-            hog_descriptor, num_hog_rows, num_hog_cols);
+        	this->visualizer.SetObservationFaceAlign(sim_warped_img);
+        	this->visualizer.SetObservationHOG(
+            	hog_descriptor, num_hog_rows, num_hog_cols);
 
-        this->visualizer.SetObservationLandmarks(
-            this->face_model.detected_landmarks,
-            this->face_model.detection_certainty,
-            this->face_model.GetVisibilities());
+        	this->visualizer.SetObservationLandmarks(
+           		this->face_model.detected_landmarks,
+            	this->face_model.detection_certainty,
+            	this->face_model.GetVisibilities());
 
-        this->visualizer.SetObservationPose(
-            pose_estimate, this->face_model.detection_certainty);
+        	this->visualizer.SetObservationPose(
+            	pose_estimate, this->face_model.detection_certainty);
 
-        this->visualizer.SetObservationGaze(
-            gazeDirection0,
-            gazeDirection1,
-            CalculateAllEyeLandmarks(this->face_model),
-            Calculate3DEyeLandmarks(this->face_model,
-                                    this->sequence_reader.fx,
-                                    this->sequence_reader.fy,
-                                    this->sequence_reader.cx,
-                                    this->sequence_reader.cy),
-            this->face_model.detection_certainty);
+        	this->visualizer.SetObservationGaze(
+            	gazeDirection0,
+            	gazeDirection1,
+	            CalculateAllEyeLandmarks(this->face_model),
+    	        Calculate3DEyeLandmarks(this->face_model,
+    	                                this->sequence_reader.fx,
+    	                                this->sequence_reader.fy,
+    	                                this->sequence_reader.cx,
+    	                                this->sequence_reader.cy),
+    	        this->face_model.detection_certainty);
 
-        this->visualizer.SetObservationActionUnits(
-            face_analyser.GetCurrentAUsReg(),
-            face_analyser.GetCurrentAUsClass());
+    	    this->visualizer.SetObservationActionUnits(
+    	        face_analyser.GetCurrentAUsReg(),
+    	        face_analyser.GetCurrentAUsClass());
 
-        this->visualizer.SetFps(this->fps_tracker.GetFPS());
+    	    this->visualizer.SetFps(this->fps_tracker.GetFPS());
 
-        // Detect key press 
-        char character_press = this->visualizer.ShowObservation();
-        if (character_press == 'r') {
-            this->face_model.Reset();
-        }
+    	    // Detect key press 
+    	    char character_press = this->visualizer.ShowObservation();
+    	    if (character_press == 'r') {
+    	        this->face_model.Reset();
+    	    }
+    	    else if (character_press == 'q') {
+				break;
+			}
 
-        // Setting up the recorder output
-        open_face_rec.SetObservationHOG(
-            detection_success,
-            hog_descriptor,
-            num_hog_rows,
-            num_hog_cols,
-            31); // The number of channels in HOG is fixed at the moment, as
-                 // using FHOG
-        open_face_rec.SetObservationVisualization(
-            this->visualizer.GetVisImage());
-        open_face_rec.SetObservationActionUnits(
-            face_analyser.GetCurrentAUsReg(),
-            face_analyser.GetCurrentAUsClass());
-        open_face_rec.SetObservationTimestamp(
-        	this->sequence_reader.time_stamp);
+       		// Setting up the recorder output
+        	open_face_rec.SetObservationHOG(
+        	    detection_success,
+        	    hog_descriptor,
+        	    num_hog_rows,
+        	    num_hog_cols,
+        	    31); // The number of channels in HOG is fixed at the moment, as
+        	         // using FHOG
+        	open_face_rec.SetObservationVisualization(
+        	    this->visualizer.GetVisImage());
+        	open_face_rec.SetObservationActionUnits(
+        	    face_analyser.GetCurrentAUsReg(),
+        	    face_analyser.GetCurrentAUsClass());
+        	open_face_rec.SetObservationTimestamp(
+        		this->sequence_reader.time_stamp);
+        		
+        	json j;
+        	j = {
+        			{"timestamp", open_face_rec.get_timestamp()},
+        			{"au_intensities", open_face_rec.get_au_intensities()},
+           		 	{"au_occurences", open_face_rec.get_au_occurences()}
+        		};
+        	std::cout << j.dump(4) << std::endl;
+        		
+        	// Grabbing the next frame in the sequence
+        	this->rgb_image = this->sequence_reader.GetNextFrame();
+        	
+		}
 
-        json j;
-        j = {
-        		{"timestamp", open_face_rec.get_timestamp()},
-        		{"au_intensities", open_face_rec.get_au_intensities()},
-             	{"au_occurences", open_face_rec.get_au_occurences()}
-        	};
-        std::cout << j.dump(4) << std::endl;
-        
-        // Grabbing the next frame in the sequence
-        this->rgb_image = this->sequence_reader.GetNextFrame();
-        
+          
+        open_face_rec.Close();
+        this->sequence_reader.Close();
+
         // Reset the models for the next video
         face_analyser.Reset();
         face_model.Reset();
