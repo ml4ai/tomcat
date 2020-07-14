8db537177345c537545c9d35e0b86c4c0991c7f9
Loren Champlin <lorenchamplin@SBS-7185.local>
2020-07-14 16:47:30 -0700

    The domain now estimates time and can tell if all the rooms have been searched. If starting with the yellow first strategy, it knows to indicate a strategy change after 9 minutes or when all rooms are first searched.

--- src/lisp/sar-individual-domain.lisp
+++ src/lisp/sar-individual-domain.lisp
@@ -13,11 +13,18 @@
 
 (progn (ql:quickload "shop3")
        (load "util.lisp")
-       (defvar shop-user::qroom (create-room-queue "sar-room-list.txt")))
+       (defparameter *roomlist* (load-object-from-file "sar-room-list.txt"))
+       (defvar *s-paths* (load-json-database "sar_shortest_path_lengths.json"))
+       (defparameter *time-passed* 0)
+       (defvar *number-of-rooms* (length *roomlist*)))
 
 (in-package :shop-user)
 
 ;;(shop-trace :methods)
+
+(defun plan-found-hook (state which plan cost depth)
+                 (setf cl-user::*time-passed* (+ cl-user::*time-passed* cost)))
+
 (defdomain (sar-individual-domain :type pddl-domain :redefine-ok T) (
     (:types human ;; Everything, including 'human' inherits from the base 'object' type
             victim rescuer - human ;; The rescuer and the victims are humans.
@@ -34,42 +41,49 @@
                  (triaging ?t - rescuer ?v - victim) ;; Indicates that a rescuer is in the process of examining a victim
                  (severe-injuries ?v - victim)
                  (victims-cleared ?t - rescuer ?r - room)
+                 (marked-for-revisit ?r - room)
     )
 
     (:action start-searching ;; Rescuer checks the room, any victims in the room are "spotted" 
       :parameters (?t - rescuer ?r - room)
       :precondition (and (in ?t ?r) (not (searched ?t ?r))) 
       :effect (and (searching ?t ?r)) 
+      :cost (+ 5 (random 10))
     )
 
     (:action continue-searching ;; Rescuer checks the room, any victims in the room are "spotted" 
       :parameters (?t - rescuer ?r - room)
       :precondition (and (in ?t ?r) (searching ?t ?r) (not (searched ?t ?r))) 
       :effect ()
+      :cost (+ 5 (random 10))
     )
 
     (:action done-searching
       :parameters (?t - rescuer ?r - room)
       :precondition (and (in ?t ?r) (searching ?t ?r))
       :effect (and (searched ?t ?r) (not (searching ?t ?r)))
+      :cost 0
     )
     
     (:action start-triaging-victim ;;
       :parameters (?t - rescuer ?v - victim)
       :precondition (and (spotted ?t ?v ?r) (not (triaged ?v)) (not (triaging ?t ?v)))
       :effect (triaging ?t ?v)
+      :cost 0
     )
 
     (:action finish-triaging-victim ;; Rescuer finishs examining the current victim and moves to the next
       :parameters (?t - rescuer ?v - victim)
       :precondition (and (spotted ?t ?v ?r) (triaging ?t ?v))
       :effect (and (triaged ?v) (not (triaging ?t ?v)))
+      :cost (cl-user::triage-cost '?v t)
     )
 
     (:action stop-triaging-victim ;; Rescuer finishs examining the current victim and moves to the next
       :parameters (?t - rescuer ?v - victim)
       :precondition (and (spotted ?t ?v ?r) (triaging ?t ?v))
       :effect (not (triaging ?t ?v))
+      :cost (cl-user::triage-cost '?v nil)
     )
 
 
@@ -77,12 +91,28 @@
       :parameters (?t - rescuer ?source ?destination - room)
       :precondition (and (in ?t ?source) (not (in ?t ?destination)))
       :effect (and (in ?t ?destination) (not (in ?t ?source)))
+      :cost (ceiling (/ (+ 1 (cl-user::shortest-path-cost cl-user::*s-paths* '?source '?destination)) 5.612))
     )
 
-    (:action leave ;; Rescuer leaves the building
+    (:action end-mission ;; Rescuer leaves the building
       :parameters (?t - rescuer ?r - room)
       :precondition (in ?t ?r)
       :effect (not (in ?t ?r))
+      :cost 0
+    )
+
+    (:operator (!!mark-for-revisit ?r)
+               (())
+               (())
+               ((marked-for-revisit ?r))
+               0
+    )
+
+    (:operator (!change-strategy)
+               (())
+               (())
+               ((strategy-changed))
+               0
     )
 
     (:operator (!!assert ?fact) 
@@ -101,43 +131,69 @@
 
     (:method (perform-next-task-without-victim-priority ?t ?r) ;; Method for examining and triaging victims
              search ;; Search strategy where agent stops searching to treat any victims currently found
-             (and (not (triaging ?t ?_)) (not (searched ?t ?r)) 
+             (and (eval (< cl-user::*time-passed* 900)) (not (triaging ?t ?_)) (not (searched ?t ?r)) 
                   (or (not (spotted ?t ?v ?r)) (and (spotted ?t ?v ?r) (triaged ?v))))
              (:task search ?t ?r)
 
              triage
-             (or (triaging ?t ?v) (and (spotted ?t ?v ?r) (not (triaged ?v))))
+             (and (eval (< cl-user::*time-passed* 900)) 
+                  (or (triaging ?t ?v) 
+                      (and (spotted ?t ?v ?r) (not (triaged ?v)) 
+                           (not (and (severe-injuries ?v) (eval (>= cl-user::*time-passed* 540)))))))
              (:task triage ?t ?v)
 
              move
-             (and (assign ?room (queues::qpop qroom)) (room ?r2) (different ?r ?r2) (eval (cl-user::symbol-equals-keyword '?room '?r2))) 
+             (and (eval (< cl-user::*time-passed* 900)) (assign ?room (cl-user::get-next-room '?r cl-user::*roomlist*)) 
+                  (room ?r2) (different ?r ?r2) (eval (cl-user::symbol-equals-keyword '?room '?r2))
+                  (eval (cl-user::remove-from-room-list '?r "sar-room-list.txt"))
+                  (eval (setf cl-user::*roomlist* (cl-user::load-object-from-file "sar-room-list.txt"))))
              (:task !move ?t ?r ?r2)
 
-             leave
+             end-mission
              ()
-             (:task !leave ?t ?r)
+             (:task !end-mission ?t ?r)
     )
 
 
 
     (:method (perform-next-task-that-prioritizes-yellow-victims ?t ?r) ;; Method for examining and triaging victims
+             change-strategy
+             (and (eval (< cl-user::*time-passed* 900)) (not (strategy-changed)) 
+                  (or (all-rooms-searched) (eval (> cl-user::*time-passed* 525))))
+             (:task !change-strategy)
+
              search ;; Search strategy where agent only stops to treat gold victims.
-             (and (not (triaging ?t ?_)) (not (searched ?t ?r)) 
+             (and (eval (< cl-user::*time-passed* 900)) (not (triaging ?t ?_)) (not (searched ?t ?r)) 
                   (or (not (spotted ?t ?v ?r)) (and (spotted ?t ?v ?r) 
                                                     (or (triaged ?v) (not (severe-injuries ?v))))))
              (:task search ?t ?r)
 
              triage
-             (or (triaging ?t ?v) (and (spotted ?t ?v ?r) (not (triaged ?v)) (severe-injuries ?v)))
+             (and (eval (< cl-user::*time-passed* 900)) 
+                  (or (triaging ?t ?v) 
+                      (and (spotted ?t ?v ?r) (not (triaged ?v)) (severe-injuries ?v) (eval (< cl-user::*time-passed* 540)))))
              (:task triage ?t ?v)
 
+             move-and-mark-for-revisit
+             (and (eval (< cl-user::*time-passed* 900)) (assign ?room (cl-user::get-next-room '?r cl-user::*roomlist*)) 
+                  (room ?r2) (different ?r ?r2) (eval (cl-user::symbol-equals-keyword '?room '?r2))
+                  (spotted ?t ?v ?r) (not (severe-injuries ?v))
+                  (eval (cl-user::remove-from-room-list '?r "sar-room-list.txt"))
+                  (eval (cl-user::add-to-room-list '?r "sar-room-list.txt"))
+                  (eval (setf cl-user::*roomlist* (cl-user::load-object-from-file "sar-room-list.txt"))))
+             ((:task !move ?t ?r ?r2)
+              (:task !!mark-for-revisit ?r))
+
              move
-             (and (assign ?room (queues::qpop qroom)) (room ?r2) (different ?r ?r2) (eval (cl-user::symbol-equals-keyword '?room '?r2)))
+             (and (eval (< cl-user::*time-passed* 900)) (assign ?room (cl-user::get-next-room '?r cl-user::*roomlist*)) 
+                  (room ?r2) (different ?r ?r2) (eval (cl-user::symbol-equals-keyword '?room '?r2))
+                  (eval (cl-user::remove-from-room-list '?r "sar-room-list.txt"))
+                  (eval (setf cl-user::*roomlist* (cl-user::load-object-from-file "sar-room-list.txt"))))
              (:task !move ?t ?r ?r2)
 
-             leave
+             end-mission
              ()
-             (:task !leave ?t ?r)
+             (:task !end-mission ?t ?r)
     )
 
     (:method (search ?t ?r)
@@ -164,7 +220,7 @@
              (:task !start-triaging-victim ?t ?v)
 
              finish-triaging
-             (and (triaging ?t ?v) (eval (cl-user::triage-successful .9)))
+             (and (triaging ?t ?v) (eval (cl-user::triage-successful .9)) (not (and (severe-injuries ?v) (eval (>= cl-user::*time-passed* 540)))))
              (:task !finish-triaging-victim ?t ?v)
 
              stop-triaging
@@ -197,5 +253,6 @@
     (:- (same ?x ?x) nil) ;; Helper axiom for "different" axoim
     (:- (different ?x ?y) ((not (same ?x ?y)))) ;; Axiom that determines that two variables contain different values. 
     (:- (how-many-found ?t ?r ?c) (and (setof ?v (spotted ?t ?r ?v) ?vs) (assign ?c (eval (length '?vs))))) 
+    (:- (all-rooms-searched) (and (setof ?r (searched ?t ?r) ?rs) (eval (= (length '?rs) cl-user::*number-of-rooms*))))
   )
 )
