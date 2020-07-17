f5b35772d933f36fe6e292b88457c31c3b748b27
adi-ua <abanerjee@email.arizona.edu>
2020-07-16 22:34:32 -0700

    Updated program options

--- src/cpp/pro_gen/generator.cpp
+++ src/cpp/pro_gen/generator.cpp
@@ -7,45 +7,7 @@
 using namespace std;
 namespace po = boost::program_options;
 
-/**
- * @brief Directive method to create the world and write the JSON and TSV
- * output to file.
- */
-int main(int argc, char* argv[]) {
-
-    string jsonPath, tsvPath;
-
-    // Handle options
-    po::options_description desc("Allowed options");
-    desc.add_options()("help,h", "produce help message")(
-        "json_path",
-        po::value<string>()->default_value("procedural.json"),
-        "Specify where to save the JSON file with filename and extension.")(
-        "tsv_path",
-        po::value<string>()->default_value("procedural.tsv"),
-        "Specify where to save the TSV file with filename an extension.");
-
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        cout << desc << endl;
-        return 0;
-    }
-
-    if (vm.count("json_path")) {
-        jsonPath = vm["json_path"].as<string>();
-    }
-
-    if (vm.count("tsv_path")) {
-        tsvPath = vm["tsv_path"].as<string>();
-    }
-
-    // Process input and generate output
-    cout << "Generating zombieworld..." << endl;
-    ZombieWorldGenerator zombiegen(12319);
-    World& world = zombiegen.getWorld();
+void writeToFile(string jsonPath, string tsvPath, World& world) {
     cout << "Writing to file..." << endl;
 
     // Write JSON
@@ -57,10 +19,99 @@ int main(int argc, char* argv[]) {
     ofstream outputTSV(tsvPath, ios::out);
     outputTSV << world.toTSV();
     outputTSV.close();
+}
 
-    cout << "Done. The generated files are in Minecraft/run/procedural.json "
-            "and Minecraft/run/procedural.tsv"
-         << endl;
+/**
+ * @brief Directive method to create the world and write the JSON and TSV
+ * output to file.
+ */
+int main(int argc, char* argv[]) {
 
+    int choice, seed;
+    string jsonPath, tsvPath;
+
+    // Handle options
+    po::options_description general("Allowed options");
+    general.add_options()("help,h", "Show program options")(
+        "gen_type",
+        po::value<int>()->default_value(0),
+        "Choose the type of world to generate.\nZombieworld = 0\nGridworld = "
+        "1")(
+        "help_zombieworld",
+        "Show required and optional arguments when generating a Zombieworld.")(
+        "help_gridworld",
+        "Show required and optional arguments when generating a Gridworld.")(
+        "json_path",
+        po::value<string>()->default_value("procedural.json"),
+        "Specify where to save the JSON file with filename and extension.")(
+        "tsv_path",
+        po::value<string>()->default_value("procedural.tsv"),
+        "Specify where to save the TSV file with filename and extension.");
+
+    po::options_description zombieWorldOptions(
+        "Allowed options for the Zombieworld generator");
+    zombieWorldOptions.add_options()("seed",
+                                     po::value<int>()->default_value(12319),
+                                     "The seed used to initialize the random "
+                                     "object used within the generator.");
+
+    po::options_description gridWorldOptions(
+        "Allowed options for the Gridworld generator");
+    gridWorldOptions.add_options()("N",
+                                   po::value<int>()->default_value(3),
+                                   "Number of AABB per axis of the gridworld.")(
+        "sep",
+        po::value<int>()->default_value(0),
+        "The separation between AABB in the cardinal directions.")(
+        "AABB_size",
+        po::value<int>()->default_value(10),
+        "The size of the cubic AABB used.")(
+        "seed",
+        po::value<int>()->default_value(12319),
+        "The seed used to initialize the random "
+        "object used within the generator.");
+
+    po::options_description all("Allowed options");
+    all.add(general).add(zombieWorldOptions).add(gridWorldOptions);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(all).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        cout << general << endl;
+        return 0;
+    }
+    if (vm.count("help_zombieworld")) {
+        cout << zombieWorldOptions << endl;
+        return 0;
+    }
+    if (vm.count("help_gridworld")) {
+        cout << gridWorldOptions << endl;
+        return 0;
+    }
+
+    choice = vm["gen_type"].as<int>();
+    seed = vm["seed"].as<int>();
+    jsonPath = vm["json_path"].as<string>();
+    tsvPath = vm["tsv_path"].as<string>();
+
+    cout << "Generating world..." << endl;
+    if (choice == 0) {
+        ZombieWorldGenerator zombieGen(seed);
+        World& world = zombieGen.getWorld();
+        writeToFile(jsonPath, tsvPath, world);
+    }
+    else if (choice == 1) {
+        int N = vm["N"].as<int>();
+        int sep = vm["sep"].as<int>();
+        int AABB_size = vm["AABB_size"].as<int>();
+        GridworldGenerator gridGen(N, sep, AABB_size, seed);
+        World& world = gridGen.getWorld();
+        writeToFile(jsonPath, tsvPath, world);
+    }
+
+    cout << "Done. The generated files are in " << jsonPath << " and "
+         << tsvPath << endl;
     return 0;
 }
