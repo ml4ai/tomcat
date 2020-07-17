a87c3356bde8595121658cc527d065b0569fc25c
Adarsh Pyarelal <adarsh@email.arizona.edu>
2020-07-17 06:27:40 -0700

    Adding C++ executable to get spec version from spec.yml

--- src/cpp/getSpecVersion/getSpecVersion.cpp
+++ src/cpp/getSpecVersion/getSpecVersion.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    YAML::Node node = YAML::LoadFile("docs/spec.yml");
+    YAML::Node node = YAML::LoadFile(argv[1]);
     std::cout << node["info"]["version"] << std::endl;
     return 0;
 }
