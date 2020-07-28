cd068090324acbcb4994c2723d50aa25e46a07b6
adi-ua <abanerjee@email.arizona.edu>
2020-07-27 23:29:03 -0700

    Adding weapon and armor tracking and level number

--- src/cpp/pro_gen/Entity.cpp
+++ src/cpp/pro_gen/Entity.cpp
@@ -1,20 +1,48 @@
 #include "Entity.h"
+#include <iostream>
 using namespace std;
 using json = nlohmann::json;
 
-Entity::Entity(string type, Pos& pos) : type{type}, pos{pos} {}
-
-int Entity::getX(){
-    return this->pos.getX();
+Entity::Entity(string type,
+               Pos& pos,
+               int helmet,
+               int chestplate,
+               int leggings,
+               int boots,
+               int weapon)
+    : type{type}, pos{pos} {
+    this->equipment.push_back(helmet);
+    this->equipment.push_back(chestplate);
+    this->equipment.push_back(leggings);
+    this->equipment.push_back(boots);
+    this->equipment.push_back(weapon);
 }
 
-int Entity::getY(){
-    return this->pos.getY();
-}
+int Entity::getX() { return this->pos.getX(); }
 
-int Entity::getZ(){
-    return this->pos.getZ();
-}
+int Entity::getY() { return this->pos.getY(); }
+
+int Entity::getZ() { return this->pos.getZ(); }
+
+int Entity::getHelmet() { return this->equipment.at(0); }
+
+int Entity::getChestplate() { return this->equipment.at(1); }
+
+int Entity::getLeggings() { return this->equipment.at(2); }
+
+int Entity::getBoots() { return this->equipment.at(3); }
+
+int Entity::getWeapon() { return this->equipment.at(4); }
+
+void Entity::setHelmet(int helmet) { this->equipment[0] = helmet; }
+
+void Entity::setChestplate(int chestplate) { this->equipment[1] = chestplate; }
+
+void Entity::setLeggings(int leggings) { this->equipment[2] = leggings; }
+
+void Entity::setBoots(int boots) { this->equipment[3] = boots; }
+
+void Entity::setWeapon(int weapon) { this->equipment[4] = weapon; }
 
 void Entity::setX(int x) { this->pos.setX(x); }
 
@@ -26,17 +54,27 @@ string Entity::getType() { return this->type; }
 
 void Entity::setType(string type) { this->type = type; }
 
-json Entity::toJSON(){
+json Entity::toJSON() {
     json entity_json;
     entity_json["type"] = this->getType();
     entity_json["x"] = to_string(this->getX());
     entity_json["y"] = to_string(this->getY());
     entity_json["z"] = to_string(this->getZ());
+    entity_json["helmet"] = to_string(this->getHelmet());
+    entity_json["chestplate"] = to_string(this->getChestplate());
+    entity_json["leggings"] = to_string(this->getLeggings());
+    entity_json["boots"] = to_string(this->getBoots());
+    entity_json["weapon"] = to_string(this->getWeapon());
     return entity_json;
 }
 
 string Entity::toTSV() {
-    string retval = (this->pos).toTSV() + "\t" + (this->type);
+    string retval =
+        (this->pos).toTSV() + "\t" + "entity" + "\t" + (this->type) + "\t";
+
+    for (auto& equipment : this->equipment) {
+        retval += to_string(equipment) + "\t";
+    }
     return retval;
 }
 
--- src/cpp/pro_gen/Entity.h
+++ src/cpp/pro_gen/Entity.h
@@ -6,6 +6,7 @@ class Entity {
 
   private:
     std::string type;
+    std::vector<int> equipment;
     Pos pos;
 
   public:
@@ -13,12 +14,28 @@ class Entity {
     int getX();
     int getY();
     int getZ();
+    int getHelmet();
+    int getChestplate();
+    int getLeggings();
+    int getBoots();
+    int getWeapon();
+    void setHelmet(int helmet);
+    void setChestplate(int chestplate);
+    void setLeggings(int leggings);
+    void setBoots(int boots);
+    void setWeapon(int weapon);
     void setX(int x);
     void setY(int y);
     void setZ(int z);
     void setType(std::string type);
     nlohmann::json virtual toJSON();
     std::string virtual toTSV();
-    Entity(std::string type, Pos& pos);
+    Entity(std::string type,
+           Pos& pos,
+           int helmet = 0,
+           int chestplate = 0,
+           int leggings = 0,
+           int boots = 0,
+           int weapon = 0);
     virtual ~Entity();
 };
