f076bbb255bf192336130df3bb05bf895ba7f09d
Liam Wilson <lswilson0709@gmail.com>
2020-08-16 11:59:54 -0700

    Command developer mode (#191)
    Fully implemented command event handling, namely including a devmode json file to whitelist commands outside of developer mode or enable developer mode if all commands are permissible. Publishes command name, argument, and issuer to event bus.

---
+++ external/malmo/Minecraft/src/main/java/edu/arizona/tomcat/Events/CommandExecuted.java
@@ -0,0 +1,21 @@
+package edu.arizona.tomcat.Events;
+
+import net.minecraftforge.event.CommandEvent;
+
+public class CommandExecuted extends Event {
+
+    /**
+     * Constructor grabs the command name, the issuer of the command, and the
+     * parameters for the command.
+     */
+    private String commandName = "";
+    private String[] parameters = null;
+    private String sender = "";
+
+    public CommandExecuted(CommandEvent event) {
+        this.commandName = event.getCommand().getName();
+        this.parameters = event.getParameters();
+        this.sender = event.getSender().getName();
+    }
+
+}
