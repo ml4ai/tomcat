e406bcf76bf8cba63ef58806ddf016e2b3e3594c
Loren Champlin <lorenchamplin@SBS-7185.local>
2020-09-09 16:12:09 -0700

    Plan recognition algorithm file

---
+++ src/lisp/plan_recognition_toy/plan_recognition.lisp
@@ -0,0 +1,8 @@
+(ql:quickload "shop3")
+(load "simple-schedule-domain.lisp")
+
+(compute-posterior-prob (cats action state domain prior)
+                        )
+
+(compute-likelihood-prior (c action state domain prior)
+                          )
