25a1a0415d12a4d49e8d08228cc2d4efb53190a8
Salena Ashton <salena.ashton@gmail.com>
2020-10-06 12:29:30 -0700

    added domain where anything is mine-able without need for pick axe. Fixed typo in high-task-CraftAxe-domain

--- src/lisp/craftAxe/mineAnything-craftIronAxe.lisp
+++ src/lisp/craftAxe/mineAnything-craftIronAxe.lisp
@@ -1,23 +1,22 @@
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; high-task-CraftAxe-domain.lisp
+;; mineAnythig-craftIronAxe.lisp
 ;; Salena Torres Ashton     
-;; Created 5 October 2020
-;; ToMCAT Planning
+;; Created 6 October 2020
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 #|
  This domain crafts wooden, stone and iron axes. 
- The domain does not require mining any wood or stone, but requires mining 
-    iron ore and fuel. Because you cannot mine wood, planks and sticks are
-    assumed to be in agent's inventory and do not deplete.
+ The domain allows wood, stone, iron ore and fuel to be mined without the
+     need for any special pickaxe, significantly reducing lower-level tasks.
 
-Problem must be initialized for agent: (has-obj sticks planks stone)
-   These items are mentioned because agent still needs to place them on
-   the crafting table.
-
-Tools and furnace used are not depleted.
+oes not currently increase inventory numerically after 
+    mining or crafting. It does not decrease inventory after crafting.
+    the domain assumes that mining or crafting of item completely fills
+    and depletes inventory of said item's ingredients.
+ Tools used are not used in this domain-- they are simply crafted.
 |#
 
-(define (domain high-task-CraftAxe-domain) 
+(define (domain mineAnything-craftIronAxe)
   (:requirements :strips :typing)
   (:types
     object fuel tool blade 
@@ -26,11 +25,12 @@ Tools and furnace used are not depleted.
   (:predicates 
     ;; keep highest type level for future refactoring?
     (has-obj ?o - object)        ;; ingredients, supplies, mined ore
-    (has-ingot ?o - object)      ;; to craft blade. 
+    (has-ingot ?o - object)      ;; to craft blade. Must be diff than handle
     (has-fuel ?f - fuel)         ;; ingredients, items or fuel used to burn
     (has-tool? ?t - tool)        ;; crafted tool, furnace
     (has-blade ?b - blade)       ;; need to craft tool
     (choose-blade ?b - blade)    ;; higher-level choice
+    (placed-sticks ?o - object)  ;; to craft handle
     (placed-blade ?b - blade)    ;; to craft blade
     (placed-fuel ?fuel - fuel)   ;; to prepare for smelting
     (placed-ore ?ore - obj)      ;; to prepae for smelting
@@ -38,7 +38,14 @@ Tools and furnace used are not depleted.
     (smelt-ore ?o -object ?f - fuel) ;; to smelt ores
     ) ;; end predicates
 
-;; fuel and iron ore are the only materials you need to mine
+
+  ;; Most-primitive actions
+  (:action mine-wood
+          :parameters (?wood - object)
+          :precondition ()  ;; nothing needed to mine wood
+          :effects (and (has-obj ?wood))
+          )
+
   (:action mine-fuel
           ;; fuel can be coal, kelp, bamboo, any wood or wooden item
           :parameters (?fuel - object)
@@ -46,41 +53,63 @@ Tools and furnace used are not depleted.
           :effects (and (has-obj ?fuel))
           )
 
+  ;; Less-primitive actions
+  (:action craft-planks ;; for now, makes planks and wooden blades
+          :parameters (?wood ?planks - object ?wooden - blade )
+          :precondition (and (has-obj ?wood))
+          :effect (and (has-obj ?planks)
+                       (has-blade ?wooden)
+                       (not (has-obj ?wood)))
+          )
+
+  (:action craft-sticks ;; depletes planks for object, not for blades
+          :parameters (?planks ?sticks - object)
+          :precondition (and (has-obj ?planks))
+          :effect (and (has-obj ?sticks)
+                       (not (has-obj ?planks)))
+          )
+
+  (:action mine-stone
+          :parameters (?stone - object)
+          :precondition () 
+          :effect (and (has-obj ?stone))
+          )
+
   (:action mine-iron-ore
-          :parameters (?ore - object ?pickaxe - tool)
-          :precondition (and (has-tool ?stone-pickaxe))
+          :parameters (?ore - object)
+          :precondition ()
           :effect (and (has-obj ?iron-ore))
           )
 
-  ;; Does not specify spatial placing of ingredients. This is just me
-  ;;    working through the logic of having and using items.
+
+  ;; Higher Actions
   (:action place-sticks
           :parameters (?sticks - object)
           :precondition (and (has-obj ?sticks))
-          :effect (and (placed-sticks ?sticks))
+          :effect (and (placed-sticks ?sticks)
+                       (not (has-obj ?sticks)))
           )
 
-;; Investigate: I originally had these next two actions, then commented them
-    ;; out so it would be less had-coded. Now I'm wondering once again.
   (:action place-planks
           :parameters (wooden - blade)
           :precondition (and (has-blade wooden))
-          :effect (and (placed-blade wooden))
+          :effect (and (placed-blade wooden)
+                       (not (has-blade wooden)))
           )
 
-  ;; In this domain, stone is simply called 'stone' since there is 
-      ;; no need to mine stone. Other domains call this 'm-stone'.
-  (:action place-stone 
-          :parameters (stone - blade)
-          :precondition (and (has-blade stone))
-          :effect (and (placed-blade stone))
+  (:action place-m-stone
+          :parameters (m-stone - blade)
+          :precondition (and (has-blade m-stone))
+          :effect (and (placed-blade m-stone)
+                       (not (has-blade m-stone)))
           )
 
-  ;; use for wooden, stone or iron. I might delete this.
+
   (:action place-blade
           :parameters (?b - blade)
           :precondition (and (has-blade ?b))
-          :effect (and (placed-blade ?b))
+          :effect (and (placed-blade ?b
+                       (not (has-blade ?b)))
           )
 
   ;; to prepare for smelting ore and many other uses
@@ -88,15 +117,20 @@ Tools and furnace used are not depleted.
            :parameters (?fuel - fuel ?furnace - tool)
            :precondition (and (has-fuel ?fuel)
                               (has-furnace ?furnace))
-           :effect (and (placed-fuel ?fuel))
+           :effect (and (placed-fuel ?fuel)
+                       (not (has-fuel ?fuel)))
+           ;; does not deplete furnace
            )
+
 
   ;; to prepare for smelting ore
   (:action place-iron-ore
            :parameters (?ore - object ?furnace - tool)
            :precondition (and (has-ore ?ore)
                               (has-furnace ?furnace))
-           :effect (and (placed-ore ?ore))
+           :effect (and (placed-ore ?ore)
+                        (not (has-ore ?ore)))
+           ;; does not deplete furnace
            )
 
   ;; smelt ore in order to craft ingots
@@ -104,15 +138,20 @@ Tools and furnace used are not depleted.
            :parameters (?fuel ?ore ?o - object)
            :precondition (and (has-ore ?ore)
                               (has-fuel ?fuel))
-           :effect (and (has-ingot ?o))
+           :effect (and (has-ingot ?o)
+                        (not (has-ore ?ore)
+                             (has-fuel ?fuel)))
+           ;; does not deplete furnace
            )
 
   ;; to craft an iron tool or weapon
   (:action place-ingot
            :parameters (?ingot - object)
            :precondition (and (has-ingot ?ingot))
-           :effect (and (placed-ingot ?ingot))
+           :effect (and (placed-ingot ?ingot)
+                        (not (has-ingot ?ingot)))
            )
+
 
   ;; Methods
   ;; to craft a wooden or stone axe
@@ -127,25 +166,23 @@ Tools and furnace used are not depleted.
            (and (has-tool ?axe))
   );;end craft-axe method
 
-  ;; Needed to mine iron ore. Since stone is in inventory, there
-      ;; no need to craft a wooden pick axe.
-  (:method (craft-stone-pickaxe ?sticks ?b ?tool)
-           placed-sticks
-           (and (placed-sticks ?sticks))
-
-           placed-blade
-           (and (placed-blade ?b))
-
-           has-tool
-           (and (has-tool ?stone-pickaxe))
-  );;end craft-stone-pickaxe method
 
   (:method (craft-iron-axe ?sticks ?o ?iron-axe)
-;; To "fail faster", do I need to explicitly state?:
+;; Salena's to-do: 
+     ;; lift constraints to help decision of higher level task
+     ;; continue working on LISP syntax
+     ;; syntax for this particular problem
+;; The first subtask, placed-ingot, will prevent this method from taking effect
+;;      unless (placed-ingot ?obj) is true.
+;; You cannot create an iron axe without an ingot, which you cannot smelt
+;;      having a stone pickaxe.
+
+;; Do I need to explicitly state?:
     (:preconditions (and (placed-ingot ?obj)
                        (placed-sticks ?sticks)
                   )
         )
+
            placed-ingot
            (and (placed-ingot ?o))
 
@@ -155,5 +192,9 @@ Tools and furnace used are not depleted.
            has-tool
            (and (has-tool ?iron-axe))
   );; end craft-iron-axe method
+
+
 );; end domain
+
+
 
