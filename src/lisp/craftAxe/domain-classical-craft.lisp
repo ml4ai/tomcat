e4e4f60cb6647a0aaf911c07614ae0e3b25dba1c
SalenaAshton <salena@email.arizona.edu>
2020-10-19 23:36:14 -0700

    Typos corrected. Bow deleted for simplicity, for now.

--- src/lisp/craftAxe/domain-classical-craft.lisp
+++ src/lisp/craftAxe/domain-classical-craft.lisp
@@ -1,22 +1,14 @@
 
 #| Differences:
 
-This domain enables the crafting of an axe with a blade made from wood,
-   stone or iron. This domain has the greatest amount of low-level tasks
-   that respect the physics of the Minecraft domain. Additional files will
-   introduce heuristics or otherwise relaxed problems. 
+This domain enables the crafting of a wood, stone, or iron axe, hoe, or pickaxe 
+    using boolean-valued actions.
 
-This domain does not currently deplete inventory when ingredients or tools
-  are used. That happens in numerical-planning domain.
+This domain does not track inventory when ingredients or tools are used. Further
+    development of craftAxe will be in numeric and other planning domains.
 
  to do: Syntax for SHOP3
-        preferences and weapons?
-        Added constraints
 
-For now, the crafting of a bow will need to be enough. When I get this
-  working, the arrow can be developed. Easier to assume a bow comes with arrow
-  for two reasons: It's the only item in this domain that requires three
-  different ingredients and the only item to require feathers.|#
 
 (progn (ql:quickload "shop3"))
 
@@ -24,7 +16,7 @@ For now, the crafting of a bow will need to be enough. When I get this
 
 (defdomain (domain-classical-craft :type pddl-domain :redefine-okay T) ()
   (:types (ingredients tool weapon - object)
-          (wood stone ore ingot flint strings cobwebs - ingredients)
+          (wood stone ore ingot flint - ingredients)
             (planks sticks - wood)
 
           (wood-axe stone-axe iron-axe
@@ -33,9 +25,7 @@ For now, the crafting of a bow will need to be enough. When I get this
               flintSteel furnace - tool)
 
           ;; flintSteal mispelled this way is used only as a weapon
-          ;; for now, crafting a bow yields a bow and arrow.
-          (wood-sword stone-sword iron-sword
-                bow flintSteal - weapon))
+          (wood-sword stone-sword iron-sword flintSteal - weapon))
   
 
   (:predicates (has-wood ?w 
@@ -65,7 +55,7 @@ For now, the crafting of a bow will need to be enough. When I get this
                 has-iron-sword ?isw
                 has-flint-steal ?w - weapon))
 
- ;;; Mining Actions
+;;; Mining Actions
   (:action mine-wood
     :parameters (has-wood ?w - ingredients)
     :precondition ()   
@@ -123,7 +113,7 @@ For now, the crafting of a bow will need to be enough. When I get this
     :parameters (has-furnace ?furnace - tool
                  has-stone ?s - ingredients)
     :precondition (has-stone ?s)
-    :effect (has-furnace ?furnace)
+    :effect (has-furnace ?furnace))
 
   ;; craft ingots from iron ore
   (:action craft-ingot
@@ -133,18 +123,17 @@ For now, the crafting of a bow will need to be enough. When I get this
     :precondition (and (has-furnace ?furnace)
                        (has-fuel ?f)
                        (has-ore ?o))
-    :effect (has-ingot ?i
-    )
+    :effect (has-ingot ?i))
 
 
- ;;; Crafting Axes
+;;; Crafting Axes
   (:action craft-wood-axe
     :parameters (has-sticks ?st 
                  has-planks ?p - wood 
                  has-wood-axe ?wa - tool)
     :precondition (and (has-sticks ?st)
                        (has-planks ?p))
-    :effect (has-wood-axe ?wa)))
+    :effect (has-wood-axe ?wa))
 
 
   (:action craft-stone-axe
@@ -153,7 +142,7 @@ For now, the crafting of a bow will need to be enough. When I get this
                  has-stone-axe ?sa - tool)
     :precondition (and (has-sticks ?st)
                        (has-stone ?s))
-    :effect (has-stone-axe ?sa)))
+    :effect (has-stone-axe ?sa))
 
 
   (:action craft-iron--axe
@@ -162,11 +151,11 @@ For now, the crafting of a bow will need to be enough. When I get this
                  has-iron-axe ?ia - tool)
     :precondition (and (has-sticks ?st)
                        (has-ingot ?i))
-    :effect (has-iron-axe ?ia)))
+    :effect (has-iron-axe ?ia))
 
 
 
- ;;; Crafting Pickaxes
+;;; Crafting Pickaxes
   (:action craft-wood-pickaxe
     :parameters (has-sticks ?st 
                  has-planks ?p - wood
@@ -195,7 +184,7 @@ For now, the crafting of a bow will need to be enough. When I get this
   
   
   
-  ;;; Crafting hoes 
+;;; Crafting hoes 
   (:action craft-wood-hoe
     :parameters (has-sticks ?st
                  has-planks ?p - wood
@@ -248,19 +237,19 @@ For now, the crafting of a bow will need to be enough. When I get this
     :effect (has-iron-sword ?isw)))  
   
 
-  ;;; Craft flint and steel. This action is repeated twice because flint and
+;;; Craft flint and steel. This action is repeated twice because flint and
      ; steel can be used for a tool or weapon. 
      ; As a tool, I spell it flintSteel.
      ; As a weapon, I spell it flintSteal. 
      ; ??? Should I make this distinction or let the agent do so?  
+
   (:action craft-flint-steel
     :parameters (has-flint-steel ?flsteel - tool
                  has-flint ?fl
                  has-ingot ?i - ingredient)
     :precondition(and (has-flint ?fl)
                       (has-ingot ?i))
-    :effect (has-flint-steel ?flsteel)
-    )
+    :effect (has-flint-steel ?flsteel))
 
   (:action craft-flint-steal
     :parameters (has-flint-steal ?flsteal - weapon 
@@ -268,7 +257,6 @@ For now, the crafting of a bow will need to be enough. When I get this
                  has-ingot ?i - ingredient)
     :precondition(and (has-flint ?fl)
                       (has-ingot ?i))
-    :effect (has-flint-steal ?flsteal)
-    )
+    :effect (has-flint-steal ?flsteal))
 
-  ); end defdomain
+); end defdomain
