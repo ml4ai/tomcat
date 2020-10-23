25ba1c0307bcf51e0f1a65bec5d46c0aa9f191eb
Salena Ashton <salena.ashton@gmail.com>
2020-10-23 15:29:25 -0700

    Code runs and generates plans. Plans are valid, depending on how the problem is initiated.

--- src/lisp/craftAxe/mine-wood.lisp
+++ src/lisp/craftAxe/mine-wood.lisp
@@ -5,19 +5,24 @@
    ; No arithmetic in shop3, increment like in knights-tour example (n1 n2 n3)
    ; Numeric fluents
    ; Updated 23 Oct 2020
+;;; The code runs the plan now, but...
+    ; I need to finish some sort of action in the beginning so that
+       ; mine-wood is not skipped.
+
 
 (progn (ql:quickload "shop3"))
 
 (in-package :shop-user)
 (shop-trace :tasks :states :plans)
-(defdomain (mine-wood :type pddl-domain :redefine-ok T) (
+(defdomain (mine-wood.lisp :type pddl-domain :redefine-ok T) (
     (:types
       tool ingredients - object
-      wood planks sticks - ingredients
+      wood planks sticks begin - ingredients
       wood-axe - tool
     ); end of types
 
     (:predicates
+      (to-begin ?b - ingredients)
       (has-wood ?w - ingredients)
       (has-planks ?p - ingredients)
       (has-sticks ?s - ingredients)
@@ -25,10 +30,15 @@
 ;      (has-wood-pickaxe ?wpx - tool)
     ); end predicates
 
+;    (:action begin-mission
+;      :parameters (?b - ingredients)
+;      :precondition (to-begin ?b)
+;      :effect ()
+;      );end action begin-mission
 
     (:action mine-wood
-      :parameters (?w - ingredients)
-      :precondition ()
+      :parameters (?b ?w - ingredients)
+      :precondition (to-begin ?b)
       :effect (has-wood ?w)
     ); end action mine-wood
 
@@ -42,16 +52,21 @@
       :parameters (?p ?s - ingredients)
       :precondition (has-planks ?p)
       :effect (has-sticks ?s)
-      ); end craft-sticks
+    ); end craft-sticks
 
     (:action craft-wood-axe
       :parameters (?p ?s - ingredients ?wa - tool)
       :precondition (and (has-sticks ?s)
                          (has-planks ?p))
       :effect (has-wood-axe ?wa)
-      );end craft-wood-axe
+    );end craft-wood-axe
+
 
     (:method (craft-wood-axe ?wa)
+ ;      begin-mission
+ ;      (not (to-begin ?b))
+ ;      (:ordered (:task !to-begin ?b)
+ ;                (:task craft-axe ?wa))      
        mine-wood
        (not (has-wood ?w))
        (:ordered (:task !mine-wood ?w)
@@ -63,7 +78,7 @@
                  (:task craft-wood-axe ?wa))
 
        craft-sticks
-       (and (has-planks ?p) (not (has-sticks?s)))
+       (and (has-planks ?p) (not (has-sticks ?s)))
        (:ordered (:task !craft-sticks ?p ?s)
                  (:task craft-wood-axe ?wa))
 
@@ -71,13 +86,20 @@
        ()
        ()
     );end method craft-axe
+  )   
 ); end defdomain
 
 
 (defproblem craft-wood-axe-problem
-            ((wood w) (planks p) (sticks s) (wood-axe wa) 
-             (has-wood hw) (has-planks hp) (has-sticks hs) (has-wood-axe hwa))
-            ((craft-wood-axe wa)))
+          ;  ((begin b)
+          ;   (to-begin b)) 
+          ;   (has-wood w) (has-planks p) (has-sticks s) (has-wood-axe wa))
+          ((wood w)
+           (has-wood w)) 
+          ((craft-wood-axe wa)))
 
 (find-plans 'craft-wood-axe-problem :which :all :verbose :plans :plan-tree t)
+
+
+
 
