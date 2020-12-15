b2a8639440b5f11b759fb0cc5a3d8f821f533819
Loren Champlin <lorenchamplin@SBS-7185.local>
2020-12-14 18:30:28 -0700

    Made some revisions to the plan generation algorithm

--- src/python/PSDG_Domain.py
+++ src/python/PSDG_Domain.py
@@ -36,6 +36,7 @@ class PSDG_Domain:
     def initialize_planning(self, i_state):
         self.i_state = i_state
         self.c_state = copy.deepcopy(i_state)
+        self.states = []
         prods = []
         for i in self.methods:
             subtasks = []
@@ -54,69 +55,31 @@ class PSDG_Domain:
             )
         self.psdg = PSDG(Nonterminal("P"), prods)
 
-    def _sample_from_grammar(self, grammar, actions, start=None, depth=None):
-        """
-        This is based off of the generate function in nltk for CFG. This uses ancestral
-        sampling to generate one possible plan/sentence rather than all possible
-        plans/sentences.
-        see http://www.nltk.org/_modules/nltk/parse/generate.html#generate
-
-        :param grammar: The Grammar used to generate sentences.
-        :param start: The Nonterminal from which to start generate sentences.
-        :param depth: The maximal depth of the generated tree.
-        :param n: The maximum number of sentences to return.
-        :return: An iterator of lists of terminal tokens.
-        """
-        if not start:
-            start = grammar.start()
+    def _sample_plan(self,symbol=None,depth=None):
         if depth is None:
             depth = sys.maxsize
 
-        self.states = [copy.deepcopy(self.c_state)]
-
-        iter = self._sample_all(grammar, actions, [start], depth)
-
-        return iter
-
-    def _sample_all(self, grammar, actions, items, depth):
-        if items:
-            try:
-                for frag1 in self._sample_one(
-                    grammar, actions, items[0], depth
-                ):
-                    for frag2 in self._sample_all(
-                        grammar, actions, items[1:], depth
-                    ):
-                        yield frag1 + frag2
-            except RuntimeError as _error:
-                if _error.message == "maximum recursion depth exceeded":
-                    # Helpful error message while still showing the recursion stack.
-                    raise RuntimeError(
-                        "The grammar has rule(s) that yield infinite recursion!!"
-                    )
-                else:
-                    raise
-        else:
-            yield []
-
-    def _sample_one(self, grammar, actions, item, depth):
         if depth > 0:
-            if isinstance(item, Nonterminal):
-                prod = random.choices(
-                    [x for x in grammar.productions(lhs=item)],
-                    [x.prob() for x in grammar.productions(lhs=item)],
-                )[0]
-                for frag in self._sample_all(
-                    grammar, actions, prod.rhs(), depth - 1
-                ):
-                    yield frag
-            else:
-                for i in actions:
-                    if i.__name__ == item[1:]:
-                        self.c_state = i(self.c_state)
-                grammar.update_prods(self.c_state)
-                self.states.append(copy.deepcopy(self.c_state))
-                yield [item]
+            if symbol is None:
+                symbol = self.psdg.start()
+            plan = []
+            prod = random.choices(
+                [x for x in self.psdg.productions(lhs=symbol)],
+                [x.prob() for x in self.psdg.productions(lhs=symbol)],
+            )[0]
+
+            for i in prod.rhs():
+                if isinstance(i, Nonterminal):
+                    plan = plan + self._sample_plan(i,depth-1)
+                else:
+                    for j in self.actions:
+                        if j.__name__ == i[1:]:
+                            self.c_state = j(self.c_state)
+                    self.psdg.update_prods(self.c_state)
+                    self.states.append(copy.deepcopy(self.c_state))
+                    plan.append(i)
+            return plan
+        return []
 
     def sample_plans(
         self, n_samples=1, output_options=None, start=None, depth=None
@@ -126,13 +89,8 @@ class PSDG_Domain:
         if start is not None:
             start = Nonterminal(start)
         for i in range(n_samples):
-            plans.append(
-                list(
-                    self._sample_from_grammar(
-                        self.psdg, self.actions, start, depth
-                    )
-                )[0]
-            )
+            self.states = [self.i_state]
+            plans.append(self._sample_plan(start,depth))
             state_seqs.append(self.states)
             self.c_state = copy.deepcopy(self.i_state)
             self.psdg.update_prods(self.c_state)
@@ -151,3 +109,4 @@ class PSDG_Domain:
             trace[1::2] = plans[i]
             plan_traces.append(trace)
         return plan_traces
+
