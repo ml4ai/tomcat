3f5f02060e1b6b51a44f423befcba8f67dbe01af
adi-ua <abanerjee@email.arizona.edu>
2021-04-04 13:42:51 -0700

    More program options (including patch colors which is still WIP)

--- src/cpp/mcglib/mcglib_visualizer.py
+++ src/cpp/mcglib/mcglib_visualizer.py
@@ -10,6 +10,7 @@ Purpose: This program will vizualize the procedurally generated map from the sem
 
 import matplotlib.patches as patches
 from matplotlib.collections import PatchCollection
+from matplotlib.colors import is_color_like
 import matplotlib.pyplot as plt
 import numpy as np
 import json
@@ -22,12 +23,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--color_patches",
     help="Color the plotted patches (for supported materials). Defaults to False.",
-    default=False,
-    type=bool,
+    default="f",
+    type=str,
 )
 parser.add_argument(
     "--font_size", help="Size of label fonts. Defaults to 3.", default=3, type=int
 )
+
+parser.add_argument(
+    "--font_color",
+    help="Color of label fonts. Defaults to blue.",
+    default="blue",
+    type=str,
+)
+
 parser.add_argument(
     "--rankdir",
     help='Sets direction of graph layout. If rankdir="TB", the graph is laid out from top to bottom, i.e., directed edges tend to go from top to bottom. By default, graphs are laid out from left to right ("LR").',
@@ -53,6 +62,7 @@ semantic_json = "./semantic_map.json"
 with open(semantic_json) as f:
     data = json.load(f)
 locations = data["locations"]
+locations.reverse()
 
 
 # Visualization Logic
@@ -82,11 +92,22 @@ for location in locations:
         height = abs(z2 - z1)
 
         # Make and add the patch to our list
-        if(args.color_patches):
-            patch_color = color_index.get(location["material"], False)
-            rect = patches.Rectangle(
-                (x1, z1), width, height, linewidth=1, edgecolor="black", fill=patch_color
-            )
+        if args.color_patches[0].lower() == "t":
+            patch_color = color_index.get(location["material"], None)
+
+            if location["material"] == "blank":
+                rect = patches.Rectangle(
+                    (x1, z1), width, height, linewidth=1, edgecolor="black", fill=False
+                )
+            else:
+                rect = patches.Rectangle(
+                    (x1, z1),
+                    width,
+                    height,
+                    linewidth=1,
+                    edgecolor="black",
+                    facecolor=patch_color,
+                )
         else:
             rect = patches.Rectangle(
                 (x1, z1), width, height, linewidth=1, edgecolor="black", fill=False
@@ -96,15 +117,27 @@ for location in locations:
 
         # Annotate the patch with the name of the location
         patch_center_z = z1 + height / 2
-        ax.annotate(
-            location["id"],
-            (x2, patch_center_z),
-            color="blue",
-            weight="bold",
-            fontsize=args.font_size,
-            ha="center",
-            va="center",
-        )
+        if is_color_like(args.font_color):
+            ax.annotate(
+                location["id"],
+                (x2, patch_center_z),
+                color=args.font_color,
+                weight="bold",
+                fontsize=args.font_size,
+                ha="center",
+                va="center",
+            )
+        else:
+            ax.annotate(
+                location["id"],
+                (x2, patch_center_z),
+                color="blue",
+                weight="bold",
+                fontsize=args.font_size,
+                ha="center",
+                va="center",
+            )
+
 
 # Use patch collection to add the patches to plot
 p = PatchCollection(patch_list, match_original=True)
