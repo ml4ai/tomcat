79c02f3804b100ec2e88ee2e06c63bd7aa837dac
Megh <meghavarshini@gmail.com>
2021-07-12 13:02:53 -0700

    changes from Friday

--- exe/entrainment_webapp/app.py
+++ exe/entrainment_webapp/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, Aditya!</p>"
+    return render_template("index.html" )
