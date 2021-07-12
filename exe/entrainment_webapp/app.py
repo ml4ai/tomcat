b977054b4400a1c5d5e55612fa6c868ddbc8fe03
Aditya Jadhav <adityaj2003@Adityas-MacBook-Air.local>
2021-07-12 23:59:05 +0530

    Update app.py

--- exe/entrainment_webapp/app.py
+++ exe/entrainment_webapp/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+import os
+from flask import Flask,render_template,Response,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, Aditya!</p>"
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
