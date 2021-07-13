69132952a00b67dc958e783651b39d5d57b984aa
Aditya Jadhav <adityaj2003@Adityas-MacBook-Air.local>
2021-07-13 17:28:27 +0530

    With Auto Download and User ID

--- exe/entrainment_webapp/app.py
+++ exe/entrainment_webapp/app.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask,render_template,Response,request
+TEMPLATE_DIR = os.path.abspath('../templates')
+STATIC_DIR = os.path.abspath('../static')
 
-app = Flask(__name__)
+# app = Flask(__name__) # to make the app run without any
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run()
