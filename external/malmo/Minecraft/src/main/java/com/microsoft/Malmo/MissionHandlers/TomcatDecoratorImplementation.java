ac178308fdebae21d345d8148d5ed21b4e5a6dd8
shambhavi12001 <57306180+shambhavi12001@users.noreply.github.com>
2021-07-22 13:09:18 -0700

    Update TomcatDecoratorImplementation.java

--- external/malmo/Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/TomcatDecoratorImplementation.java
+++ external/malmo/Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/TomcatDecoratorImplementation.java
@@ -43,7 +43,8 @@ public class TomcatDecoratorImplementation
             this.decorator.getMission().intValue(),
             this.decorator.getTimeLimitInSeconds().intValue(),
             this.decorator.getSelfReportPromptTimeInSeconds().intValue(),
-            this.decorator.getLevelOfDifficulty().intValue());
+            this.decorator.getLevelOfDifficulty().intValue()),
+            this.decorator.getNumberOfPlayers().intValue());
 
     }
 
