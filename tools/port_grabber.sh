d0f479e51fef76a8c880c3d39fc3255d170ecc20
Shambhavi Singh <shambhavisingh@Shambhavis-MacBook-Pro.local>
2021-10-12 22:27:52 -0700

    error code

--- tools/port_grabber.sh
+++ tools/port_grabber.sh
@@ -12,7 +12,10 @@ done
 
 # Once it exists, grab the port when it is printed to the file
 echo 'Log file found. Monitoring for port...';
-
+if [ ! -f $log_file ]; then
+    echo "heyyyyyy--------------------> exiting because there is no log file to grab a port from.";
+    exit 0;
+fi
 # Grab the tail -f output of the log file and pipe it into
 # the subshell generated
 tail -f $log_file |
@@ -24,7 +27,7 @@ tail -f $log_file |
       # only the port id. Print the output to the variable
       # only when the pattern is matched
       lan_port=$(sed -n -e 's/^.*\[Server\ thread\/INFO\]:\ Started\ on\ //p' <<< "$cur_line");
-
+      echo "$cur_line"
       # If the port id non-empty, then we are done.
       # So, message is published to the topic lan_port_id and exit
       if [ -n "$lan_port" ]; then
@@ -34,7 +37,8 @@ tail -f $log_file |
 
       # Exit if there is no file at any point
       if [ ! -f $log_file ]; then
-        echo "Exiting because there is no log file to grab a port from.";
+        echo "Exiting because there is no log file to grab a port from"
+
         exit 0;
       fi
 
