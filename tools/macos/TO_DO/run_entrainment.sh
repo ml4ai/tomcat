18c43a139fe4547d4d46fcb74e2a9848d0b2dde0
Vincent Raymond <vraymond6@arizona.edu>
2021-11-09 14:45:09 -0500

    Initial commit for applescript_testing

---
+++ tools/macos/TO_DO/run_entrainment.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+echo $TOMCAT_DIRECTORY
+#pushd $TOMCAT_DIRECTORY
+#	pushd exe/entrainment_webapp/webmic
+#		python webmic.py &
+#	popd
+#	pushd exe/entrainment_webapp
+#		python websocket.py &
+#	popd
+#popd
