40a2ed33a0b890c12d27f338e36a7b76f6f0efd8
MykeDuong <hongminh4402@gmail.com>
2022-05-19 14:24:15 -0700

    Add an app checking for input devices

---
+++ exe/audioInputCheck/build/CMakeFiles/audioInputCheck.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audioInputCheck.dir/main.cpp.o"
+  "CMakeFiles/audioInputCheck.dir/main.cpp.o.d"
+  "audioInputCheck"
+  "audioInputCheck.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audioInputCheck.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
