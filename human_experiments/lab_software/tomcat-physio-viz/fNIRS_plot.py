aca9d135ed6694249508ce74deba0561d05b2a30
CalebUAz <calebshibu@email.arizona.edu>
2022-05-19 16:10:25 -0700

    new updates to fNIRS viz

--- human_experiments/lab_software/tomcat-physio-viz/fNIRS_plot.py
+++ human_experiments/lab_software/tomcat-physio-viz/fNIRS_plot.py
@@ -14,32 +14,45 @@ inlet = StreamInlet(streams[0])
 
 
 plt.ion()
+
 buffer = np.empty([1,80])
 
-fig, ax = plt.subplots(40)
+fig, ax = plt.subplots(20)
 
 fig.set_size_inches(18.5, 10)
+fig.suptitle('fNIRS Lion', fontsize=16)
 fig.set_dpi(100)
 
-while True:
-    
-   # if buffer.shape[0] >= 100:
-    #    print('Clearning buffer')
-     #   fig.clf()
-      #  buffer = np.empty([1,80])
+channel_list = ['S1-D1', 'S1-D2', 'S2-D1', 'S2-D3', 'S3-D1', 'S3-D3', 'S3-D4', 'S4-D2', 'S4-D4', 
+                'S4-D5', 'S5-D3', 'S5-D4', 'S5-D6', 'S6-D4', 'S6-D6', 'S6-D7', 'S7-D5', 'S7-D7',
+                'S8-D6', 'S8-D7']
 
+while True:
+    sample,time = inlet.pull_sample()
+    buffer = np.append(buffer, np.asarray([sample[1:]]), axis = 0)
+   
+    if buffer.shape[0] >= 300:
+        print('Clearning buffer')
+        buffer = np.empty([1,80])
         
-    for i in range(1, 40):
-        sample,time = inlet.pull_sample()
-        buffer = np.append(buffer, np.asarray([sample[1:]]), axis = 0)
+        for i in range(0, 20):
+            ax[i].cla()
+        
+    for i in range(0, 20):
         ax[i].cla()
-        ax[i].plot(buffer[:,i])
-        ax[i].axes.xaxis.set_visible(False)
-        ax[i].axes.yaxis.set_visible(False)
+        ax[i].plot(buffer[:,i+40], linewidth='0.6')
+        ax[i].plot(buffer[:,i+60], linewidth='0.6')
+        ax[i].set_ylim([-50, 50])
+        ax[i].set_ylabel(channel_list[i], fontsize=10, rotation=0, labelpad=20)
+        ax[i].set_xticks([])
+        ax[i].set_yticks([])
+        #ax[i].axis("off")
+        
+        #ax[i].axes.xaxis.set_visible(False)
+        #ax[i].axes.yaxis.set_visible(False)
         
     fig.canvas.draw()
     fig.canvas.flush_events()
     plt.pause(0.0001)
     clear_output(wait = True)
-    
     display(fig)
