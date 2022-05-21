fbd7a2c0c41777ac6392979cd13eec64ec27a2fa
MykeDuong <hongminh4402@gmail.com>
2022-05-20 17:10:12 -0700

    Change input device to 1 for Pi

---
+++ exe/audioStreamer/build/CMakeFiles/audioStreamer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audioStreamer.dir/audioStreamer.cpp.o"
+  "CMakeFiles/audioStreamer.dir/audioStreamer.cpp.o.d"
+  "audioStreamer"
+  "audioStreamer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audioStreamer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
