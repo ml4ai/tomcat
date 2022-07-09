b9fecbebe1f84b3c1596e5a63dbf1b943ac6d797
Eric Duong <eduong@email.arizona.edu>
2022-07-09 16:51:32 -0700

    check compliance room escape request

--- scripts/evaluation/study3/identify_compliance_instances.py
+++ scripts/evaluation/study3/identify_compliance_instances.py
@@ -32,9 +32,7 @@ class Intervention:
 
 
 class HelpRequestCritcalVictimIntervention(Intervention):
-    def __init__(self,
-                 timestamp: datetime,
-                 for_player: str) -> None:
+    def __init__(self, timestamp: datetime, for_player: str) -> None:
         super().__init__(timestamp, for_player)
         self.type = InterventionType.UTTERANCE
         self.description = "help_request_for_critical_victim"
@@ -42,6 +40,21 @@ class HelpRequestCritcalVictimIntervention(Intervention):
             timedelta(seconds=CHECK_UTTERANCE_TIME_WINDOW_SECONDS)
         self.compliance_criteria = ["CriticalVictim",
                                     "CriticalMarkerBlock"]
+
+
+class HelpRequestRoomEscapeIntervention(Intervention):
+    def __init__(self, timestamp: datetime, for_player: str) -> None:
+        super().__init__(timestamp, for_player)
+        self.type = InterventionType.UTTERANCE
+        self.description = "help_request_for_room_escape"
+        self.expiration = timestamp + \
+            timedelta(seconds=CHECK_UTTERANCE_TIME_WINDOW_SECONDS)
+        self.compliance_criteria = ["Stuck",
+                                    "HelpRequest",
+                                    "NeedAction",
+                                    "NeedItem",
+                                    "NeedRole",
+                                    "SOSMarker"]
 
 
 def extract_player_information(message) -> dict[str, str]:
@@ -65,6 +78,11 @@ def extract_intervention(message, timestamp: datetime) -> list[Intervention]:
                 intervention = HelpRequestCritcalVictimIntervention(
                     timestamp, receiver)
                 interventions.append(intervention)
+        if "threat room" in explanation:
+            for receiver in message["data"]["receivers"]:
+                intervention = HelpRequestRoomEscapeIntervention(
+                    timestamp, receiver)
+                interventions.append(intervention)
     else:
         print("[INFO] Event is ignored: " +
               message["data"]["explanation"]["info"])
@@ -72,8 +90,10 @@ def extract_intervention(message, timestamp: datetime) -> list[Intervention]:
     return interventions
 
 
-def log_complied_intervention(intervention: Intervention) -> None:
-    print(f"[INFO] Intervention {intervention.description} succeeded by player {intervention.for_player}")
+def log_report(output_dir: str, report: dict[str, any]) -> None:
+    with open(output_dir + "/compliance_instances_report.txt", 'w') as file:
+        for key, value in report.items():
+            file.write(f"{key}: {value}\n")
 
 
 if __name__ == "__main__":
@@ -85,7 +105,21 @@ if __name__ == "__main__":
         help="Directory containing .metadata files",
         default="/media/mule/projects/tomcat/protected/study-3_2022",
     )
+    parser.add_argument(
+        "--output_dir",
+        help="Output directory",
+        default=".",
+    )
     args = parser.parse_args()
+
+    report = {
+        "num_interventions": 0,
+        "num_compiled_interventions": 0,
+        "num_intervention_help_request_for_critical_victim": 0,
+        "num_complied_intervention_help_request_for_critical_victim": 0,
+        "num_intervention_help_request_for_room_escape": 0,
+        "num_complied_intervention_help_request_for_room_escape": 0
+    }
 
     for filepath in glob(args.data_dir + "/*T00*UAZ*.metadata"):
         player_information: dict[str, str] = {}
@@ -125,6 +159,14 @@ if __name__ == "__main__":
                 interventions = extract_intervention(message, timestamp)
                 watch_interventions += interventions
 
+                for intervention in interventions:
+                    if isinstance(intervention, HelpRequestCritcalVictimIntervention):
+                        report["num_intervention_help_request_for_critical_victim"] += 1
+                    elif isinstance(intervention, HelpRequestRoomEscapeIntervention):
+                        report["num_intervention_help_request_for_room_escape"] += 1
+
+                report["num_interventions"] += len(interventions)
+
             # parse dialog agent message to check for compliance
             if "topic" in message and message["topic"] == AGENT_DIALOG_TOPIC:
                 if message["data"]["participant_id"] == "Server":
@@ -133,15 +175,27 @@ if __name__ == "__main__":
                 # ensure player identification consistency
                 assert message["data"]["participant_id"] in player_information.keys()
 
+                # check for any intervention that has been complied by the subject
                 complied_interventions = []
                 for labels in message["data"]["extractions"]:
                     for intervention in watch_interventions:
+                        # check if the message's subject matches the intervention's target subject
                         if intervention.for_player == player_information[message["data"]["participant_id"]]:
+                            # check compliance criteria for intervention about utterance
                             if intervention.type == InterventionType.UTTERANCE:
                                 for compliance_label in intervention.compliance_criteria:
                                     if compliance_label in labels and intervention not in complied_interventions:
-                                        complied_interventions.append(intervention)
+                                        complied_interventions.append(
+                                            intervention)
 
                 for intervention in complied_interventions:
-                    log_complied_intervention(intervention)
                     watch_interventions.remove(intervention)
+
+                    if isinstance(intervention, HelpRequestCritcalVictimIntervention):
+                        report["num_complied_intervention_help_request_for_critical_victim"] += 1
+                    elif isinstance(intervention, HelpRequestRoomEscapeIntervention):
+                        report["num_complied_intervention_help_request_for_room_escape"] += 1
+
+                    report["num_compiled_interventions"] += 1
+
+    log_report(args.output_dir, report)
