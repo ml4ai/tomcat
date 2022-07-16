dbaca631421803555d8a4fcb934969349a4279f0
Joseph Astier <jastier@gmail.com>
2022-07-15 17:35:22 -0700

    Test module argument

--- tools/test_scripts/test_gs_data.py
+++ tools/test_scripts/test_gs_data.py
@@ -4,43 +4,48 @@ import argparse
 import subprocess
 import sys
 import json
-import ac_tamu_ta1_dialog_act_classifier
+import importlib
 
 # Authors:  Joseph Astier, Adarsh Pyarelal
 
-# Check metadata files in target directories.  No recursion.
+# Check metadata files in first level of target directories
 
-def test_file(dataset, filename):
-    test = {'file':filename}
+def test_file(filename, args):
 
-    filepath = f'{dataset}/{filename}'
+    test_module = __import__(f'{args.test_module}',fromlist=[''])
 
-    print(f'Testing {filepath}')
+    return {
+      'file':filename,
+      'results': test_module.test_metadata_file(filename)
+    }
 
-    results = ac_tamu_ta1_dialog_act_classifier.test_metadata_file(filepath)
-    test['results'] = results
-    return test
 
-def get_filenames(datasets):
-    results = []
-    for dataset in datasets:
-        proc = subprocess.run(['ls', dataset], capture_output=True)
-        if proc.returncode == 0:
-            result = {'name':dataset}
-            tests = []
-            output = proc.stdout.decode('utf-8')
-            output_lines = output.split('\n')
-            for line in output_lines:
-                if line.endswith('.metadata'):
-                    tests.append(test_file(dataset, line))
-            result['tests']=tests
-            results.append(result)
+# Test a directory of files, one level deep
+def test_dataset(dataset, args):
 
-        else:
-            print(f'Problem reading from directory {dataset}')
-            print(proc.stderr.decode('utf-8'))
+    d = {
+        'dataset':dataset,
+        'tests': []
+    }
 
-    return results
+    proc = subprocess.run(['ls', dataset], capture_output=True)
+    if not proc.returncode == 0:
+        print(f'Problem reading from directory {dataset}')
+        print(proc.stderr.decode('utf-8'))
+        return d
+
+    output = proc.stdout.decode('utf-8')
+    lines = output.split('\n')
+    filenames = filter(lambda x: x.endswith('.metadata'), lines)
+    filepaths = map(lambda x: f'{dataset}/{x}', filenames)
+    results = map(lambda x: test_file(x, args), filepaths)
+    d['results'] = list(results)
+    return d
+
+def test_datasets(datasets, args):
+    tests = map(lambda x: test_dataset(x, args), datasets)
+    results = filter(lambda x: len(x['results']) > 0, tests)
+    return list(results)
 
 def report_results(report, args):
     print(json.dumps(report, indent=4, sort_keys=True))
@@ -54,24 +59,28 @@ def report_results(report, args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-
+    parser.add_argument(
+        'test_module', 
+        action='store', 
+        help = (
+            'Name of test module.   It must contain a method called '
+            'test_metadata_file that takes a filename argument. '
+        )
+    ) 
     parser.add_argument(
         'datasets', 
         action='store', 
         nargs = '+',
         help = 'One or more dataset names, e.g. \"TM000nnn\"'
     ) 
-
-    # Optionally write the test results to a JSON file
     parser.add_argument(
         '-o',
         '--output',
-        help = 'Output filename'
+        help = 'Write the test results to a JSON file'
     )
 
     args = parser.parse_args(sys.argv[1:])
 
     # Test all the Testbed datasets specified by the user
-    test_results = get_filenames(args.datasets)
-
+    test_results = test_datasets(args.datasets, args)
     report_results(test_results, args)
