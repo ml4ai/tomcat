d268db87501646f6826ec31016182dc0411759c5
Joseph Astier <jastier@gmail.com>
2022-07-28 09:07:19 -0700

    Added JSON parse exception handler

--- exe/cpp_agent_template/ReferenceAgent.cpp
+++ exe/cpp_agent_template/ReferenceAgent.cpp
@@ -7,10 +7,18 @@ using namespace std;
 using json = nlohmann::json;
 
 void ReferenceAgent::process(mqtt::const_message_ptr msg) {
-    json jv = json::parse(msg->to_string());
+    string msgstr = msg->to_string();
+
+    try {
+        json jv = json::parse(msgstr);
+        cout << jv << endl;
+    }
+    catch (nlohmann::detail::parse_error) {
+        cout << "Could not parse " << msgstr << endl;
+
+    }
 
     // Uncomment the line below to print the message
-    cout << jv << endl;
 }
 
 ReferenceAgent::ReferenceAgent(string address) : Agent(address){};
