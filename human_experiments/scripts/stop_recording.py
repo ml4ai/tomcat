65917c69879e26acf75beba68c8c8feb9c4a046a
rchamplin <rchamplin@itcap.com>
2022-08-12 14:47:00 -0700

    created python script to stop the 3 intances of LabRecorder from recording by using LabRecorder's API, this python script is called from stop_lab_recorder

--- human_experiments/scripts/stop_recording.py
+++ human_experiments/scripts/stop_recording.py
@@ -1,21 +1,28 @@
 import socket
-import subprocess, os
 import time
 import sys
 
-s1 = socket.create_connection(("localhost", 22345))
-s1.sendall(b"stop\n")
-s1.close()
+#s1 = socket.create_connection(("localhost", 22345))
+#s1.sendall(b"stop\n")
+#s1.close()
 
-s2 = socket.create_connection(("localhost", 22346))
-s2.sendall(b"stop\n")
-s2.close()
+#s2 = socket.create_connection(("localhost", 22346))
+#s2.sendall(b"stop\n")
+#s2.close()
 
-s3 = socket.create_connection(("localhost", 22347))
-s3.sendall(b"stop\n")
-s3.close()
+#s3 = socket.create_connection(("localhost", 22347))
+#s3.sendall(b"stop\n")
+#s3.close()
 
+# Send LabRecorder API stop recording command to each intance running on CAT
+# (Lion on port:22345, Tiger on port:22346 and Leopard on port:22347)
+for port in (22345, 22346, 22347):
+    s = socket.create_connection(("localhost", port))
+    s.sendall(b"stop\n")
+    s.close()
+
+# Must give LabRecorder instances time to switch to "stop recording mode" and
+# properly close their "xdf" files.
 time.sleep(10.0)
 
-sys.exit("All LabRecorder Recordings Stopped (Lion, Tiger and Leopard)!")
-
+sys.exit("All LabRecorder recordings stopped (Lion, Tiger and Leopard)!")
