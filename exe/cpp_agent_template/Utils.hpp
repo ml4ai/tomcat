05e5ef456efc96cba0465269a7229516f2e59107
Joseph Astier <jastier@gmail.com>
2022-08-16 12:46:29 -0700

    Changed a variable name

--- exe/cpp_agent_template/Utils.hpp
+++ exe/cpp_agent_template/Utils.hpp
@@ -14,8 +14,8 @@ class Utils {
 
     bool value_matches(json::object obj1, json::object obj2, string key); 
 
-    json::object get_object(string name, json::object source);
-    string get_string(string name, json::object source);
+    json::object get_object(string key, json::object source);
+    string get_string(string key, json::object source);
 
     string get_timestamp();
 };
