38faf93b48914e56009b088dedf06314cff1f229
Joseph Astier <jastier@gmail.com>
2022-08-16 12:45:45 -0700

    Removed Processors (they became agents)

--- exe/cpp_agent_template/Coordinator.hpp
+++ exe/cpp_agent_template/Coordinator.hpp
@@ -8,12 +8,11 @@
 
 #include <mqtt/async_client.h>
 
-#include "Processor.hpp"
-#include "HeartbeatProducer.hpp"
-#include "ReferenceAgentProcessor.hpp"
-#include "RollcallRequestProcessor.hpp"
-#include "VersionInfoProcessor.hpp"
-#include "Utils.hpp"
+#include "BaseAgent.hpp"
+#include "ReferenceAgent.hpp"
+#include "VersionInfoAgent.hpp"
+#include "RollcallAgent.hpp"
+#include "HeartbeatAgent.hpp"
 
 namespace json = boost::json;
 
@@ -24,17 +23,12 @@ using namespace std;
 class Coordinator {
 
     /** Processors for subscribed message topics */
-    ReferenceAgentProcessor p0;
-    RollcallRequestProcessor p1;
-    VersionInfoProcessor p2;
-    HeartbeatProducer heartbeat_producer;
-    static const int N_PROCESSORS = 4;
-    Processor *processors[N_PROCESSORS] = {
-        &p0, &p1, &p2, &heartbeat_producer
-    };
-
-    /** general functions */
-    Utils utils;
+    ReferenceAgent p0;
+    VersionInfoAgent p1;
+    RollcallAgent p2;
+    HeartbeatAgent heartbeat;
+    static const int N_AGENTS = 4;
+    BaseAgent *agents[N_AGENTS] = {&p0, &p1, &p2, &heartbeat};
 
     /** config state */
     json::object config;
@@ -42,17 +36,17 @@ class Coordinator {
     /** Flag to specify whether the agent is running or not */
     bool running = true;
 
-    /** Function that processes incoming messages */
-    virtual void process(mqtt::const_message_ptr msg) {};
-
-    /** heartbeats every 10 s */
+    /** MQTT client */
+    std::shared_ptr<mqtt::async_client> mqtt_client;
 
   public:
-    std::shared_ptr<mqtt::async_client> mqtt_client;
 
     /** Constructor */
     Coordinator(json::object config);
 
     /** Stop the agent */
     void stop();
+
+    /** Start the agent */
+    void start();
 };
