165e1299f9f039c5f75215dbc2c7e1205ce6223b
Paulo Soares <paulosoares@SBS-7186.local>
2022-08-29 13:39:21 -0700

    Changing filename format.

--- human_experiments/scripts/macos/record_screen.sh
+++ human_experiments/scripts/macos/record_screen.sh
@@ -9,7 +9,7 @@ frame_rate=$2
 # ffmpeg was installed with MacPorts. The PATH variable is updated in ~/.zprofile by MacPorts.
 source "$HOME/.zprofile"
 
-RECORDINGS_FILEPATH="$out_dir/%Y-%m-%d_%H-%M-%S.%s.png"
+RECORDINGS_FILEPATH="$out_dir/output%06d.png.png"
 LOG_FILEPATH="$out_dir/ffmpeg.log"
 
 ffmpeg -f avfoundation -i 'Capture screen 0' -pix_fmt rgb24 -r "$frame_rate" -s 1280x720 "$RECORDINGS_FILEPATH" &> "$LOG_FILEPATH" &
