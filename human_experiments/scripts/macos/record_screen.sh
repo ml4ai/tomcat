c2c8f57ab082f6e25988cf8980ad6cc287784249
Paulo Soares <paulosoares@email.arizona.edu>
2022-08-29 13:45:18 -0700

    Ffmpeg revert (#395)
    * Removing timestamp configuration from the ffmpeg command.
    
    * Kill audio streamer (#394)
    
    * Removing unecessary padding in the call to the kill command
    
    * n/a
    
    * Skipping checking the connections for a while.
    
    * Killing audio streamer process ok.
    
    Co-authored-by: Paulo Soares <paulosoares@SBS-7186.local>
    
    * n/a
    
    * Changing filename format.
    
    * Adding checking for connections back.
    
    Co-authored-by: Paulo Soares <paulosoares@SBS-7186.local>

--- human_experiments/scripts/macos/record_screen.sh
+++ human_experiments/scripts/macos/record_screen.sh
@@ -9,9 +9,7 @@ frame_rate=$2
 # ffmpeg was installed with MacPorts. The PATH variable is updated in ~/.zprofile by MacPorts.
 source "$HOME/.zprofile"
 
-CONFIG="settb=AVTB,setpts='trunc(PTS/1K)*1K+st(1,trunc(RTCTIME/1K))-1K*trunc(ld(1)/1K)', "
-CONFIG+="drawtext=text='%{localtime}.%{eif\:1M*t-1K*trunc(t*1K)\:d}':fontsize=54:fontcolor=red@0.9:x=10:y=1400"
-RECORDINGS_FILEPATH="$out_dir/%Y-%m-%d_%H-%M-%S.%s.png"
+RECORDINGS_FILEPATH="$out_dir/output%06d.png.png"
 LOG_FILEPATH="$out_dir/ffmpeg.log"
 
-ffmpeg -f avfoundation -i 'Capture screen 0' -pix_fmt rgb24 -r "$frame_rate" -s 1280x720 -vf "$CONFIG" -strftime 1 "$RECORDINGS_FILEPATH" &> "$LOG_FILEPATH" &
+ffmpeg -f avfoundation -i 'Capture screen 0' -pix_fmt rgb24 -r "$frame_rate" -s 1280x720 "$RECORDINGS_FILEPATH" &> "$LOG_FILEPATH" &
