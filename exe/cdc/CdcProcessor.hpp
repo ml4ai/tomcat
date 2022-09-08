9a355c469c5c8e4933d55f88c09bc56712cebc6c
Joseph Astier <jastier@gmail.com>
2022-09-08 00:18:10 -0700

    Deleted some unused vectors

--- exe/cdc/CdcProcessor.hpp
+++ exe/cdc/CdcProcessor.hpp
@@ -13,18 +13,18 @@ class CdcProcessor : public Processor {
     const std::string dialog_topic = "agent/dialog";
     const std::string dialog_type = "event";
     const std::string dialog_sub_type = "Event:dialogue_event";
-    std::vector<std::string> input_topics;
 
     // publication
     const std::string cdc_topic = "agent/tomcat-CDC/coordination_event";
     const std::string cdc_type = "status";
     const std::string cdc_sub_type = "Event:dialog_coordination_event";
-    std::vector<std::string> output_topics;
     void publish_coordination_message(const json::object &evidence);
     
     // Simple function that allows you to look for a simple label
     bool look_for_label(const json::array &extractions,
 		        const std::string label);
+
+    json::object find_evidence(const json::object &input_file);
 
   public:
 
