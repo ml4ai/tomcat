51f9481f09d643c627279e4ee1233ebbf56abdb7
Joseph Astier <jastier@gmail.com>
2022-10-03 13:07:48 -0700

    Transitioning to a C project

--- exe/paho_mqtt/main.c
+++ exe/paho_mqtt/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*#include <mqtt/async_client.h>a */
+#include <string.h>
+#include <MQTTClient.h>
 
 /* A Paho MQTT hello world program to test Docker */
+/* Further reading:  https://github.com/eclipse/paho.mqtt.c */
 
 int main(int argc, char* argv[]) {
 
     printf("Hello world!\n");
 
     char* address = "tcp://localhost:1883";
+
+    MQTTClient handle;
+
+    MQTTClient_connectOptions options;
+
+    int rc = MQTTClient_connect(handle, &options);
+
 
     /*
     mqtt::async_client async_client = mqtt::async_client(
