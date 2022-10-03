4ed972fec01f083d381bee2b5c170c5ac313f2e8
Joseph Astier <jastier@gmail.com>
2022-10-03 10:34:37 -0700

    Added a hello world Paho MQTT project to test Docker

---
+++ exe/paho_mqtt/main.cpp
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <csignal>
+#include <fstream>
+#include <functional>
+#include <iostream>
+
+// A Paho MQTT hello world program to test Docker
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "Hello world!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
