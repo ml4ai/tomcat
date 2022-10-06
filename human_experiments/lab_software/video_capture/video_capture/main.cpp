933f67e16e018af577cf776ee8d1027b05c7ec7f
Manujinda Wathugala <manujinda@email.arizona.edu>
2022-10-06 08:38:56 -0700

    Frame output directory
    The path to the directory is accepted as the first command line argument
    of the program.
    
    The program checks whether the directory exists and if not creates it.

--- human_experiments/lab_software/video_capture/video_capture/main.cpp
+++ human_experiments/lab_software/video_capture/video_capture/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
 #include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace cv;
 
+void create_output_directory( string directory ) {
+    const std::filesystem::path p(directory);
+    std::error_code ec;
+    
+    if (std::filesystem::exists( p, ec )) {
+        cout << "\n\tWrinig frames to already exisiting path: " << directory << endl;
+    } else {
+        cout << "\n\tDirectory: " << directory << " does not exists. Creating it.\n";
+        
+        if (std::filesystem::create_directories( directory, ec)) {
+            cerr << "\n\t**** ERROR: Directory " << directory << " Could not be created. ****\n\n\t\tExiting!";
+            return;
+        }
+    }
+}
 
-void webcam() {
+
+void webcam(string directory) {
+    create_output_directory( directory );
+
     VideoCapture cap(0);
     cap.open(0); //turn on camera
     
@@ -26,6 +47,7 @@ void webcam() {
 
 
 int main(int argc, const char * argv[]) {
-    webcam();
+    string directory = argv[1];
+    webcam( directory );
     return 0;
 }
