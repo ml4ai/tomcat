7bc6ff22807a8eb7f4943e56c740f46e86755764
Joseph Astier <jastier@gmail.com>
2022-10-08 23:31:23 -0700

    Added a dockerized base project

---
+++ exe/docker_base/README.txt
@@ -0,0 +1,13 @@
+
+To build with Dockerfile
+
+docker build . -t c_hello_world
+
+
+To run with docker-compose.yml
+
+docker-compose up
+
+
+
+docker-compose up --build
---
+++ exe/docker_base/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* A hello world C program to gain proficiency with Docker */
+
+int main(int argc, char* argv[]) {
+
+    printf("Hello world!\n");
+
+    return EXIT_SUCCESS;
+}
