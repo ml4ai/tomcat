d8b55fc54b3c12acce74c683a64cd2c744e4bab6
Joseph Astier <jastier@gmail.com>
2022-10-10 16:10:57 -0700

    Build with gcc

--- exe/paho_mqtt_c/main.c
+++ exe/paho_mqtt_c/main.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <MQTTClient.h>
-
-/* A Paho MQTT hello world program to test Docker */
-/* Further reading:  https://github.com/eclipse/paho.mqtt.c */
-
-int main(int argc, char* argv[]) {
-
-    printf("Hello world!\n");
-
-    char* address = "tcp://localhost:1883";
-
-    MQTTClient handle;
-
-    MQTTClient_connectOptions options;
-
-/*    int rc = MQTTClient_connect(handle, &options); */
-
-
-    /*
-    mqtt::async_client async_client = mqtt::async_client(
-        address, "paho_mqtt_async_client");
-	*/
-
-    return EXIT_SUCCESS;
+#include "MQTTClient.h"
+ 
+#define ADDRESS     "tcp://mqtt.eclipseprojects.io:1883"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "MQTT Examples"
+#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
+ 
+int main(int argc, char* argv[])
+{
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+    int rc;
+ 
+    if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+         printf("Failed to create client, return code %d\n", rc);
+         exit(EXIT_FAILURE);
+    }
+    
+    /* truncated source example */
+    printf("So far so good\n");
+    exit(EXIT_FAILURE);
 }
