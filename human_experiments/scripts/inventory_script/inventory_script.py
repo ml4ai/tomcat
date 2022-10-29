489b397b52a32629b319b4f76287cd076b8a3ed9
CalebUAz <calebshibu@email.arizona.edu>
2022-10-28 21:30:43 -0700

    uncomment main call to functions

--- human_experiments/scripts/inventory_script/inventory_script.py
+++ human_experiments/scripts/inventory_script/inventory_script.py
@@ -20,15 +20,15 @@ def inventory_script(rootdir):
         )
     else:
         folder_seq = ['baseline_tasks', 'minecraft', 'lion', 'tiger', 'leopard', 'asist_logs', 'vocalics']
-        # fcount_baseline_task(rootdir+'baseline_tasks')
-        # checkfile_minecraft(rootdir+'minecraft')
-        # check_xdf(rootdir)
+        fcount_baseline_task(rootdir+'baseline_tasks')
+        checkfile_minecraft(rootdir+'minecraft')
+        check_xdf(rootdir)
         check_pupil_recorder(rootdir)
-        # check_audio(rootdir, 'lion')
-        # check_audio(rootdir, 'tiger')
-        # check_audio(rootdir, 'leopard')
-        # check_tar_file(rootdir)
-        # check_vocalics(rootdir)
+        check_audio(rootdir, 'lion')
+        check_audio(rootdir, 'tiger')
+        check_audio(rootdir, 'leopard')
+        check_tar_file(rootdir)
+        check_vocalics(rootdir)
             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
