2e85e2a3cf9d64261a14d109622993a0b0f6a144
CalebUAz <calebshibu@email.arizona.edu>
2022-10-28 20:18:40 -0700

    inventory script ready

---
+++ human_experiments/scripts/inventory_script/inventory_script.py
@@ -0,0 +1,53 @@
+from itertools import count
+import sys  # We use sys.exit() so that all exceptions can properly propogate up and cause the interpreter to exit.
+import os
+import json
+import argparse
+import datetime
+from termcolor import colored
+from utils import checkfile_minecraft, fcount_baseline_task, check_audio, check_tar_file, check_vocalics
+
+def inventory_script(rootdir, sid):
+    dir = os.listdir(rootdir)
+
+    if len(dir) == 0:
+        # sometime .DS_Store might be there so the length would be 1
+        print(
+            colored(
+                "\n[Error] Root directory file is missing", "red", attrs=["bold"]
+            ),
+            "\N{cross mark}",
+        )
+    else:
+        folder_seq = ['baseline_tasks', 'minecraft', 'lion', 'tiger', 'leopard', 'asist_logs', 'vocalics']
+        fcount_baseline_task(rootdir+'baseline_tasks', sid)
+        # checkfile_minecraft(rootdir+'minecraft')
+        # check_audio(rootdir, 'lion')
+        # check_audio(rootdir, 'tiger')
+        # check_audio(rootdir, 'leopard')
+        # check_tar_file(rootdir)
+        # check_vocalics(rootdir)
+            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Inventory Script that checks for metadata files from baseline task and minecraft"
+    )
+    parser.add_argument(
+        "--p1",
+        required=True,
+        help="Path to experiment folder",
+    )
+    parser.add_argument(
+        "--s", 
+        required=True, 
+        help="Enter the Subject ID", 
+        action='append'
+    )
+    arg = parser.parse_args()
+    rootdir = arg.p1
+    sid = arg.s
+    print(
+        colored("[Status] Root Directory:", "blue", attrs=["bold"]),
+        colored(rootdir, "cyan"),
+    )
+    sys.exit(inventory_script(rootdir, sid))
