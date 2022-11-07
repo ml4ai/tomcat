6d3a8b20497da84bedd38c3e5f7cf85a07054f6d
CalebUAz <calebshibu@email.arizona.edu>
2022-11-07 13:34:46 -0700

    exclude no show participants

--- human_experiments/lab_software/xdf_to_csv_conversion/XDF_to_csv.py
+++ human_experiments/lab_software/xdf_to_csv_conversion/XDF_to_csv.py
@@ -6,63 +6,68 @@ import argparse
 from termcolor import colored
 from utils import get_start_stop_time_from_xdf, dataframe_to_csv, create_time_distribution, str2bool
 
-def read_xdf(xdf_file_paths, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv):
+def read_xdf(xdf_file_paths, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv, exclude):
     """
     Read the XDF files.
     """
     columns = shutil.get_terminal_size().columns
     for path in xdf_file_paths:
         data, header = pyxdf.load_xdf(path)
+        
+        if exclude not in path:             
+            if 'lion' in path: print(colored('Lion ', 'magenta', attrs=['bold', 'blink']).center(columns))
+            elif 'leopard' in path: print(colored('Leopard ', 'magenta', attrs=['bold', 'blink']).center(columns))
+            else: print(colored('Tiger ', 'magenta', 'on_blue', attrs=['bold', 'blink']).center(columns))
 
-        if 'lion' in path: print(colored('Lion ', 'magenta', attrs=['bold', 'blink']).center(columns))
-        elif 'leopard' in path: print(colored('Leopard ', 'magenta', attrs=['bold', 'blink']).center(columns))
-        else: print(colored('Tiger ', 'magenta', 'on_blue', attrs=['bold', 'blink']).center(columns))
+            for i in range(0,len(data)):
+                if data[i]['info']['type'] == ['NIRS']:
+                    print(
+                    colored('[Status] Reading ', 'green', attrs=['bold']), 
+                    colored(data[i]['info']['type'], 'blue'))
+                    time_start_streams_nirs, time_end_streams_nirs = get_start_stop_time_from_xdf(data[i]) #get the unix time
+                    time_distribution_human_readable_nirs, time_distribution_unix_nirs = create_time_distribution(time_start_streams_nirs, 
+                                                                                time_end_streams_nirs, len(data[i]['time_series']))
+                    dataframe_to_csv(path, data[i]['time_series'], 'NIRS', time_distribution_human_readable_nirs, 
+                    time_distribution_unix_nirs, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv)
 
-        for i in range(0,len(data)):
-            if data[i]['info']['type'] == ['NIRS']:
-                print(
-                colored('[Status] Reading ', 'green', attrs=['bold']), 
-                colored(data[i]['info']['type'], 'blue'))
-                time_start_streams_nirs, time_end_streams_nirs = get_start_stop_time_from_xdf(data[i]) #get the unix time
-                time_distribution_human_readable_nirs, time_distribution_unix_nirs = create_time_distribution(time_start_streams_nirs, 
-                                                                            time_end_streams_nirs, len(data[i]['time_series']))
-                dataframe_to_csv(path, data[i]['time_series'], 'NIRS', time_distribution_human_readable_nirs, 
-                time_distribution_unix_nirs, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv)
+                elif data[i]['info']['type'] == ['Markers']:
+                    #We don't have physical marker for our physio data
+                    print(
+                    colored('[Status] Skipping ', 'green', attrs=['bold']), 
+                    colored(data[i]['info']['type'], 'blue'))                
 
-            elif data[i]['info']['type'] == ['Markers']:
-                #We don't have physical marker for our physio data
-                print(
-                colored('[Status] Skipping ', 'green', attrs=['bold']), 
-                colored(data[i]['info']['type'], 'blue'))                
+                elif data[i]['info']['type'] == ['EEG']:
+                    print(
+                    colored('[Status] Reading ', 'green', attrs=['bold']), 
+                    colored(data[i]['info']['type'], 'blue'))
+                    time_start_streams_eeg, time_end_streams_eeg = get_start_stop_time_from_xdf(data[i]) #get the unix time
+                    time_distribution_human_readable_eeg, time_distribution_unix_eeg = create_time_distribution(time_start_streams_eeg, 
+                                                                                time_end_streams_eeg, len(data[i]['time_series'])) 
+                    dataframe_to_csv(path, data[i]['time_series'], 'EEG', time_distribution_human_readable_eeg, 
+                    time_distribution_unix_eeg, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv)
 
-            elif data[i]['info']['type'] == ['EEG']:
-                print(
-                colored('[Status] Reading ', 'green', attrs=['bold']), 
-                colored(data[i]['info']['type'], 'blue'))
-                time_start_streams_eeg, time_end_streams_eeg = get_start_stop_time_from_xdf(data[i]) #get the unix time
-                time_distribution_human_readable_eeg, time_distribution_unix_eeg = create_time_distribution(time_start_streams_eeg, 
-                                                                            time_end_streams_eeg, len(data[i]['time_series'])) 
-                dataframe_to_csv(path, data[i]['time_series'], 'EEG', time_distribution_human_readable_eeg, 
-                time_distribution_unix_eeg, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv)
+                elif data[i]['info']['type'] == ['Gaze']:
+                    print(
+                    colored('[Status] Reading ', 'green', attrs=['bold']), 
+                    colored(data[i]['info']['type'], 'blue'))
+                    time_start_streams_gaze, time_end_streams_gaze = get_start_stop_time_from_xdf(data[i]) #get the unix time
+                    time_distribution_human_readable_gaze, time_distribution_unix_gaze = create_time_distribution(time_start_streams_gaze, 
+                                                                                time_end_streams_gaze, len(data[i]['time_series'])) 
+                    dataframe_to_csv(path, data[i]['time_series'], 'Gaze', time_distribution_human_readable_gaze, 
+                    time_distribution_unix_gaze, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv)
+                
+                elif data[i]['info']['type'] == ['Accelerometer']:
+                    print(
+                    colored('[Status] Skipping ', 'green', attrs=['bold']), 
+                    colored(data[i]['info']['type'], 'blue')) 
+                    #create_csv_file(path, 'Accelerometer')
+                    # time_start_streams_accel, time_end_streams_accel = get_start_stop_time_from_xdf(data[i]) #get the unix time
+        else:
+            print(
+            colored('[Status] Skipping ', 'yellow', attrs=['bold']), 
+            colored(data[i]['info']['type'], 'red'))         
 
-            elif data[i]['info']['type'] == ['Gaze']:
-                print(
-                colored('[Status] Reading ', 'green', attrs=['bold']), 
-                colored(data[i]['info']['type'], 'blue'))
-                time_start_streams_gaze, time_end_streams_gaze = get_start_stop_time_from_xdf(data[i]) #get the unix time
-                time_distribution_human_readable_gaze, time_distribution_unix_gaze = create_time_distribution(time_start_streams_gaze, 
-                                                                            time_end_streams_gaze, len(data[i]['time_series'])) 
-                dataframe_to_csv(path, data[i]['time_series'], 'Gaze', time_distribution_human_readable_gaze, 
-                time_distribution_unix_gaze, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv)
-            
-            elif data[i]['info']['type'] == ['Accelerometer']:
-                print(
-                colored('[Status] Skipping ', 'green', attrs=['bold']), 
-                colored(data[i]['info']['type'], 'blue')) 
-                #create_csv_file(path, 'Accelerometer')
-                # time_start_streams_accel, time_end_streams_accel = get_start_stop_time_from_xdf(data[i]) #get the unix time
-
-def look_for_XDF_files(rootdir_xdf, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv):
+def look_for_XDF_files(rootdir_xdf, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv, exclude):
     """
     Walk through root directory, looking for the xdf files. 
     """
@@ -75,7 +80,7 @@ def look_for_XDF_files(rootdir_xdf, rootdir_baseline_task, rootdir_minecraft_dat
                     colored('[Status] xdf file found at ', 'green', attrs=['bold']), 
                     colored(os.path.join(root, file), 'blue'))
     
-    read_xdf(xdf_file_paths, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv) #1. read all the XDF files 
+    read_xdf(xdf_file_paths, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv, exclude) #1. read all the XDF files 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -111,16 +116,23 @@ if __name__ == "__main__":
         '--csv',
         default=True, 
         type=str2bool, 
-        help="By setting pkl to True you extract xdf files as pickle file")
+        help="By setting csv to True you extract xdf files as csv file")
+
+    parser.add_argument(
+        '--exclude',
+        required=False,
+        default=None,
+        help="Enter iMAC name you'd like to exclude")
 
     arg = parser.parse_args()
+
     rootdir_xdf = arg.p1
     rootdir_baseline_task = arg.p2
     rootdir_minecraft_data = arg.p3
     subject_id = arg.s
     extract_pkl = arg.pkl
     extract_csv = arg.csv
-    print(colored('[Status] Root Directory:', 'green', attrs=['bold']), colored(rootdir_xdf, 'blue'))
-    sys.exit(look_for_XDF_files(rootdir_xdf, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv))
+    exclude  = str(arg.exclude)
 
-#python3 XDF_to_csv.py --p1 /Users/calebjonesshibu/Desktop/tom/dry_runs/exp_2022_09_13_10/exp_2022_09_13_10/ --p2 /Users/calebjonesshibu/Desktop/tom/dry_runs/exp_2022_09_13_10/exp_2022_09_13_10/baseline_tasks --p3 /Users/calebjonesshibu/Desktop/tom/dry_runs/exp_2022_09_13_10/exp_2022_09_13_10/minecraft/ --s 0913_A_1 --s 0913_A_2 --s 0913_A_3
+    print(colored('[Status] Root Directory:', 'green', attrs=['bold']), colored(rootdir_xdf, 'blue'))
+    sys.exit(look_for_XDF_files(rootdir_xdf, rootdir_baseline_task, rootdir_minecraft_data, subject_id, extract_pkl, extract_csv, exclude))
