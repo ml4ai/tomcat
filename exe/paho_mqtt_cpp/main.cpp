9ec1fff1b805ff2eb9370f226ce8f0183cf69db2
Joseph Astier <jastier@gmail.com>
2022-11-10 10:44:24 -0700

    Cleaned up prototype main.cpp

--- exe/paho_mqtt_cpp/main.cpp
+++ exe/paho_mqtt_cpp/main.cpp
@@ -7,7 +7,15 @@
 #include <cstring>
 #include "mqtt/async_client.h"
 
+#define ADDRESS     "tcp://mosquitto:1883"
+#define CLIENTID    "ExampleClientSub"
+
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello world!" << std::endl;
+
+    // demonstrate that library objects are accessible
+    mqtt::async_client ac(ADDRESS, CLIENTID, nullptr);
+
 }
