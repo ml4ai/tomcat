ae6b2d8acb5ee14248da6e737153b05fb57f4a76
Manujinda Wathugala <manujinda@email.arizona.edu>
2022-11-23 09:51:53 -0700

    Updated the code to:
    1. Identify the first and last image file names for each mission and
    each workstation.
    
    2. To output csv files with all the files belonging to each mission,
    each workstation and each type of image capture (face & screen).

--- scripts/metadata/process_baseline_task_metadata.py
+++ scripts/metadata/process_baseline_task_metadata.py
@@ -8,23 +8,122 @@ import os.path
 experiment_dir = '/Users/manujinda/Documents/manujinda/ubunosx/tomcat_data/exp_2022_11_10_10/'
 individual_file = 'individual_00074_1668105048.csv'
 
-meta_file = os.path.join(experiment_dir, 'baseline_tasks/affective/', individual_file)
+meta_file = os.path.join(experiment_dir, 'baseline_tasks/', 'affective/', individual_file)
 #'/Users/manujinda/Documents/manujinda/ubunosx/tomcat_data/exp_2022_11_15_13/baseline_tasks/affective/individual_00014_1668547218.csv'
 
 meta_df = pd.read_csv(meta_file, sep=';', usecols=['time', 'monotonic_time', 'human_readable_time', 'image_path',
                                                    'subject_id', 'event_type'])
 # print(meta_df)
 
-meta_df.to_csv(os.path.join(experiment_dir, 'useful_timestams/', individual_file))
+meta_df.to_csv(os.path.join(experiment_dir, 'useful_timestamps/', individual_file))
 #'/Users/manujinda/Documents/manujinda/ubunosx/tomcat_data/exp_2022_11_15_13/useful_timestams/affective/individual_00014_1668547218.csv')
 
 # meta_minecraft = '/Users/manujinda/Documents/manujinda/ubunosx/tomcat_data/exp_2022_11_15_13/minecraft/MinecraftData_Trial-2_ID-38075ea6-1243-4c7d-8ef4-92b8cdc28fdb.metadata'
 
-start_end = []
+# Load image file listings
+
+workstations = ['cheetah', 'leopard', 'lion', 'tiger']
+workstation = workstations[1]
+
+image_types = ['face_images', 'screenshots']
+image_type = image_types[0]
+
+# 0 - Face images
+# 1 - Screenshots
+file_listings = [{}, {}]
+for idx, image_type in enumerate(image_types):
+    print(image_type)
+    for workstation in workstations:
+        image_listing = os.path.join(experiment_dir, workstation, image_type, 'listing.txt')
+        if not os.path.isfile(image_listing):
+            continue
+
+        print('\t', workstation)
+        file_names_df = pd.read_csv(image_listing, sep='\s+', skiprows=[0], skipfooter=3, engine='python',
+                                    names=['permission', 'links', 'owner', 'group', 'size', 'month', 'date', 'time', 'file name'])
+        file_names_df['png'] = file_names_df['file name'].apply(lambda file_name: file_name[-5:] == 'Z.png')
+        file_names_df = file_names_df[file_names_df['png']]
+        file_names_df['timestamp'] = file_names_df['file name'].apply(lambda dt: pd.Timestamp(dt[:-4].replace('_', ':')))
+        file_names_df.sort_values(by=['timestamp'], inplace=True)
+        file_listings[idx][workstation] = file_names_df
+        file_names_df.to_csv(os.path.join(experiment_dir, 'useful_timestamps/', f'{workstation}_{image_type}_listing.csv'), index=False)
+
+# print(file_listings)
+# exit()
+
+# Process Minecraft metadata
+
+
+def map_image_timestamps(_mission_id, _mission_name, _mission_state, _mission_timestamp, _image_file_listings, _image_types, _mission_start_timestamp, _experiment_dir):
+    _ws_file_names = {}
+    for _idx, _img_listings in enumerate(_image_file_listings):
+        _img_type = _image_types[_idx]
+        print(_img_type)
+
+        for _ws, _ws_img_listing_df in _img_listings.items():
+
+            _img_file_name = ''
+            if _mission_state == 'Start':
+                _img_file_name = _ws_img_listing_df[_ws_img_listing_df['timestamp'] >= _mission_timestamp].iloc[0]['file name']
+            elif _mission_state == 'Stop':
+                _ws_mission_images_df = _ws_img_listing_df[(_ws_img_listing_df['timestamp'] >= _mission_start_timestamp) &
+                                                    (_ws_img_listing_df['timestamp'] <= _mission_timestamp)]
+                _img_file_name = _ws_mission_images_df.iloc[-1]['file name']
+                _ws_mission_images_df.to_csv(os.path.join(_experiment_dir, 'useful_timestamps/', f'Minecraft_mission_{_img_type}_{_ws}_{_mission_id}_{_mission_name}.csv'), index=False)
+
+            print(_ws, _mission_state, '\t', _mission_timestamp, '\t', _img_file_name)
+            _ws_file_names[f'{_ws}_{_img_type}'] = _img_file_name
+
+    return _ws_file_names
+
 
 meta_minecraft_dir = os.path.join(experiment_dir, 'minecraft')
 
-lion_faces = os.path.join(experiment_dir, 'lion', 'face_images', 'listing.txt')
+start_end = [{'mission_timestamp': None}]
+
+for filename in os.listdir(meta_minecraft_dir):
+    trial = filename.split('_')[1].split('-')[1]
+    print(trial)
+    meta_minecraft = os.path.join(meta_minecraft_dir, filename)
+    # checking if it is a file
+    if os.path.isfile(meta_minecraft):
+        with open(meta_minecraft) as fp:
+            for line in fp:
+                ln_json = json.loads(line)
+                if 'data' in ln_json and 'mission_state' in ln_json['data']:
+
+                    mission = ln_json['data']['mission']
+                    mission_state = ln_json['data']['mission_state']
+                    mission_timestamp = pd.Timestamp(ln_json['header']['timestamp'])
+
+                    row = {'trial': trial,
+                           'experiment_id': ln_json['msg']['experiment_id'],
+                           'trial_id': ln_json['msg']['trial_id'],
+                           'mission': mission,
+                           'mission_state': mission_state,
+                           'timestamp_1': ln_json['msg']['timestamp'],
+                           'timestamp_2': ln_json['header']['timestamp'],
+                           '@timestamp': ln_json['@timestamp'],
+                           'mission_timestamp': mission_timestamp,
+                           }
+
+                    row.update(map_image_timestamps(trial, mission, mission_state, mission_timestamp, file_listings, image_types, start_end[-1]['mission_timestamp'], experiment_dir))
+
+                    start_end.append(row)
+
+start_end_df = pd.DataFrame(start_end[1:])
+start_end_df.sort_values(by=['trial', 'timestamp_1'], inplace=True)
+start_end_df.to_csv(os.path.join(experiment_dir, 'useful_timestamps/', 'Minecraft_mission_timestamps.csv'), index=False)
+
+exit()
+
+
+
+
+
+
+
+lion_faces = os.path.join(experiment_dir, workstation, image_type, 'listing.txt')
 lion_faces_df = pd.read_csv(lion_faces, sep='\s+', skiprows=[0], skipfooter=3, engine='python',
                             names=['permission', 'links', 'owner', 'group', 'size', 'month', 'date', 'time', 'file name'])
 lion_faces_df['timestamp'] = lion_faces_df['file name'].apply(lambda dt: pd.Timestamp(dt[:-4].replace('_', ':')))
@@ -33,7 +132,7 @@ lion_faces_df['timestamp'] = lion_faces_df['file name'].apply(lambda dt: pd.Time
 # lion_faces_df['second'] = lion_faces_df['second'].apply(lambda sec: float(sec[:-5]))
 lion_faces_df.sort_values(by=['timestamp'], inplace=True)
 print(lion_faces_df)
-lion_faces_df.to_csv(os.path.join(experiment_dir, 'useful_timestams/lion_faces_listing.csv'), index=False)
+lion_faces_df.to_csv(os.path.join(experiment_dir, 'useful_timestamps/', f'{workstation}_faces_listing.csv'), index=False)
 #'/Users/manujinda/Documents/manujinda/ubunosx/tomcat_data/exp_2022_11_15_13/useful_timestams/lion_faces_listing.csv', index=False)
 
 
@@ -88,7 +187,7 @@ for filename in os.listdir(meta_minecraft_dir):
 start_end_df = pd.DataFrame(start_end)
 start_end_df.sort_values(by=['trial', 'timestamp_1'], inplace=True)
 # print(start_end_df)
-start_end_df.to_csv(os.path.join(experiment_dir, 'useful_timestams/Minecraft_mission_timestamps.csv'), index=False)
+start_end_df.to_csv(os.path.join(experiment_dir, 'useful_timestamps/', f'{workstation}_Minecraft_mission_timestamps.csv'), index=False)
 #start_end_df.to_csv('/Users/manujinda/Documents/manujinda/ubunosx/tomcat_data/exp_2022_11_15_13/useful_timestams/Minecraft_mission_timestamps.csv', index=False)
 
 
