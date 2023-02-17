bb6c70316c0e1b51f6776412a32c75f06484acea
Joseph Astier <jastier@arizona.edu>
2023-02-17 14:52:12 -0700

    Changed the MQTT hostname to 'mosquitto' for use within Docker

--- rust/tomcat/src/cli.rs
+++ rust/tomcat/src/cli.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Cli {
     /// MQTT broker host
-    #[arg(long, default_value_t = String::from("localhost"))]
+    #[arg(long, default_value_t = String::from("mosquitto"))]
     pub host: String,
 
     /// MQTT broker port
