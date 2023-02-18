66256da0dc80704cdd16f4f75ce0c39ef5cf6d18
Joseph Astier <jastier@arizona.edu>
2023-02-17 21:42:52 -0700

    Command line args now override config file settings

--- rust/tomcat/src/main.rs
+++ rust/tomcat/src/main.rs
@@ -3,7 +3,8 @@
 use clap::Parser;
 use tomcat::agent::Agent;
 use tomcat::cli::Cli;
-use tomcat::config::Config;
+use tomcat::config::{Config, MqttOpts};
+
 
 fn main() {
     // Initialize the logger from the environment
@@ -11,8 +12,11 @@ fn main() {
 
     let args = Cli::parse();
 
-    let cfg: Config = confy::load_path(&args.config)
+    let mut cfg: Config = confy::load_path(&args.config)
         .unwrap_or_else(|_| panic!("Unable to load config file {}!", &args.config));
+
+    // allow user command line args to override config file settings.
+    cfg.mqtt_opts = MqttOpts{host: args.host, port: args.port};
 
     let mut agent = Agent::new(cfg);
     agent.run().unwrap();
