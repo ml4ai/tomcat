662bc31405d891f53d5081a0ab652fb4ad67e9f9
Joseph Astier <jastier@users.noreply.github.com>
2023-02-19 08:48:41 -0700

    Dockerization (#484)
    * Commented out a bug
    
    * Added Docker files
    
    * Added comment with agent name
    
    * Set the agent name as 'uaz_rust_agent'
    
    * Changed the MQTT hostname to 'mosquitto' for use within Docker
    
    * Reverted all changes to this file
    
    * Added MQTT host and port as command line args
    
    * Added a note to a known bug
    
    * Added some print debugging
    
    * Fixed Docker file
    
    * Command line args now override config file settings
    
    * Removed some debugging print statements
    
    * Config file host and port are used if not set as command line args
    
    ---------
    
    Co-authored-by: Joseph Astier <jastier@arizona.edu>

--- rust/tomcat/src/cli.rs
+++ rust/tomcat/src/cli.rs
@@ -4,12 +4,12 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Cli {
     /// MQTT broker host
-    #[arg(long, default_value_t = String::from("localhost"))]
-    pub host: String,
+    #[arg(long)]
+    pub host: Option<String>,
 
     /// MQTT broker port
-    #[arg(short, long, default_value_t = 1883)]
-    pub port: u16,
+    #[arg(short, long)]
+    pub port: Option<u16>,
 
     /// Config file
     #[arg(short, long, default_value_t = String::from("config.yml"))]
--- rust/tomcat/src/main.rs
+++ rust/tomcat/src/main.rs
@@ -5,14 +5,25 @@ use tomcat::agent::Agent;
 use tomcat::cli::Cli;
 use tomcat::config::Config;
 
+
 fn main() {
     // Initialize the logger from the environment
     pretty_env_logger::init();
 
     let args = Cli::parse();
 
-    let cfg: Config = confy::load_path(&args.config)
+    let mut cfg: Config = confy::load_path(&args.config)
         .unwrap_or_else(|_| panic!("Unable to load config file {}!", &args.config));
+
+    // allow user command line args to override config file settings
+    if let Some(host) = args.host {
+        cfg.mqtt_opts.host = host;
+    }
+    if let Some(port) = args.port {
+        cfg.mqtt_opts.port = port;
+    }
+
+    println!("MQTT host = {}, port = {}", cfg.mqtt_opts.host, cfg.mqtt_opts.port);
 
     let mut agent = Agent::new(cfg);
     agent.run().unwrap();
