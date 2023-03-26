d01b66ce1ec0c1a04da5181c45dee13ff2277424
Paulo Soares <paulosoares@SBS-7186.local>
2023-03-26 08:07:46 -0700

    Splitting LSL streams for affective and ping-pong task for the individual/team and competitive/cooprative scenarions.

--- human_experiments/lab_software/tomcat-baseline-tasks/run_server.py
+++ human_experiments/lab_software/tomcat-baseline-tasks/run_server.py
@@ -40,11 +40,13 @@ def _run_ping_pong(to_client_connections: list,
 def _run_affective_task(to_client_connections: list,
                         from_client_connections: dict,
                         session_name: str,
-                        data_save_path: str = ''):
+                        data_save_path: str = '',
+                        lsl: Optional[LSLStringStream] = None):
     server_affective_task = ServerAffectiveTask(to_client_connections,
                                                 from_client_connections,
                                                 session_name=session_name,
-                                                data_save_path=data_save_path)
+                                                data_save_path=data_save_path,
+                                                lsl=lsl)
 
     server_affective_task.run("./tasks/affective_task/images/task_images", collaboration=False)
 
@@ -84,8 +86,10 @@ if __name__ == "__main__":
     # Also, to make parsing the .xdf for a specific task easier, I decided to create individual streams per task.
     rest_state_lsl_stream = LSLStringStream("Rest State", "rest_state")
     finger_tapping_lsl_stream = LSLStringStream("Finger Tapping", "finger_tapping")
-    affective_task_lsl_stream = LSLStringStream("Affective Task", "affective_task")
-    ping_pong_lsl_stream = LSLStringStream("Ping-Pong", "ping_pong")
+    individual_affective_task_lsl_stream = LSLStringStream("Individual Affective Task", "individual_affective_task")
+    team_affective_affective_task_lsl_stream = LSLStringStream("Team Affective Task", "team_affective_task")
+    competitive_ping_pong_lsl_stream = LSLStringStream("Competitive Ping-Pong", "competitive_ping_pong")
+    cooperative_ping_pong_lsl_stream = LSLStringStream("Cooperative Ping-Pong", "cooperative_ping_pong")
 
     tasks = TASK_LIST.copy()
 
@@ -134,7 +138,6 @@ if __name__ == "__main__":
 
         tasks.pop(0)
 
-    # Individual affective task
     if tasks[0] == "affective":
         print("")
         print("----------------------------------------------")
@@ -142,6 +145,7 @@ if __name__ == "__main__":
         print("----------------------------------------------")
         print("")
 
+        # Individual affective task
         server.establish_connections(num_required_connections=num_participants)
 
         _send_start(list(server.to_client_connections.values()))
@@ -153,7 +157,7 @@ if __name__ == "__main__":
             process = Process(target=_run_affective_task,
                               args=(
                                   [to_client_connection], {from_client_connection: client_name}, session_name,
-                                  args.save))
+                                  args.save, individual_affective_task_lsl_stream))
             affective_task_processes.append(process)
 
         for process in affective_task_processes:
@@ -163,7 +167,6 @@ if __name__ == "__main__":
             process.join()
 
         # Team affective task
-
         server.establish_connections(num_required_connections=num_participants)
 
         _send_start(list(server.to_client_connections.values()))
@@ -172,7 +175,7 @@ if __name__ == "__main__":
                                                     server.from_client_connections,
                                                     session_name="team",
                                                     data_save_path=args.save,
-                                                    lsl=affective_task_lsl_stream)
+                                                    lsl=team_affective_affective_task_lsl_stream)
 
         server_affective_task.run("./tasks/affective_task/images/task_images", collaboration=True)
 
@@ -180,7 +183,6 @@ if __name__ == "__main__":
 
         tasks.pop(0)
 
-    # Ping pong competitive
     if tasks[0] == "ping_pong":
         print("")
         print("----------------------------------------------")
@@ -194,6 +196,7 @@ if __name__ == "__main__":
         else:
             num_required_connections = num_participants
 
+        # Ping pong competitive
         server.establish_connections(num_required_connections=num_required_connections)
 
         _send_start(list(server.to_client_connections.values()))
@@ -209,7 +212,7 @@ if __name__ == "__main__":
             session_name = "competitive_" + str(session_id)
             process = Process(target=_run_ping_pong,
                               args=(to_client_connections, from_client_connection_pair, session_name, True, args.save,
-                                    ping_pong_lsl_stream))
+                                    competitive_ping_pong_lsl_stream))
             ping_pong_processes.append(process)
 
         for process in ping_pong_processes:
@@ -219,7 +222,6 @@ if __name__ == "__main__":
             process.join()
 
         # Ping pong cooperative
-
         server.establish_connections(num_required_connections=num_required_connections)
 
         _send_start(list(server.to_client_connections.values()))
@@ -235,7 +237,8 @@ if __name__ == "__main__":
             session_name = "cooperative_" + str(session_id)
             process = Process(target=_run_ping_pong,
                               args=(
-                                  to_client_connections, from_client_connection_teams, session_name, False, args.save))
+                                  to_client_connections, from_client_connection_teams, session_name, False, args.save,
+                                  cooperative_ping_pong_lsl_stream))
             ping_pong_processes.append(process)
 
         for process in ping_pong_processes:
