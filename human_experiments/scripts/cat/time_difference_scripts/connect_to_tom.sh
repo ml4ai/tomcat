ad5bd209bbfdfa980abece8de3a19e087d739e6e
Eric Duong <eduong@email.arizona.edu>
2023-03-28 14:37:49 -0700

    wrap command around bash

--- human_experiments/scripts/cat/time_difference_scripts/connect_to_tom.sh
+++ human_experiments/scripts/cat/time_difference_scripts/connect_to_tom.sh
@@ -8,7 +8,7 @@ TIME_DIFFERENCE_CLIENT_NAME="time-difference-client"
 __launch_time_difference_cat_client() {
   local command="cd ~/git/ML4AI/tomcat/human_experiments/lab_software/tomcat-time-difference && "
   command+="python3 run_client.py -n CAT"
-  tmux new-session -d -s $TIME_DIFFERENCE_SESSION_NAME -n $TIME_DIFFERENCE_CLIENT_NAME $command
+  tmux new-session -d -s $TIME_DIFFERENCE_SESSION_NAME -n $TIME_DIFFERENCE_CLIENT_NAME "bash -c '$command'"
 }
 
 __launch_time_difference_cat_client
