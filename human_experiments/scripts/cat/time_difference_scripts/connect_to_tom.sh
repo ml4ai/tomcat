155b48c66076da5f46c5f95472fa5a26d83892ca
Eric Duong <eduong@email.arizona.edu>
2023-03-28 14:32:32 -0700

    cat navigation

--- human_experiments/scripts/cat/time_difference_scripts/connect_to_tom.sh
+++ human_experiments/scripts/cat/time_difference_scripts/connect_to_tom.sh
@@ -6,7 +6,7 @@ TIME_DIFFERENCE_SESSION_NAME="time-difference"
 TIME_DIFFERENCE_CLIENT_NAME="time-difference-client"
 
 __launch_time_difference_cat_client() {
-  local command="cd $HOME/$SYSTEMS_DIR/tomcat/human_experiments/lab_software/tomcat-time-difference && "
+  local command="cd ~/git/ML4AI/tomcat/human_experiments/lab_software/tomcat-time-difference && "
   command+="python3 run_client.py -n CAT"
   tmux new-session -d -s $TIME_DIFFERENCE_SESSION_NAME -n $TIME_DIFFERENCE_CLIENT_NAME $command
 }
