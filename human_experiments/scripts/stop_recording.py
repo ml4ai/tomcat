120cfe5243076914cd7226d73774af9635fe0f7d
Paulo Soares <paulosoares@SBS-7186.local>
2023-04-02 13:52:11 -0700

    Updating uber script

--- human_experiments/scripts/stop_recording.py
+++ human_experiments/scripts/stop_recording.py
@@ -5,11 +5,10 @@ import sys
 """Script to stop LabRecorder recording data."""
 
 # Send LabRecorder API stop recording command to each instance running on CAT
-# (Lion on port:22345, Tiger on port:22346, Leopard on port:22347, and TOM (Baseline Tasks) on port:22348)
-for port in (22345, 22346, 22347, 22348):
-    s = socket.create_connection(("localhost", port))
-    s.sendall(b"stop\n")
-    s.close()
+PORT = 22349  # single lab recorder instance with all the streams
+s = socket.create_connection(("localhost", PORT))
+s.sendall(b"stop\n")
+s.close()
 
 # Must give LabRecorder instances time to switch to "stop recording mode" and
 # properly close their XDF files.
