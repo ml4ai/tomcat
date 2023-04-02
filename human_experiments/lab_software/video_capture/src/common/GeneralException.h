afeacbd753d8c509fbff8326eaae50dc9dd3ad01
Adarsh Pyarelal <adarsh@arizona.edu>
2023-04-02 08:39:28 -0700

    Removing video_capture directory

--- human_experiments/lab_software/video_capture/src/common/GeneralException.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdexcept>
-
-class GeneralException : public std::runtime_error {
-  public:
-    GeneralException(std::string const& msg) : std::runtime_error(msg) {}
-};
