93514c89f4094545085b40a706a10cf97ad5bf83
Paulo Soares <paulosoares@SBS-7186.local>
2023-04-02 11:27:20 -0700

    Updating pupil scripts.

--- human_experiments/scripts/local/pupil_api_handler.py
+++ human_experiments/scripts/local/pupil_api_handler.py
@@ -1,9 +1,23 @@
+from typing import Any
+
 import argparse
 import zmq
 
 DEFAULT_PUPIL_PORT = 50020
 START_RECORDING = "R"
 STOP_RECORDING = "r"
+TIMESTAMP = "t"
+
+
+def is_alive(pupil_remote: Any) -> bool:
+    try:
+        pupil_remote.send_string(TIMESTAMP)
+        pupil_remote.recv_string(zmq.NOBLOCK)
+    except zmq.ZMQERROR:
+        return False
+
+    return True
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Controls Pupil Capture remotely via its API.')
@@ -17,11 +31,18 @@ if __name__ == "__main__":
     pupil_remote = zmq.Socket(ctx, zmq.REQ)
     pupil_remote.connect(f"tcp://{args.host}:{DEFAULT_PUPIL_PORT}")
 
-    if args.action == "start":
-        pupil_remote.send_string(START_RECORDING)
-        if "ok" not in pupil_remote.recv_string(zmq.NOBLOCK).lower():
-            raise "Could not start recording."
-    elif args.action == "stop":
-        pupil_remote.send_string(STOP_RECORDING)
-        if "ok" not in pupil_remote.recv_string(zmq.NOBLOCK).lower():
-            raise "Could not stop recording."
+    if is_alive(pupil_remote):
+        if args.action == "start":
+            pupil_remote.send_string(START_RECORDING)
+            return_code = pupil_remote.recv_string()
+
+            # An empty string means it's already recording
+            if return_code != "" and "ok" not in return_code.lower():
+                raise "Could not start recording."
+        elif args.action == "stop":
+            pupil_remote.send_string(STOP_RECORDING)
+            return_code = pupil_remote.recv_string()
+
+            # An empty string means it has already stopped recording
+            if return_code != "" and "ok" not in return_code.lower():
+                raise "Could not stop recording."
