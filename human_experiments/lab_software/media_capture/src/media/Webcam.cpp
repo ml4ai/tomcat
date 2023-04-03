1740f3d4cec896ed1dd9d31b9e9e89167934eac8
Paulo Soares <paulosoares@dhcp-10-142-246-46.uawifi.arizona.edu>
2023-04-03 11:55:32 -0700

    Updating uber script

--- human_experiments/lab_software/media_capture/src/media/Webcam.cpp
+++ human_experiments/lab_software/media_capture/src/media/Webcam.cpp
@@ -34,8 +34,10 @@ Webcam::Webcam(const string& camera_name, int frame_width, int frame_height) {
     }
 
     this->camera_index = this->video_device_name_to_index[camera_name];
-    cout << fmt::format(
-        "Found index {} for camera {}.", this->camera_index, camera_name);
+    cout << fmt::format("Found index {} for camera {}.",
+                        this->camera_index,
+                        camera_name)
+         << endl;
     this->camera_device = cv::VideoCapture(this->camera_index);
     this->camera_device.set(cv::CAP_PROP_FRAME_WIDTH, frame_width);
     this->camera_device.set(cv::CAP_PROP_FRAME_HEIGHT, frame_height);
