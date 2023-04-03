1d28035f791ff33456dd153c221ac1dbd4edfe18
Paulo Soares <paulosoares@dhcp-10-142-246-46.uawifi.arizona.edu>
2023-04-03 12:54:39 -0700

    Fixing bugs

--- human_experiments/scripts/cat/image_timestamp_scripts/start_image_timestamp.sh
+++ human_experiments/scripts/cat/image_timestamp_scripts/start_image_timestamp.sh
@@ -4,13 +4,14 @@ IMAGE_TIMESTAMP_SESSION_NAME="image-timestamp"
 
 __tmux_start_image_timestamp() {
     local experiment_path=$1
+    local experiment_block=$2
 
-    local monitoring_lion_face="$experiment_path/lion/face_images/"
-    local monitoring_lion_screen="$experiment_path/lion/screenshots/"
-    local monitoring_tiger_face="$experiment_path/tiger/face_images/"
-    local monitoring_tiger_screen="$experiment_path/tiger/screenshots/"
-    local monitoring_leopard_face="$experiment_path/leopard/face_images/"
-    local monitoring_leopard_screen="$experiment_path/leopard/screenshots/"
+    local monitoring_lion_face="$experiment_path/lion/face_images/block_$experiment_block"
+    local monitoring_lion_screen="$experiment_path/lion/screenshots/block_$experiment_block"
+    local monitoring_tiger_face="$experiment_path/tiger/face_images/block_$experiment_block"
+    local monitoring_tiger_screen="$experiment_path/tiger/screenshots/block_$experiment_block"
+    local monitoring_leopard_face="$experiment_path/leopard/face_images/block_$experiment_block"
+    local monitoring_leopard_screen="$experiment_path/leopard/screenshots/block_$experiment_block"
 
     mkdir -p "$monitoring_lion_face" >/dev/null
     mkdir -p "$monitoring_lion_screen" >/dev/null
