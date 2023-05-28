9387d7c0b9ccc9e02f8a6b91083dc4018a321185
CalebUAz <calebshibu@email.arizona.edu>
2023-05-27 19:21:46 -0700

    added multiprocessing to wrapper script

--- human_experiments/lab_software/tomcat-physio-data-extraction/wrapper_script.sh
+++ human_experiments/lab_software/tomcat-physio-data-extraction/wrapper_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Directories to ignore
+# Define an array of directories to ignore
 ignore=("exp_2022_04_01_13" "exp_2022_04_22_09" "exp_2023_04_17_13" "exp_2023_04_18_14" "exp_2023_04_20_14" "exp_2023_04_21_10" "exp_2023_04_24_13" "exp_2023_04_27_14" "exp_2023_04_28_10" "exp_2023_05_01_13" "exp_2023_05_02_14" "exp_2023_05_03_10")
 
 while getopts ":d:o:" opt; do
@@ -29,6 +29,18 @@ error_log="$outputdir/error_log.txt"
 # Search for directories starting with exp_ under the root directory
 directories=($(find "$rootdir" -type d -name "exp_*"))
 
+# Define the function to execute the Python script
+execute_python_script() {
+    local dir="$1"
+    local str_array=("$2" "$3" "$4")
+
+    # Call python script and save stderr to error log
+    if ! python3 run_physio_data_extraction.py --p1 "$dir" --p2 "$dir/baseline_tasks/" --p3 "$dir/minecraft/" --s "${str_array[0]}" --s "${str_array[1]}" --s "${str_array[2]}" --filter True --hdf5 True --output_path "$outputdir" 2>> "$error_log"; then
+        echo "Python script failed for $dir."
+    fi
+}
+
+# Iterate over the directories and start a separate process for each
 for dir in "${directories[@]}"; do
     # Extract the base directory name
     base_dir=$(basename "$dir")
@@ -51,11 +63,12 @@ for dir in "${directories[@]}"; do
     done
     
     # Print the command before executing
-    echo "Running: python3 run_physio_data_extraction.py --p1 \"$dir\" --p2 \"$dir/baseline_tasks/\" --p3 \"$dir/minecraft/\" --s1 \"${str_array[0]}\" --s2 \"${str_array[1]}\" --s3 \"${str_array[2]}\" --filter True --hdf5 True --output_path \"$outputdir\""
+    echo "Running: python3 run_physio_data_extraction.py --p1 \"$dir\" --p2 \"$dir/baseline_tasks/\" --p3 \"$dir/minecraft/\" --s \"${str_array[0]}\" --s \"${str_array[1]}\" --s \"${str_array[2]}\" --filter True --hdf5 True --output_path \"$outputdir\""
 
-    # Call python script with all the values and save stderr to error log
-    if ! python3 run_physio_data_extraction.py --p1 "$dir" --p2 "$dir/baseline_tasks/" --p3 "$dir/minecraft/" --s1 "${str_array[0]}" --s2 "${str_array[1]}" --s3 "${str_array[2]}" --filter True --hdf5 True --output_path "$outputdir" 2>> "$error_log"; then
-        echo "Python script failed for $dir. Continuing with the next directory."
-    fi
+    # Start a separate process to execute the Python script
+    execute_python_script "$dir" "${str_array[0]}" "${str_array[1]}" "${str_array[2]}" &
 
 done
+
+# Wait for all child processes to finish
+wait
