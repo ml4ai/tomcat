ff7e67a4ba6d9b8ee9c5af5ec182ee46f5359ea3
CalebUAz <calebshibu@email.arizona.edu>
2023-05-27 22:33:38 -0700

    bug fix on gauss?

--- human_experiments/lab_software/tomcat-physio-data-extraction/utils/folder_name_manipulation.py
+++ human_experiments/lab_software/tomcat-physio-data-extraction/utils/folder_name_manipulation.py
@@ -4,6 +4,18 @@ def get_new_file_paths(output_path, csv_file_name):
     # Ensure csv_file_name is a relative path
     csv_file_name = csv_file_name.lstrip("/")
     
+    # Split the path into its components
+    path_parts = csv_file_name.split("/")
+    
+    # Find the index of the part of the path we're interested in
+    try:
+        start_index = path_parts.index('study_3_pilot')
+    except ValueError:
+        raise ValueError("The csv file path must contain 'study_3_pilot'")
+    
+    # Include only the parts of the path after 'study_3_pilot'
+    csv_file_name = "/".join(path_parts[start_index + 1:])
+    
     # Ignore 'eeg_fnirs_pupil' in the path
     csv_file_name = csv_file_name.replace("eeg_fnirs_pupil/", "")
     
