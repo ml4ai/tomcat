06e59b42fc1867c784d734fc88abd036b08550b3
Adarsh Pyarelal <adarsh@arizona.edu>
2023-05-29 10:49:46 -0700

    improved logging

--- human_experiments/build_database/build_database.py
+++ human_experiments/build_database/build_database.py
@@ -7,13 +7,13 @@ import json
 from dataclasses import dataclass
 from glob import glob
 import logging
-from logging import info, warning
+from logging import info, warning, error
 from tqdm import tqdm
 import sqlite3
 import csv
 
 
-file_handler = logging.FileHandler(filename='build_database.log', mode='w')
+file_handler = logging.FileHandler(filename="build_database.log", mode="w")
 stderr_handler = logging.StreamHandler(stream=sys.stderr)
 handlers = [file_handler, stderr_handler]
 
@@ -59,8 +59,6 @@ def process_metadata_file(filepath, team_id, db_connection):
                 score = message["data"]["scoreboard"]["TeamScore"]
                 scores.append(score)
 
-
-
     if len(scores) != 0:
         warning(f"\t\tNo scoreboard messages found!")
         final_team_score = scores[-1]
@@ -84,7 +82,7 @@ def process_metadata_file(filepath, team_id, db_connection):
         )
     # info(f"Inserted row: {data}")
     # except sqlite3.IntegrityError:
-        # info(f"Unable to insert row: {data}")
+    # info(f"Unable to insert row: {data}")
 
 
 if __name__ == "__main__":
@@ -97,7 +95,7 @@ if __name__ == "__main__":
         db_connection.executescript(schema)
 
     with cd("/tomcat/data/raw/LangLab/experiments/study_3_pilot/group"):
-        for session in tqdm(sorted(os.listdir("."))):
+        for session in tqdm(sorted(os.listdir(".")[20:])):
             year, month, day, hour = [int(x) for x in session.split("_")[1:]]
 
             if year == 2022 and ((month < 9) or (month == 9 and day < 30)):
@@ -121,29 +119,44 @@ if __name__ == "__main__":
                 continue
 
             else:
+                info(f"Processing directory {session}")
                 team_id = None
+                info(f"\tProcessing redcap_data/team_data.csv")
                 with open(f"{session}/redcap_data/team_data.csv") as f:
                     reader = csv.DictReader(f)
                     for i, row in enumerate(reader):
-                       if i!=0:
-                           raise ValueError("More than one record found in team_data.csv!")
-                       team_id = int(row["team_id"])
-                       participants = [int(x) for x in row["subject_id"].split(",")]
-                       data = [(participant_id, team_id) for participant_id in participants]
-
-                       with db_connection:
-                            db_connection.executemany(
-                                "INSERT into participant VALUES(?, ?)", data
+                        if i != 0:
+                            raise ValueError(
+                                "More than one record found in team_data.csv!"
                             )
-                            info(f"Inserted rows: {data}")
+                        team_id = int(row["team_id"])
+                        participants = [
+                            int(x) for x in row["subject_id"].split(",")
+                        ]
+                        data = [
+                            (participant_id, team_id)
+                            for participant_id in participants
+                        ]
+
+                        try:
+                            with db_connection:
+                                db_connection.executemany(
+                                    "INSERT into participant VALUES(?, ?)",
+                                    data,
+                                )
+                                info(f"Inserted rows: {data}")
+                        except sqlite3.IntegrityError:
+                            error(f"Unable to insert rows: {data}")
+                            raise
 
                 try:
                     with cd(f"{session}/minecraft"):
-                        info(f"Processing directory {session}")
                         metadata_files = sorted(glob("*.metadata"))
                         for metadata_file in metadata_files:
                             info(f"\tProcessing file {metadata_file}")
-                            process_metadata_file(metadata_file, team_id, db_connection)
+                            process_metadata_file(
+                                metadata_file, team_id, db_connection
+                            )
 
                 except FileNotFoundError:
                     warning(f"minecraft directory not in {session}")
