b34eb69a10a08bd11c072de378bdd68b65e48ab7
CalebUAz <calebshibu@email.arizona.edu>
2023-06-02 13:37:20 -0700

    nirs merge fix needed

--- human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/NIRS_tasks_merge.py
+++ human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/NIRS_tasks_merge.py
@@ -11,16 +11,31 @@ def create_task_label_distribution(block, start, end, state):
     state = [state] * len(range_)
     state = {i: x for i, x in enumerate(state, start_index)}
 
-    block['event_type'] = block.index.map(state)
+    # block['event_type'] = block.index.map(state)
 
     print("Index of state_start:", start_index)
     print("Index of state_end:", end_index)
+    return start_index, end_index, state
 
-def NIRS_tasks_merge(lion_0297_block, tiger_0239_block, leopard_0171_block, rest_state):
+def NIRS_tasks_merge(lion_0297_block, tiger_0239_block, leopard_0171_block, task):
     iMacs = {'lion_0297_block': lion_0297_block, 'tiger_0239_block': tiger_0239_block, 'leopard_0171_block': leopard_0171_block}
 
+    start_stop_time = {}  # Create a dictionary that would contain the start and stop time of each task followed by the task name and the participant id.
+    idx = 0  # Index for the dictionary
+
     for iMac_name, iMac in iMacs.items():
-        iMac = create_task_label_distribution(iMac, rest_state[0], rest_state[1], state='rest_state') #1. Create task label distribution for rest state
+        start_index, end_index, state_series = create_task_label_distribution(iMac, rest_state[0], rest_state[1], state='rest_state') #1. Create task label distribution for rest state
         print(colored("[Status] Labeling dataframe with rest state for", "green", attrs=["bold"]), colored(iMac_name, "blue", attrs=["bold"]))
+        start_stop_time[idx] = {
+                        "state": "rest_state",
+                        "participant": None,
+                        "start_time":start_index,
+                        "end_time": end_index,
+                    }
+        idx += 1
+
+        start_index, end_index, state_series = create_task_label_distribution(iMac, rest_state[2], rest_state[3], state='finger_tapping') #2. Create task label distribution for finger tapping
+
+
 
         
