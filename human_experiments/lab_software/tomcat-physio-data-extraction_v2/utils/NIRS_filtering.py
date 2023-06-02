6858fa609c99c85311afb9e4fe0b0f19fa3f212c
CalebUAz <calebshibu@email.arizona.edu>
2023-06-01 22:59:08 -0700

    rest state labeled

---
+++ human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/NIRS_filtering.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import pandas as pd
+from .folder_name_manipulation import get_new_file_paths
+from termcolor import colored
+from scipy.signal import butter, lfilter_zi, lfilter
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    return butter(order, [lowcut, highcut], fs=fs, btype="band")
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def check_cv(
+    data,
+    path,
+    iloc_idx_start,
+    iloc_idx_end,
+    output_path,
+):
+    """
+    Calculate coeffecient of variance for fNIRS raw siganls, if cv is
+    greater than 7.5 then discard then log that channel as
+    bad.
+    https://doi.org/10.3390/app12010316 refer section 2.4. Channel Exclusion Criterion.
+    This function recives raw+HbO data but will use raw W1 to calculate
+    the cv.
+    """
+    new_csv_file_path = get_new_file_paths(output_path, path)
+
+    # Ensure the directory exists
+    if not os.path.exists(os.path.dirname(new_csv_file_path)):
+        os.makedirs(os.path.dirname(new_csv_file_path))
+
+    channels = {
+        "S1-D1",
+        "S1-D2",
+        "S2-D1",
+        "S2-D3",
+        "S3-D1",
+        "S3-D3",
+        "S3-D4",
+        "S4-D2",
+        "S4-D4",
+        "S4-D5",
+        "S5-D3",
+        "S5-D4",
+        "S5-D6",
+        "S6-D4",
+        "S6-D6",
+        "S6-D7",
+        "S7-D5",
+        "S7-D7",
+        "S8-D6",
+        "S8-D7",
+    }
+
+    print(
+        colored("[INFO]", "green", attrs=["bold"]),
+        colored("calulating coeffecient of variance of NIRS", "green", attrs=["bold"]),
+    )
+
+    data = pd.DataFrame(data[:, 1:21])
+
+    cv = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
+    coef_var = cv(data[iloc_idx_start:iloc_idx_end])
+    cv_vals = coef_var
+
+    channel_good_or_bad = (coef_var < 7.5).replace(
+        {True: "good_channel", False: "bad_channel"}
+    )
+
+    df = pd.DataFrame(
+        list(zip(channels, cv_vals, channel_good_or_bad)),
+        columns=["Channels", "coeff_of_var", "status"],
+    )
+    df.to_csv(new_csv_file_path + '/' +"NIRS_channel_quality.csv", index=False)
+
+
+def filter_NIRS(data):
+    """
+    Use butterworth bandpass filter with highpass set at 0.2hz and
+    lowpass set to 0.01hz. Also with a filter order of 3.
+    https://doi.org/10.3389%2Ffnhum.2018.00505,
+    https://doi.org/10.3390/a11050067,
+    https://doi.org/10.3389/fnhum.2019.00331
+    """
+
+    fs = 10.2
+    lowcut = 0.01
+    highcut = 0.2
+
+    fb, fa = butter_bandpass(lowcut, highcut, fs)
+    zi = lfilter_zi(fb, fa)
+
+    for i in range(40):
+        data.iloc[:, i] = butter_bandpass_filter(
+            data.iloc[:, i], lowcut, highcut, fs, order=3
+        )
+
+    return data
--- human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/NIRS_tasks_merge.py
+++ human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/NIRS_tasks_merge.py
@@ -1,21 +1,26 @@
 import numpy as np
 from termcolor import colored
 
-def create_task_label_distribution(block, start, end):
+def create_task_label_distribution(block, start, end, state):
     #Gets the index of the start and end of the rest state with respect to unix_time of the block. 
     #This is used to create a distribution of the task labels for the task.
     start_index = np.searchsorted(block['unix_time'].values, start)
     end_index = np.searchsorted(block['unix_time'].values, end)
 
-    print("Index of rest_state_start:", start_index)
-    print("Index of rest_state_end:", end_index)
+    range_ = list(range(start_index, end_index))
+    state = [state] * len(range_)
+    state = {i: x for i, x in enumerate(state, start_index)}
+
+    block['event_type'] = block.index.map(state)
+
+    print("Index of state_start:", start_index)
+    print("Index of state_end:", end_index)
 
 def NIRS_tasks_merge(lion_0297_block, tiger_0239_block, leopard_0171_block, rest_state):
-    iMacs = [lion_0297_block, tiger_0239_block, leopard_0171_block]
+    iMacs = {'lion_0297_block': lion_0297_block, 'tiger_0239_block': tiger_0239_block, 'leopard_0171_block': leopard_0171_block}
 
-    for iMac in iMacs:
-        iMac = create_task_label_distribution(iMac, rest_state[0], rest_state[1]) #1. Create task label distribution for rest state
-        print(colored("[Status] Labeling dataframe with Rest state ", "green", attrs=["bold"]))
-
+    for iMac_name, iMac in iMacs.items():
+        iMac = create_task_label_distribution(iMac, rest_state[0], rest_state[1], state='rest_state') #1. Create task label distribution for rest state
+        print(colored("[Status] Labeling dataframe with rest state for", "green", attrs=["bold"]), colored(iMac_name, "blue", attrs=["bold"]))
 
         
