0fa2c3ceda55012c2a099805d9031f14eca44bba
CalebUAz <calebshibu@email.arizona.edu>
2023-06-04 14:56:07 -0700

    make mapping concise

--- human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/label_data.py
+++ human_experiments/lab_software/tomcat-physio-data-extraction_v2/utils/label_data.py
@@ -1,128 +1,19 @@
-
+def update_state(block, state, start_time, end_time):
+    start_index = block["unix_time"].searchsorted(start_time)
+    end_index = block["unix_time"].searchsorted(end_time)
+    block.loc[start_index:end_index, 'state'] = state
 
 def label_data(lion_0297_block, tiger_0239_block, leopard_0171_block, markers):
-    for key, value in markers.items():
-        if value['state'] == 'rest_state':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'rest_state'
+    block_mapping = {'lion': lion_0297_block, 'tiger': tiger_0239_block, 'leopard': leopard_0171_block}
+    
+    for value in markers.values():
+        state = value['state']
+        participants = value.get('participant', {'lion', 'tiger', 'leopard'}) 
+
+        # If 'participants' is None or a string, convert it to a set
+        if participants is None or isinstance(participants, str):
+            participants = {'lion', 'tiger', 'leopard'} if participants is None else {participants}
             
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'rest_state'
-            
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'rest_state'
-
-        if value['state'] == 'finger_tapping':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'finger_tapping'
-
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'finger_tapping'
-
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'finger_tapping'
-        
-        if value['state'] == 'affective_task_individual' and value['participant'] == 'lion':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'affective_task_individual'
-        
-        if value['state'] == 'affective_task_individual' and value['participant'] == 'tiger':
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'affective_task_individual'
-        
-        if value['state'] == 'affective_task_individual' and value['participant'] == 'leopard':
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'affective_task_individual'
-
-        if value['state'] == 'affective_task_team':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'affective_task_team'
-            
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'affective_task_team'
-            
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'affective_task_team'
-
-        if value['state'] == 'ping_pong_competetive_0' and value['participant'] == {'tiger', 'lion'}:
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'ping_pong_competetive_0'
-
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'ping_pong_competetive_0'
-        
-        if value['state'] == 'ping_pong_competetive_1' and value['participant'] == {'leopard'}:
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'ping_pong_competetive_1'
-
-        if value['state'] == 'ping_pong_cooperative_0': 
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            print(start_index, end_index)
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'ping_pong_cooperative_0'
-
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            print(start_index, end_index)
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'ping_pong_cooperative_0'
-
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            print(start_index, end_index)
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'ping_pong_cooperative_0'
-
-        if value['state'] == 'hands_on_training':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'hands_on_training'
-
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'hands_on_training'
-
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'hands_on_training'
-
-        if value['state'] == 'saturn_a':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'saturn_a'
-
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'saturn_a'
-
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'saturn_a'
-        
-        if value['state'] == 'saturn_b':
-            start_index = lion_0297_block["unix_time"].searchsorted(value['start_time'])
-            end_index = lion_0297_block["unix_time"].searchsorted(value['end_time'])
-            lion_0297_block.loc[start_index:end_index, 'state'] = 'saturn_b'
-
-            start_index = tiger_0239_block["unix_time"].searchsorted(value['start_time'])
-            end_index = tiger_0239_block["unix_time"].searchsorted(value['end_time'])
-            tiger_0239_block.loc[start_index:end_index, 'state'] = 'saturn_b'
-
-            start_index = leopard_0171_block["unix_time"].searchsorted(value['start_time'])
-            end_index = leopard_0171_block["unix_time"].searchsorted(value['end_time'])
-            leopard_0171_block.loc[start_index:end_index, 'state'] = 'saturn_b'
-
-    lion_0297_block.to_csv('/Users/calebjonesshibu/Desktop/test/lion_0297_block.csv')        
+        for participant in participants:
+            if participant in block_mapping:
+                update_state(block_mapping[participant], state, value['start_time'], value['end_time'])
