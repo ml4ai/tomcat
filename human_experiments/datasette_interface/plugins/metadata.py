93fa9814ba13a18dbbd53bd57d81e7ca35ef048f
Adarsh Pyarelal <adarsh@email.arizona.edu>
2023-06-14 10:15:34 -0700

    Sqlite DB and Datasette interface (#505)
    This PR adds scripts to create an SQLite DB for the ToMCAT data and configure/launch a Datasette instance for the public to interactively explore the data.
    
    ---------
    
    Co-authored-by: Adarsh Pyarelal <adarsh@arizona.edu>
    Co-authored-by: Paulo Soares <paulosoares@SBS-7186.local>

---
+++ human_experiments/datasette_interface/plugins/metadata.py
@@ -0,0 +1,39 @@
+"""Plugin to do the following:
+1. Get metadata information into custom page templates
+2. Set up a route (/-/structured_metadata.json) to get structured metadata.
+3. Add extra menu links.
+"""
+
+from pathlib import Path
+from datasette import hookimpl, Response
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+@hookimpl
+def extra_template_vars():
+    # We do the convoluted way to find the metadata.yml file below to work with
+    # Docker Compose mounts.
+    with open((Path(__file__).parents[1]/"metadata.yml").resolve()) as f:
+        metadata = yaml.safe_load(f.read())
+
+    return {"metadata": metadata}
+
+def get_structured_metadata():
+    # We do the convoluted way to find the metadata.yml file below to work with
+    # Docker Compose mounts.
+
+    # Adarsh: The two lines below overlap with the extra_template_vars function, but I
+    # got some errors when I tried to refactor, so I'm living with the code
+    # duplication for now.
+    with open((Path(__file__).parents[1]/"metadata.yml").resolve()) as f:
+        metadata = yaml.safe_load(f.read())
+
+    return Response.json(metadata["structured_metadata"])
+
+@hookimpl
+def register_routes():
+    return [("/-/structured_metadata.json", get_structured_metadata)]
---
+++ human_experiments/datasette_interface/process_rest_state_task_data.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import sqlite3
+from utils import (
+    cd,
+    should_ignore_directory,
+    logging_handlers,
+    convert_unix_timestamp_to_iso8601,
+    is_directory_with_unified_xdf_files,
+)
+import pyxdf
+import logging
+from logging import info, error
+from config import DB_PATH, logging_handlers
+from tqdm import tqdm
+import pandas as pd
+from glob import glob
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=logging_handlers,
+)
+
+
+def process_directory_v1(session, db_connection):
+    info(f"Processing directory {session}")
+    with cd(f"{session}/baseline_tasks/rest_state"):
+        csv_files = glob("*.csv")
+
+        # We expect exactly one CSV file.
+        assert len(csv_files) == 1
+
+        if os.stat(csv_files[0]).st_size == 0:
+            error(
+                f"[MISSING DATA]: The size of the CSV file {csv_files[0]} that"
+                " should have contained the rest state task timestamps is"
+                " zero, so we cannot process it."
+            )
+            return
+
+        df = pd.read_csv(csv_files[0], delimiter=";")
+
+        start_timestamp_unix = df["time"].iloc[0]
+        stop_timestamp_unix = df["time"].iloc[-1]
+        row = (
+            session,
+            start_timestamp_unix,
+            convert_unix_timestamp_to_iso8601(start_timestamp_unix),
+            stop_timestamp_unix,
+            convert_unix_timestamp_to_iso8601(stop_timestamp_unix),
+        )
+
+        with db_connection:
+            db_connection.execute(
+                "INSERT into rest_state_task VALUES(?, ?, ?, ?, ?);", row
+            )
+
+
+def process_directory_v2(session, db_connection):
+    """Process directory assuming unified XDF files."""
+    info(f"Processing directory {session}")
+    with cd(f"{session}/lsl"):
+        streams, header = pyxdf.load_xdf(
+            "block_1.xdf", select_streams=[{"type": "rest_state"}]
+        )
+        stream = streams[0]
+        start_timestamp_lsl, stop_timestamp_lsl = stream["time_stamps"]
+
+        row = (
+            session,
+            start_timestamp_lsl,
+            convert_unix_timestamp_to_iso8601(start_timestamp_lsl),
+            stop_timestamp_lsl,
+            convert_unix_timestamp_to_iso8601(stop_timestamp_lsl),
+        )
+
+        with db_connection:
+            db_connection.execute(
+                "INSERT into rest_state_task VALUES(?, ?, ?, ?, ?);", row
+            )
+
+
+def process_rest_state_task_data():
+    info("Processing directories...")
+
+    db_connection = sqlite3.connect(DB_PATH)
+    with db_connection:
+        info("Dropping rest_state_task table")
+        db_connection.execute("DROP TABLE IF EXISTS rest_state_task")
+        db_connection.execute(
+            """
+            CREATE TABLE rest_state_task (
+                group_session_id TEXT NOT NULL,
+                start_timestamp_unix TEXT NOT NULL,
+                start_timestamp_iso8601 TEXT NOT NULL,
+                stop_timestamp_unix TEXT NOT NULL,
+                stop_timestamp_iso8601 TEXT NOT NULL,
+                FOREIGN KEY(group_session_id) REFERENCES group_session(id)
+            );"""
+        )
+
+    with cd("/tomcat/data/raw/LangLab/experiments/study_3_pilot/group"):
+        directories_to_process = [
+            directory
+            for directory in os.listdir(".")
+            if not should_ignore_directory(directory)
+        ]
+
+        for session in tqdm(
+            sorted(directories_to_process), unit="directories"
+        ):
+            if not is_directory_with_unified_xdf_files(session):
+                process_directory_v1(session, db_connection)
+            else:
+                process_directory_v2(session, db_connection)
+
+
+if __name__ == "__main__":
+    info(
+        """
+        Processing rest state task data. For the CSV files predating the
+        unified XDF file era, We will use the `time` column in the CSV,"
+        ignoring the `monotonic_time` and `human_readable` time columns,
+        since those timestamps are systematically a few microseconds later
+        than the timestamps in the `time` column, since they are created by
+        separate invocations to monotonic() and datetime.utcnow() respectively.
+        """
+    )
+    process_rest_state_task_data()
