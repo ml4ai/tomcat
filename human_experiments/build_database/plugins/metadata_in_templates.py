0792c13c08b61f3335f6df00b047808057f6161e
Adarsh Pyarelal <adarsh@arizona.edu>
2023-06-13 20:15:16 -0700

    added route that returns structured metadata

--- human_experiments/build_database/plugins/metadata_in_templates.py
+++ human_experiments/build_database/plugins/metadata_in_templates.py
@@ -1,12 +1,13 @@
 """Plugin to get metadata information into custom page templates"""
 
 from pathlib import Path
-from datasette import hookimpl
+from datasette import hookimpl, Response
 import yaml
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
+
 
 @hookimpl
 def extra_template_vars():
@@ -14,4 +15,17 @@ def extra_template_vars():
     # Docker Compose mounts.
     with open((Path(__file__).parents[1]/"metadata.yml").resolve()) as f:
         metadata = yaml.safe_load(f.read())
+
     return {"metadata": metadata}
+
+def get_structured_metadata():
+    # We do the convoluted way to find the metadata.yml file below to work with
+    # Docker Compose mounts.
+    with open((Path(__file__).parents[1]/"metadata.yml").resolve()) as f:
+        metadata = yaml.safe_load(f.read())
+
+    return Response.json(metadata["structured_metadata"])
+
+@hookimpl
+def register_routes():
+    return [("/-/structured_metadata.json", get_structured_metadata)]
