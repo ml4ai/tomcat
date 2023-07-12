f8a5fde7f8570299948b3f9e8bc36c583863699e
Adarsh Pyarelal <adarsh@arizona.edu>
2023-07-12 05:49:41 -0700

    made build_database_from_scratch script executable

--- human_experiments/datasette_interface/build_database_from_scratch.sh
+++ human_experiments/datasette_interface/build_database_from_scratch.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+
+# Build database from scratch
+
 ./process_rick_workbook.py
-./process_rest_state_task_data.py
+#./process_rest_state_task_data.py
+#./process_fnirs_data.py
 ./process_affective_task_data.py
-./process_minecraft_data.py
-./process_fnirs_data.py
+./process_testbed_messages.py
