a2074f7c4ac5594ebfbc81b0991e99c727b59707
Adarsh Pyarelal <adarsh@arizona.edu>
2023-07-14 10:22:43 -0700

    increasing facet time limit

--- human_experiments/datasette_interface/launch_dev.sh
+++ human_experiments/datasette_interface/launch_dev.sh
@@ -10,5 +10,6 @@ datasette \
     --plugins-dir=plugins/ \
     --inspect-file=inspect-data.json \
     --setting sql_time_limit_ms 10000 \
+    --setting facet_time_limit_ms 10000 \
     --setting num_sql_threads 10 \
     --setting max_csv_mb 4000 \
