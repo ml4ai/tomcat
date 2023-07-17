4a34fff2275f2524a1560531706bb6aa47afc4ab
Adarsh Pyarelal <adarsh@arizona.edu>
2023-07-16 23:04:03 -0700

    renamed process_rick_workbook.py to build_base_tables.py, updated build_database_from_scratch.sh script

--- human_experiments/datasette_interface/build_database_from_scratch.sh
+++ human_experiments/datasette_interface/build_database_from_scratch.sh
@@ -2,8 +2,12 @@
 
 # Build database from scratch
 
-./process_rick_workbook.py
-#./process_rest_state_task_data.py
-#./process_fnirs_data.py
+./build_base_tables.py
+./process_rest_state_task_data.py
 ./process_affective_task_data.py
+./process_fingertapping_task_data.py
+./process_ping_pong_competitive_data.py
+./process_ping_pong_cooperative_data.py
 ./process_testbed_messages.py
+./process_fnirs_data.py
+./update_inspect_file.sh
