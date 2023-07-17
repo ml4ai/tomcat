9718c43822a901c872ca98345db28efa46cf4e85
Adarsh Pyarelal <adarsh@arizona.edu>
2023-07-17 14:08:14 -0700

    added comment about generating database diagram

--- human_experiments/datasette_interface/generate_database_diagram.sh
+++ human_experiments/datasette_interface/generate_database_diagram.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# This script generates an up-to-date schema diagram for the ToMCAT database.
+# It uses the SchemaCrawler tool. You can get it from here: https://github.com/schemacrawler/SchemaCrawler/releases
+# To run the script, ensure that the directory containing the schemacrawler.sh
+# script is in your PATH.
+
 schemacrawler.sh \
     -c schema \
     -F png \
