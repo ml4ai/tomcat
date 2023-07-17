d1ae57e715e4e891486cbbb298bbf993822577e7
Adarsh Pyarelal <adarsh@arizona.edu>
2023-07-17 07:39:47 -0700

    wip

--- human_experiments/datasette_interface/build_database_from_scratch.sh
+++ human_experiments/datasette_interface/build_database_from_scratch.sh
@@ -2,12 +2,16 @@
 
 # Build database from scratch
 
-./build_base_tables.py
-./process_rest_state_task_data.py
-./process_affective_task_data.py
-./process_fingertapping_task_data.py
-./process_ping_pong_competitive_data.py
-./process_ping_pong_cooperative_data.py
-./process_testbed_messages.py
-./process_fnirs_data.py
-./update_inspect_file.sh
+echo "Building database from scratch."
+
+./build_base_tables.py \
+&& ./process_rest_state_task_data.py \
+&& ./process_affective_task_data.py \
+&& ./process_fingertapping_task_data.py \
+&& ./process_ping_pong_competitive_data.py \
+&& ./process_ping_pong_cooperative_data.py \
+&& ./process_testbed_messages.py \
+&& ./process_fnirs_data.py \
+&& ./update_inspect_file.sh
+
+echo "Finished building database from scratch"
