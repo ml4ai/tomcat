831e7f932ed08766df102f78fe42434712bc0b5e
Adarsh Pyarelal <adarsh@arizona.edu>
2023-08-17 16:42:14 -0700

    Added launch_dev_mutable.sh script

---
+++ human_experiments/datasette_interface/launch_dev_mutable.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Shell script for launching the Datasette instance in dev mode
+
+set -euo pipefail
+datasette \
+    /space/${USER}/tomcat/tomcat.db \
+    --metadata metadata.yml \
+    --reload \
+    --template-dir=templates \
+    --plugins-dir=plugins/ \
+    --static assets:static \
+    --setting sql_time_limit_ms 10000 \
+    --setting facet_time_limit_ms 10000 \
+    --setting num_sql_threads 10 \
+    --setting max_csv_mb 12000 \
