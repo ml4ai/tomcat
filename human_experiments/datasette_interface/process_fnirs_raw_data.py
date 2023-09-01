3aab3b9ba65642fceae7beb8d63e0a2afaffa388
Paulo Soares <paulosoares@SBS-7186.local>
2023-09-01 12:45:46 -0300

    Debugging

--- human_experiments/datasette_interface/process_fnirs_raw_data.py
+++ human_experiments/datasette_interface/process_fnirs_raw_data.py
@@ -66,6 +66,7 @@ def get_signals(stream, group_session, station):
     task = None
     participant_id = -1
     channels = stream["info"]["desc"][0]["channels"][0]["channel"][41:]
+    print(channels)
     signals = [
         FNIRSRaw(group_session_id=group_session,
                  id=i,
