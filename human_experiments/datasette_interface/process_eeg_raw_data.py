6b63ffa0ec02862b7064a046675e32cc8a45583f
Paulo Soares <paulosoares@SBS-7186.local>
2023-09-06 12:47:26 -0300

    Debug

--- human_experiments/datasette_interface/process_eeg_raw_data.py
+++ human_experiments/datasette_interface/process_eeg_raw_data.py
@@ -74,7 +74,7 @@ def get_signals(stream, group_session, station, initial_id):
 
     print(stream["info"]["desc"][0]["channels"][0]["channel"])
 
-    channels = [channel["custom_name"][0].lower() for channel in stream["info"]["desc"][0]["channels"][0]["channel"]]
+    channels = [channel["label"][0].lower() for channel in stream["info"]["desc"][0]["channels"][0]["channel"]]
 
     print(channels)
 
