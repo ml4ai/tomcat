95aa96f97f64e6cd113651bc7300933467a66a6d
Paulo Soares <paulosoares@SBS-7186.local>
2023-09-08 08:23:48 -0300

    Fix header in PCM audio files

---
+++ human_experiments/data_pre_processing/fix_audio_header.py
@@ -0,0 +1,88 @@
+from utils import cd, is_directory_with_unified_xdf_files
+import logging
+from logging import info, error
+import argparse
+from config import EXP_DIR, OUT_DIR, USER
+import sys
+import os
+from entity.pcm_audio import PCMAudio
+from tqdm import tqdm
+
+"""
+Some audio files do not contain the Subchunk2Size part of the PCM-format header filled, which causes the file not
+to be recognized by the apple audio player or even python libraries to read wave files. This script completes the
+Subchunk2Size data in the header of the audio files according to what it is supposed to contain as described in
+http://soundfile.sapp.org/doc/WaveFormat/
+"""
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=(
+        logging.FileHandler(
+            # filename=f"/space/{USER}/tomcat/fix_audio_header.log",
+            filename=f"/Users/{USER}/data/langlab/study3/fix_audio_header.log",
+            mode="w",
+        ),
+        logging.StreamHandler(stream=sys.stderr),
+    ),
+)
+
+
+def fix_audio_header(experiments_dir: str, out_dir: str):
+    info("Processing directories...")
+
+    with cd(experiments_dir):
+        directories_to_process = [directory for directory in os.listdir(".") if directory[:4] == "exp_"]
+
+        for group_session in tqdm(sorted(directories_to_process), unit="directories"):
+            info(f"Processing group session {group_session}")
+
+            if not is_directory_with_unified_xdf_files(group_session):
+                process_directory_v1(group_session, out_dir)
+            else:
+                process_directory_v2(group_session, out_dir)
+
+
+def process_directory_v1(group_session: str, out_dir: str):
+    for station in ["lion", "tiger", "leopard"]:
+        audio_dir = f"{group_session}/{station}/audio"
+        if not os.path.exists(audio_dir):
+            error(f"Audio folder does not exist for station {station} in group session {group_session}.")
+            continue
+
+        for audio_file in os.listdir(audio_dir):
+            audio = PCMAudio(filepath=f"{audio_dir}/{audio_file}")
+
+            os.makedirs(f"{out_dir}/{audio_dir}", exist_ok=True)
+            audio.fix_header(out_filepath=f"{out_dir}/{audio_dir}/{audio_file}")
+
+
+def process_directory_v2(group_session: str, out_dir: str):
+    for station in ["lion", "tiger", "leopard"]:
+        audio_dir = f"{group_session}/{station}/audio/block_2"
+        if not os.path.exists(audio_dir):
+            error(f"Audio folder does not exist for station {station} in group session {group_session}.")
+            continue
+
+        for audio_file in os.listdir(audio_dir):
+            audio = PCMAudio(filepath=f"{audio_dir}/{audio_file}")
+
+            os.makedirs(f"{out_dir}/{audio_dir}", exist_ok=True)
+            audio.fix_header(out_filepath=f"{out_dir}/{audio_dir}/{audio_file}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Parses a collection of experiments, finds the audio files, fix their header by filling the"
+                    " Subchunk2Size portion and save them to the output folder, maintaining the folder structure"
+                    " in the original experiment."
+    )
+
+    parser.add_argument("--experiments_dir", type=str, required=True, default=EXP_DIR,
+                        help="Directory containing experiment folders.")
+    parser.add_argument("--out_dir", type=str, required=True, default=OUT_DIR,
+                        help="Directory where experiment folder structure containing fixed audios must be saved.")
+
+    args = parser.parse_args()
+
+    fix_audio_header(args.experiments_dir, args.out_dir)
---
+++ human_experiments/data_pre_processing/utils.py
@@ -0,0 +1,17 @@
+import os
+import contextlib
+
+
+@contextlib.contextmanager
+def cd(path):
+    old_path = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+def is_directory_with_unified_xdf_files(session):
+    year, month, day, hour = [int(x) for x in session.split("_")[1:]]
+    return (year, month) >= (2023, 4)
