e24e173c6af1c0ec4784143557ba5c62288d376b
Paulo Soares <paulosoares@email.arizona.edu>
2023-09-11 09:10:47 -0400

    Fix audio file headers and generate summary (#541)
    * Fix header in PCM audio files
    
    * Generate audio summary
    
    ---------
    
    Co-authored-by: Paulo Soares <paulosoares@SBS-7186.local>

---
+++ human_experiments/data_pre_processing/entity/pcm_audio.py
@@ -0,0 +1,33 @@
+import os
+
+
+class PCMAudio:
+
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+
+    def fix_header(self, out_filepath: str):
+        """
+        Fill header fields that depend on the final size of the audio file.
+
+        Reference: http://soundfile.sapp.org/doc/WaveFormat/
+        """
+
+        file_size = os.path.getsize(self.filepath)
+        chunksize = file_size - 8  # file size in bytes - 8 bytes of header (ChunkID and ChunkSize)
+        chunksize = chunksize.to_bytes(4, "little")
+
+        subchunk2size = file_size - 44  # file size in bytes - 44 bytes of header
+        subchunk2size_in_bytes = subchunk2size.to_bytes(4, "little")
+
+        with open(self.filepath, 'rb') as input_file, open(out_filepath, 'wb') as output_file:
+            input_data = input_file.read()
+            output_file.write(input_data)
+
+            # Write chunksize at 4th byte
+            output_file.seek(4)
+            output_file.write(chunksize)
+
+            # Write subchunk2size at 40th byte
+            output_file.seek(40)
+            output_file.write(subchunk2size_in_bytes)
