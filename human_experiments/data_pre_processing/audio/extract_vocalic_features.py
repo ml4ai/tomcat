dd88565a34c7d172a4623155a2a9cc0b3b39a059
Paulo Soares <paulosoares@SBS-7186.local>
2023-09-13 08:29:00 -0300

    Add override option to vocalics script

--- human_experiments/data_pre_processing/audio/extract_vocalic_features.py
+++ human_experiments/data_pre_processing/audio/extract_vocalic_features.py
@@ -12,7 +12,7 @@ from common.config import EXP_DIR, OUT_DIR, LOG_DIR
 from utils import cd, is_directory_with_unified_xdf_files
 
 
-def extract_vocalic_features(experiments_dir: str, out_dir: str):
+def extract_vocalic_features(experiments_dir: str, out_dir: str, override: bool):
     info("Processing directories...")
 
     directories_to_process = [directory for directory in os.listdir(experiments_dir) if
@@ -24,20 +24,20 @@ def extract_vocalic_features(experiments_dir: str, out_dir: str):
         experiment_dir = f"{experiments_dir}/{group_session}"
 
         if not is_directory_with_unified_xdf_files(experiment_dir):
-            process_directory_v1(experiment_dir, out_dir)
+            process_directory_v1(experiment_dir, out_dir, override)
         else:
-            process_directory_v2(experiment_dir, out_dir)
+            process_directory_v2(experiment_dir, out_dir, override)
 
 
-def process_directory_v1(experiment_dir: str, out_dir: str):
-    return process_directory(experiment_dir, out_dir, lambda g, s: f"{g}/{s}/audio")
+def process_directory_v1(experiment_dir: str, out_dir: str, override: bool):
+    return process_directory(experiment_dir, out_dir, lambda g, s: f"{g}/{s}/audio", override)
 
 
-def process_directory_v2(experiment_dir: str, out_dir: str):
-    return process_directory(experiment_dir, out_dir, lambda g, s: f"{g}/{s}/audio/block_2")
+def process_directory_v2(experiment_dir: str, out_dir: str, override: bool):
+    return process_directory(experiment_dir, out_dir, lambda g, s: f"{g}/{s}/audio/block_2", override)
 
 
-def process_directory(experiment_dir: str, out_dir: str, audio_dir_fn: Callable):
+def process_directory(experiment_dir: str, out_dir: str, audio_dir_fn: Callable, override: bool):
     for station in ["lion", "tiger", "leopard"]:
         audio_dir = audio_dir_fn(experiment_dir, station)
         if not os.path.exists(audio_dir):
@@ -49,14 +49,20 @@ def process_directory(experiment_dir: str, out_dir: str, audio_dir_fn: Callable)
             if audio_file[audio_file.rfind("."):].lower() != ".wav":
                 continue
 
-            audio = PCMAudio(filepath=f"{audio_dir}/{audio_file}")
-            logging.info(f"Processing audio {audio_file}.")
-
             sub_dir = audio_dir[audio_dir.find("exp_"):] + "/vocalics"
             os.makedirs(f"{out_dir}/{sub_dir}", exist_ok=True)
 
             vocalic_filename = audio_file[:audio_file.rfind(".")] + ".csv"
-            audio.extract_vocalic_features(f"{out_dir}/{sub_dir}/{vocalic_filename}")
+            vocalic_filepath = f"{out_dir}/{sub_dir}/{vocalic_filename}"
+
+            if os.path.exists(vocalic_filepath) and not override:
+                info(f"Skipping file {audio_file}. Vocalics already found.")
+                continue
+
+            audio = PCMAudio(filepath=f"{audio_dir}/{audio_file}")
+            info(f"Processing audio {audio_file}.")
+
+            audio.extract_vocalic_features(vocalic_filepath)
 
 
 if __name__ == "__main__":
@@ -71,6 +77,7 @@ if __name__ == "__main__":
                         help="Directory where experiment folder structure containing vocalic features files must be saved.")
     parser.add_argument("--log_dir", type=str, required=False, default=LOG_DIR,
                         help="Directory where log files must be saved.")
+    parser.add_argument("--override", action='store_true', help="Do not reprocess data already processed.")
 
     args = parser.parse_args()
 
@@ -86,4 +93,4 @@ if __name__ == "__main__":
         ),
     )
 
-    extract_vocalic_features(args.experiments_dir, args.out_dir)
+    extract_vocalic_features(args.experiments_dir, args.out_dir, args.override)
