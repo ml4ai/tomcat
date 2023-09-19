f29fed25360f2cc9ec570afe7baced16275d2325
Paulo Soares <paulosoares@SBS-7186.local>
2023-09-19 10:56:09 -0300

    Save MC trial start and end messages to the database as well.

--- human_experiments/datasette_interface/process_minecraft_data.py
+++ human_experiments/datasette_interface/process_minecraft_data.py
@@ -26,7 +26,7 @@ logging.basicConfig(
     level=logging.INFO,
     handlers=(
         logging.FileHandler(
-            filename=f"/space/{USER}/tomcat/build_testbed_message_table.log", mode="w"
+            filename=f"/space/{USER}/tomcat/build_testbed_message_table_tmp.log", mode="w"
         ),
         logging.StreamHandler(stream=sys.stderr),
     ),
