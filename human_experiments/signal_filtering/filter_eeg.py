e0a44e7e1ee9fb853706ba4dd6df4f3edc7b5ee2
Eric Duong <eduong@arizona.edu>
2023-09-26 13:07:22 -0700

    Use the correct table name

--- human_experiments/signal_filtering/filter_eeg.py
+++ human_experiments/signal_filtering/filter_eeg.py
@@ -5,7 +5,7 @@ from write_data import write_db_all, write_csv_all
 
 if __name__ == "__main__":
     blacklist_experiment = ["exp_2022_10_24_12"]
-    experiments_eeg_raw = read_raw_db_all("eeg_raw_tmp", NUM_PROCESSES, Modality.EEG, blacklist_experiment)
+    experiments_eeg_raw = read_raw_db_all("eeg_raw", NUM_PROCESSES, Modality.EEG, blacklist_experiment)
     experiments_eeg_filtered = filter_signal_all(experiments_eeg_raw, eeg_filter, NUM_PROCESSES)
     write_csv_all(experiments_eeg_filtered, EEG_FILTERED_PATH, NUM_PROCESSES)
     # write_db_all(experiments_eeg_filtered, "eeg_filtered", NUM_PROCESSES)
--- human_experiments/signal_filtering/filter_fnirs.py
+++ human_experiments/signal_filtering/filter_fnirs.py
@@ -4,7 +4,7 @@ from read_data import read_raw_db_all
 from write_data import write_db_all, write_csv_all
 
 if __name__ == "__main__":
-    experiments_fnirs_raw = read_raw_db_all("fnirs_raw_tmp", NUM_PROCESSES)
+    experiments_fnirs_raw = read_raw_db_all("fnirs_raw", NUM_PROCESSES)
     experiments_fnirs_filtered = filter_signal_all(experiments_fnirs_raw, fnirs_filter, NUM_PROCESSES)
     write_csv_all(experiments_fnirs_filtered, FNIRS_FILTERED_PATH, NUM_PROCESSES)
     # write_db_all(experiments_fnirs_filtered, "fnirs_filtered", NUM_PROCESSES)
