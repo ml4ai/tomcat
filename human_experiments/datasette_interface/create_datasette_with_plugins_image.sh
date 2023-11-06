7dba69ade262fb21a1714e8063ef1634ffd316e6
Adarsh Pyarelal <adarsh@arizona.edu>
2023-11-06 07:33:40 -0700

    Updating create_datasette_with_plugins_image.sh to include datasette-copyable and datasette-render-image-tags plugins

--- human_experiments/datasette_interface/create_datasette_with_plugins_image.sh
+++ human_experiments/datasette_interface/create_datasette_with_plugins_image.sh
@@ -7,6 +7,7 @@ docker pull datasetteproject/datasette:0.64.3
 docker run datasetteproject/datasette \
     pip install \
         datasette-pretty-json \
-        datasette-render-markdown
+        datasette-copyable \ # plugin for outputting tables in formats suitable for copy and paste
+        datasette-render-image-tags # Turn any URLs ending in .jpg/.png/.gif into img tags with width 200
 
 docker commit $(docker ps -lq) datasette-with-plugins
