e0df93e0415b8bf620b7b195fbbec4174839f700
Paulo Soares <paulosoares@dhcp-10-142-175-96.uawifi.arizona.edu>
2024-01-02 10:15:21 -0700

    Minor fix

--- human_experiments/datasette_interface/datasette_interface/derived/helper/factory.py
+++ human_experiments/datasette_interface/datasette_interface/derived/helper/factory.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy import Engine
 
 from datasette_interface.derived.helper.eeg import EEGHelper
 from datasette_interface.derived.helper.fnirs import FNIRSHelper
@@ -6,7 +6,7 @@ from datasette_interface.derived.helper.modality import ModalityHelper
 
 
 def create_modality_helper(
-    modality: str, group_session: str, station: str, db: Session
+        modality: str, group_session: str, station: str, db_engine: Engine
 ) -> ModalityHelper:
     """
     Creates a modality helper from a name.
@@ -14,12 +14,12 @@ def create_modality_helper(
     :param modality: name of the modality. One of eeg or fnirs.
     :param group_session: group session to process.
     :param station: station to process.
-    :param db: active database session.
+    :param db_engine: database engine.
     """
     if modality == "eeg":
-        return EEGHelper(group_session=group_session, station=station, db=db)
+        return EEGHelper(group_session=group_session, station=station, db_engine=db_engine)
 
     if modality == "fnirs":
-        return FNIRSHelper(group_session=group_session, station=station, db=db)
+        return FNIRSHelper(group_session=group_session, station=station, db_engine=db_engine)
 
     raise ValueError(f"Invalid modality ({modality}).")
