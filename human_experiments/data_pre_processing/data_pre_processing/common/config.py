797206dec99509cb532c034376e4074ebc142c3e
Paulo Soares <paulosoares@dhcp-10-142-130-253.uawifi.arizona.edu>
2024-01-08 09:37:40 -0700

    Removing data_pre_processing project

--- human_experiments/data_pre_processing/data_pre_processing/common/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-USER = os.getenv("USER")
-
-EXP_DIR = "/tomcat/data/raw/LangLab/experiments/study_3_pilot/group"
-OUT_DIR = f"/space/{USER}/tomcat/preprocessed"
-LOG_DIR = f"/space/{USER}/tomcat"
-
-NUM_PROCESSES = 40
